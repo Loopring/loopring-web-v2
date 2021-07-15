b9a122a5b33ce8659556224797bf23fa87a8caaa
windatang <windatang@gmail.com>
2021-07-15 15:26:36 +0800

    format code

--- packages/webapp/src/stores/Amm/AmmActivityMap/reducer.ts
+++ packages/webapp/src/stores/Amm/AmmActivityMap/reducer.ts
@@ -15,7 +15,7 @@ const ammActivityMapSlice:Slice = createSlice({
         getAmmActivityMap(state, action:PayloadAction<string | undefined>) {
             state.status = STATUS.PENDING
         },
-        updateAmmActivityMap(state, action: PayloadAction<AmmActivityMapStates>) {
+        getAmmActivityMapStatus(state, action: PayloadAction<AmmActivityMapStates>) {
             // @ts-ignore
             if (action.error) {
                 state.status =  STATUS.ERROR
