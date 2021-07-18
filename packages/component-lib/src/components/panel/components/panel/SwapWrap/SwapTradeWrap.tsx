73d8321b9c6d0cc9d6920063a2adfa0e11888011
leo chan <leochan007@163.com>
2021-07-19 05:01:11 +0800

    add feeBips fro swap.

--- packages/component-lib/src/components/panel/components/panel/SwapWrap/SwapTradeWrap.tsx
+++ packages/component-lib/src/components/panel/components/panel/SwapWrap/SwapTradeWrap.tsx
@@ -115,6 +115,8 @@ export const SwapTradeWrap = <T extends IBData<I>,
 
     const priceImpact = tradeCalcData && tradeCalcData.priceImpact ? parseFloat(tradeCalcData.priceImpact).toPrecision(3).toString() + ' %' : EmptyValueTag
 
+    const fee = tradeCalcData && tradeCalcData.fee ? (tradeCalcData.fee + '%%') : EmptyValueTag
+
     return <Grid className={tradeCalcData ? '' : 'loading'} paddingLeft={5 / 2} paddingRight={5 / 2} container
                  direction={"column"}
                  justifyContent={'space-between'} alignItems={"center"} flex={1} height={'100%'}>
@@ -205,7 +207,7 @@ export const SwapTradeWrap = <T extends IBData<I>,
                     <Grid container justifyContent={'space-between'} direction={"row"} alignItems={"center"}>
                         <Typography component={'p'} variant="body1"> {t('swapFee')} </Typography>
                         <Typography component={'p'}
-                                    variant="body1">{t(tradeCalcData ? tradeCalcData.fee : EmptyValueTag)}</Typography>
+                                    variant="body1">{fee}</Typography>
                     </Grid>
                 </Grid>
                 <Grid item>
