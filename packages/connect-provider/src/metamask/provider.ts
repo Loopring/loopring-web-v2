46a88b5deb3dd67f6ea75f55eee39338f5b27716
windatang <windatang@gmail.com>
2021-07-20 17:48:30 +0800

    done

--- packages/connect-provider/src/metamask/provider.ts
+++ packages/connect-provider/src/metamask/provider.ts
@@ -5,21 +5,39 @@ import { walletServices } from '../walletServices';
 
 export const MetaMaskProvide = async ()=>{
     const provider:any = await detectEthereumProvider();
-    if(provider){
+    const ethereum:any = window.ethereum;
+    if(provider && ethereum){
         const web3 = new Web3(provider as any);
+        // let accounts = web3.eth.accounts;
+        // accounts = await web3.eth.getAccounts();
+        //accounts = await ethereum.request({ method: 'eth_accounts' });
         walletServices.sendConnect(web3,provider)
-        provider.on("accountsChanged", (accounts: string[]) => {
-            walletServices.sendConnect(web3,provider)
+        //
+        // ethereum
+        //     .request({ method: 'eth_accounts' })
+        //     .then((accounts)=>{
+        //
+        //     })
+        //     .catch((err) => {
+        //         // Some unexpected error.
+        //         // For backwards compatibility reasons, if no accounts are available,
+        //         // eth_accounts will return an empty array.
+        //         console.error(err);
+        //     });
+
+        console.log('provider:',provider)
+        provider.on("accountsChanged", (accounts: Array<string>) => {
+           // const _accounts = await web3.eth.getAccounts();
+            console.log('accounts:',accounts)
+           // walletServices.sendConnect(web3,provider)
         });
         provider.on("chainChanged", (chainId: number) => {
-            //walletServices.sendConnect(web3)
-            //provider.disconnect()
-            walletServices.sendChainChanged(chainId)
+            walletServices.sendChainChanged(chainId);
         });
         provider.on("disconnect", (code: number, reason: string) => {
-            console.log(code, reason);
             walletServices.sendDisconnect(code,reason);
         });
+       
     }
     return  provider;
     // provider;
