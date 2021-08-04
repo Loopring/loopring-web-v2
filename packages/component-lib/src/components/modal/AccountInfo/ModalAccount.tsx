26fcf5ce754dda7fba2bec4a6de5411efe0ecec0
windatang <windatang@gmail.com>
2021-08-04 18:48:30 +0800

    fix show providor

--- packages/component-lib/src/components/modal/AccountInfo/ModalAccount.tsx
+++ packages/component-lib/src/components/modal/AccountInfo/ModalAccount.tsx
@@ -3,17 +3,20 @@ import { Modal } from '@material-ui/core';
 import SwipeableViews from 'react-swipeable-views';
 import { useTheme } from '@emotion/react';
 import { Box } from '@material-ui/core/';
-import { ModalCloseButton, ModalAccountProps, SwitchPanelStyled } from '../../../index';
+import { ModalCloseButton, ModalAccountProps, SwitchPanelStyled, ModalBackButton } from '../../../index';
 
 export const ModalAccount = withTranslation('common', {withRef: true})((
     {
         open,
         onClose,
         step,
+        onBack,
+        style,
         panelList,
         ...rest
     }: ModalAccountProps & WithTranslation) => {
     const theme = useTheme();
+    const {w,h} = style?style:{w:undefined,h:undefined};
 
     return <Modal
         open={open}
@@ -23,8 +26,11 @@ export const ModalAccount = withTranslation('common', {withRef: true})((
     >
 
         <SwitchPanelStyled style={{boxShadow: '24'}}
-                           {...{_height: 'var(--modal-height)', _width: 'var(--modal-width)' }}>
-            <ModalCloseButton onClose={onClose} {...rest} />
+                           {...{_height: h?h:'var(--modal-height)', _width: w?w:'var(--modal-width)' }}>
+            <Box display={'flex'}>
+                <ModalCloseButton onClose={onClose} {...rest} />
+                {onBack?<ModalBackButton onBack={onBack}  {...rest}/>:<></>}
+            </Box>
             <SwipeableViews animateTransitions={false} axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'} index={step}>
                 {panelList.map((panel,index)=>{
                     return <Box key={index}>
