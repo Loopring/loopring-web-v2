391d69d2b5682c72e9f526143a66672e2e87fc95
leo chan <leochan007@163.com>
2021-08-09 11:05:29 +0800

    add global provider

--- packages/webapp/src/App.tsx
+++ packages/webapp/src/App.tsx
@@ -10,26 +10,27 @@ import { useInit } from './hook';
 import { ErrorPage } from './pages/ErrorPage';
 import { LoadingPage } from './pages/LoadingPage';
 import { ModalGroup } from './modal';
+import { GlobalProvider } from 'provider/global';
 
 const App = () => {
-    const theme: Theme = useTheme();
-    const {state} = useInit();
-    // const [status, setStatus] = React.useState<keyof typeof SagaStatus>('PENDING');
-    // check all status be
-    //TODO  demo if  tokenMapStatus is unset and tokenMap is empty. show error
-    //TODO tokenMapObj.status is pending, show global loading
-    //console.log(tokenMapObj.tokenMap && Object.keys(tokenMapObj.tokenMap).length>0,tokenMapObj.status, tokenMapObj.errorMessage)
+  const theme: Theme = useTheme();
+  const { state } = useInit();
+  // const [status, setStatus] = React.useState<keyof typeof SagaStatus>('PENDING');
+  // check all status be
+  //TODO  demo if  tokenMapStatus is unset and tokenMap is empty. show error
+  //TODO tokenMapObj.status is pending, show global loading
+  //console.log(tokenMapObj.tokenMap && Object.keys(tokenMapObj.tokenMap).length>0,tokenMapObj.status, tokenMapObj.errorMessage)
 
-    // useEffect(() => {
-    //     if (state === SagaStatus.PENDING || state === SagaStatus.ERROR) {
-    //         setStatus(state)
-    //     } else {
-    //         setStatus('DONE')
-    //     }
-    // }, [state, setStatus])
+  // useEffect(() => {
+  //     if (state === SagaStatus.PENDING || state === SagaStatus.ERROR) {
+  //         setStatus(state)
+  //     } else {
+  //         setStatus('DONE')
+  //     }
+  // }, [state, setStatus])
 
-    return <><GlobalStyles styles={css` 
-      ${globalCss({theme})};
+  return <><GlobalStyles styles={css` 
+      ${globalCss({ theme })};
       body{
           ${theme.mode === 'dark' ? `
             color: ${theme.colorBase.textPrimary};
@@ -46,20 +47,21 @@ const App = () => {
       //  flex-direction: column;
       //}
 }`}></GlobalStyles>
-        <ModalProvider>
-            {/*<GlobalProvider>*/}
-            {/*<Web3ReactManager>*/}
-            {state === 'PENDING' ?
-                <LoadingPage/>
-                // <ErrorPage {...ErrorMap.LOADING_WHOLE_SITE}/>
-                // <Avatar src={loadingSvg}/>
-                : state === 'ERROR' ? <ErrorPage {...ErrorMap.NO_NETWORK_ERROR}/> : <>
-                    <RouterView/>
+    <ModalProvider>
+      <GlobalProvider>
+        {/*<Web3ReactManager>*/}
+        {state === 'PENDING' ?
+          <LoadingPage />
+          // <ErrorPage {...ErrorMap.LOADING_WHOLE_SITE}/>
+          // <Avatar src={loadingSvg}/>
+          : state === 'ERROR' ? <ErrorPage {...ErrorMap.NO_NETWORK_ERROR} /> : <>
+            <RouterView />
 
-                    {/*    <ErrorPage {...ErrorMap.LOADING_WHOLE_SITE}/>*/}
-                </>}
+            {/*    <ErrorPage {...ErrorMap.LOADING_WHOLE_SITE}/>*/}
+          </>}
+      </GlobalProvider>
 
-        </ModalProvider></>
+    </ModalProvider></>
 
 
 }
