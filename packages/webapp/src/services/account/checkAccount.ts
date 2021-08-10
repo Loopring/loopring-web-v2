712d3d76e254de3feb2c87ca0c4412bc6e9aa0a4
windatang <windatang@me.com>
2021-08-11 01:00:49 +0800

    clean code

--- packages/webapp/src/services/account/checkAccount.ts
+++ packages/webapp/src/services/account/checkAccount.ts
@@ -8,13 +8,12 @@ export const checkAccount = (newAccAddress: string) => {
     if (account.accAddress === '' || account.accAddress !== newAccAddress) {
         myLog('After connect >>,account part: diff account, clean layer2')
         store.dispatch(cleanAccountStatus(undefined));
-    }
-    if (newAccAddress && newAccAddress !== '') {
+        walletLayer2Services.sendCheckAccount(newAccAddress);
+    }else if (newAccAddress && newAccAddress !== '') {
         myLog('After connect >>,checkAccount: step1 address',newAccAddress)
-        if (account.accountId === -1) {
+        if (account &&  account.accountId === -1) {
             myLog('After connect >>,checkAccount: step1 no account Id')
             walletLayer2Services.sendCheckAccount(newAccAddress)
-            store.dispatch(updateAccountStatus({accAddress: newAccAddress}))
         } else if (account.accountId && account.apiKey && account.eddsaKey) {
             myLog('After connect >>,checkAccount: step1 have activate account from store, account:', account)
             walletLayer2Services.sendAccountSigned();
