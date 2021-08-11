95442d64e9d74768c1410f8b5bb857e590bb27e4
SylvanasGone <sylvanasgone@gmail.com>
2021-08-12 02:10:34 +0800

    refactor: liqudity page

--- packages/common-resources/static-resources/src/i18n/zh_CN/common.ts
+++ packages/common-resources/static-resources/src/i18n/zh_CN/common.ts
@@ -195,5 +195,5 @@ export default {
     describeTitleOnErrorNetwork:'您登入的网络，路印暂不支持，请在{{connectName}}切换网络',
     describeTitleConnectToWallet:'点击按钮，连接路印钱包，开始二层之旅',
     describeTitleLocked:'已连接您的钱包，点击解锁后查看账户信息',
-
+    labelLiqudityPageTitle: 'AMM 资金池',
 }
