a43c5659637d0bcaabf0b98c41a7894d2703718a
windatang <windatang@gmail.com>
2021-08-12 14:29:51 +0800

    model

--- packages/common-resources/static-resources/src/themes/css/global.tsx
+++ packages/common-resources/static-resources/src/themes/css/global.tsx
@@ -42,7 +42,7 @@ export const colorBase = ({theme}: any) => css`
     --color-border-hover: ${theme.colorBase.borderHover};
     --color-divide: ${theme.colorBase.divide};
     --color-pop-bg: ${theme.colorBase.popBg};
-    --color-box: ${theme.coorBase.box};
+    --color-box: ${theme.colorBase.box};
     --color-box-secondary: ${theme.colorBase.boxSecondary};
     --color-box-hover: ${theme.colorBase.boxHover};
     --color-box-linear: ${theme.colorBase.boxLinear};
