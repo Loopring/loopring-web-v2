2ffa69f7104b7dcb268e744ec6306282c2157de2
leo chan <leochan007@163.com>
2021-08-12 17:43:17 +0800

    change btn def.

--- packages/webapp/src/layouts/connectStatusCallback.tsx
+++ packages/webapp/src/layouts/connectStatusCallback.tsx
@@ -1,16 +1,16 @@
 import store from '../stores';
 import { AccountStep, setShowAccount, setShowConnect, Toast, WalletConnectStep } from '@loopring-web/component-lib';
 import { fnType } from '@loopring-web/common-resources';
-import { Provider } from 'react-redux';
 import { changeShowModel } from 'stores/account';
 import { walletLayer2Services } from 'services/account/walletLayer2Services'
+import { myLog } from 'utils/log_tools';
 
-
-export const accountStaticCallBack = (onclickMap: { [ key: number ]: [fn: (props: any) => any, args?: any[]] }, deps?: any[]) => {
-    const {readyState} = store.getState().account;
+export const accountStaticCallBack = (onclickMap: { [key: number]: [fn: (props: any) => any, args?: any[]] }, deps?: any[]) => {
+    const { readyState } = store.getState().account;
     // fnType[readyState]
     let fn, args;
-    [fn, args] = onclickMap[ readyState ] ? onclickMap[ readyState ] : [];
+    [fn, args] = onclickMap[readyState] ? onclickMap[readyState] : [];
+    myLog('accountStaticCallBack:', readyState)
     if (typeof fn === 'function') {
         args = [...(args ?? []), ...(deps ?? [])] as [props: any]
         return fn.apply(this, args);
@@ -18,29 +18,29 @@ export const accountStaticCallBack = (onclickMap: { [ key: number ]: [fn: (props
 
 }
 
-export const bntLabel: typeof btnClickMap = {
+export const btnLabel: typeof btnClickMap = {
 
-    [ fnType.UN_CONNECT ]: [
+    [fnType.UN_CONNECT]: [
         function () {
             return `labelConnectWallet`
         }
     ],
-    [ fnType.ERROR_NETWORK ]: [
+    [fnType.ERROR_NETWORK]: [
         function () {
             return `labelWrongNetwork`
         }
     ],
 
-    [ fnType.DEFAULT ]: [
+    [fnType.DEFAULT]: [
         function () {
             return `depositTitleAndActive`
         }
-    ], [ fnType.ACTIVATED ]: [
+    ], [fnType.ACTIVATED]: [
         function () {
             return undefined
         }
     ]
-    , [ fnType.LOCKED ]: [
+    , [fnType.LOCKED]: [
         function () {
             return `labelUnLockLayer2`
         }
@@ -48,40 +48,39 @@ export const bntLabel: typeof btnClickMap = {
 };
 
 
-export const btnClickMap: { [ key: string ]: [fn: (props: any) => any, args?: any[]] } = {
-    [ fnType.ERROR_NETWORK ]: [
+export const btnClickMap: { [key: string]: [fn: (props: any) => any, args?: any[]] } = {
+    [fnType.ERROR_NETWORK]: [
         function () {
             //TODO toast
             return
         }
     ],
-    [ fnType.UN_CONNECT ]: [
+    [fnType.UN_CONNECT]: [
         function () {
             // setShowConnect({isShow: true})
-            store.dispatch(changeShowModel({_userOnModel: true}));
-            store.dispatch(setShowConnect({isShow: true, step:WalletConnectStep.Provider}))
+            store.dispatch(changeShowModel({ _userOnModel: true }));
+            store.dispatch(setShowConnect({ isShow: true, step: WalletConnectStep.Provider }))
         }
     ]
-    , [ fnType.NO_ACCOUNT ]: [
+    , [fnType.NO_ACCOUNT]: [
         function () {
-            store.dispatch(changeShowModel({_userOnModel: true}));
-            store.dispatch(setShowAccount({isShow: true, step: AccountStep.NoAccount}))
+            store.dispatch(changeShowModel({ _userOnModel: true }));
+            store.dispatch(setShowAccount({ isShow: true, step: AccountStep.NoAccount }))
             // ShowDeposit(true)
         }
     ]
-    , [ fnType.DEPOSITING ]: [
+    , [fnType.DEPOSITING]: [
         function () {
             walletLayer2Services.sendCheckAcc()
             // store.dispatch(setShowAccount({isShow: true, step: AccountStep.Depositing}))
             // ShowDeposit(true)
         }
     ]
-    , [ fnType.LOCKED ]: [
+    , [fnType.LOCKED]: [
         function () {
-            store.dispatch(changeShowModel({_userOnModel: true}));
-            store.dispatch(setShowAccount({isShow: true, step: AccountStep.HadAccount}))
+            store.dispatch(changeShowModel({ _userOnModel: true }));
+            store.dispatch(setShowAccount({ isShow: true, step: AccountStep.HadAccount }))
         }
     ]
 };
-
 
