fcf91c51509f9eb0d746b6b0ae090f7dd15a45ab
leo chan <leochan007@163.com>
2021-08-24 21:21:40 +0800

    update

--- packages/component-lib/src/components/panel/components/SwapWrap/SwapTradeWrap.tsx
+++ packages/component-lib/src/components/panel/components/SwapWrap/SwapTradeWrap.tsx
@@ -158,8 +158,8 @@ export const SwapTradeWrap = <T extends IBData<I>,
 
     const convertStr = _isStoB ? `1${tradeData.sell?.belong} \u2248 ${tradeCalcData?.StoB ? tradeCalcData.StoB : EmptyValueTag} ${tradeData.buy?.belong}`
         : `1${tradeData.buy?.belong} \u2248 ${tradeCalcData.BtoS ? tradeCalcData.BtoS : EmptyValueTag} ${tradeData.sell?.belong}`
-    const priceImpactColor =  'var(--color-success)'|| 'var(--color-warning)'||'error'||'inherit';
-    const priceImpact = (tradeCalcData && tradeCalcData.priceImpact) ? parseFloat(tradeCalcData.priceImpact).toPrecision(3).toString() + ' %' : EmptyValueTag
+    const priceImpactColor =  tradeCalcData?.priceImpactColor ? tradeCalcData.priceImpactColor : ''
+    const priceImpact = tradeCalcData?.priceImpact ?  tradeCalcData.priceImpact + ' %' : EmptyValueTag
 
     const fee = (tradeCalcData && tradeCalcData.fee) ? ((parseFloat(tradeCalcData.fee) / 100).toString() + '%') : EmptyValueTag
 
