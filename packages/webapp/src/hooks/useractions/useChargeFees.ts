8570be15e5e24de92ddc1a53fe2b13c5fb4c8f1c
leo chan <leochan007@163.com>
2021-08-24 09:42:13 +0800

    fix withdraw fee issue.

--- packages/webapp/src/hooks/useractions/useChargeFees.ts
+++ packages/webapp/src/hooks/useractions/useChargeFees.ts
@@ -12,6 +12,9 @@ import { useCustomDCEffect } from 'hooks/common/useCustomDCEffect';
 import { LoopringAPI } from 'api_wrapper';
 import * as sdk from 'loopring-sdk';
 import { myLog } from 'utils/log_tools';
+import React from 'react';
+import * as _ from 'lodash'
+import { globalSetup } from '@loopring-web/common-resources'
 
 export function useChargeFees(tokenSymbol: string | undefined, requestType: OffchainFeeReqType,
     tokenMap: LoopringMap<TokenInfo> | undefined, amount?: number) {
@@ -20,49 +23,58 @@ export function useChargeFees(tokenSymbol: string | undefined, requestType: Offc
 
     const [chargeFeeList, setChargeFeeList] = useState<any[]>([])
 
-    useCustomDCEffect(async () => {
+    const getFeeList = React.useCallback(
+        _.debounce(async (accountId: number, apiKey: string, tokenSymbol: string | undefined, requestType: OffchainFeeReqType, 
+            tokenMap: LoopringMap<TokenInfo> | undefined, amount: number | undefined) => {
 
-        if (account.accountId === -1 || !account.apiKey || !tokenSymbol || typeof tokenSymbol !== 'string'
-         || !tokenMap || !LoopringAPI.userAPI) {
-            return
-        }
-
-        myLog('tokenSymbol:', tokenSymbol, ' requestType:', OffchainFeeReqType[requestType])
-
-        let chargeFeeList: any[] = []
-
-        try {
-            const tokenInfo = tokenMap[ tokenSymbol ]
-
-            const request: GetOffchainFeeAmtRequest = {
-                accountId: account.accountId,
-                tokenSymbol,
-                requestType,
-                amount: amount ? toBig(amount).times('1e' + tokenInfo.decimals).toFixed(0, 0) : undefined
+            if (accountId === -1 || !apiKey || !tokenSymbol || typeof tokenSymbol !== 'string'
+                || !tokenMap || !LoopringAPI.userAPI) {
+                return
             }
 
-            const response = await LoopringAPI.userAPI.getOffchainFeeAmt(request, account.apiKey)
+            myLog('tokenSymbol:', tokenSymbol, ' requestType:', OffchainFeeReqType[requestType])
 
-            if (response?.raw_data?.fees instanceof Array) {
-                response.raw_data.fees.forEach((item: any) => {
-                    const feeRaw = item.fee
-                    const tokenInfo = tokenMap[ item.token ]
-                    const fee = sdk.toBig(item.fee).div('1e' + tokenInfo.decimals).toNumber()
-                    chargeFeeList.push({belong: item.token, fee, __raw__: feeRaw})
-                })
+            let chargeFeeList: any[] = []
 
-                setChargeFeeList(chargeFeeList)
+            try {
+                const tokenInfo = tokenMap[tokenSymbol]
+
+                const request: GetOffchainFeeAmtRequest = {
+                    accountId,
+                    tokenSymbol,
+                    requestType,
+                    amount: amount ? toBig(amount).times('1e' + tokenInfo.decimals).toFixed(0, 0) : '0'
+                }
+
+                myLog('request:', request)
+
+                const response = await LoopringAPI.userAPI.getOffchainFeeAmt(request, apiKey)
+
+                myLog('response:', response)
+
+                if (response?.raw_data?.fees instanceof Array) {
+                    response.raw_data.fees.forEach((item: any) => {
+                        const feeRaw = item.fee
+                        const tokenInfo = tokenMap[item.token]
+                        const fee = sdk.toBig(item.fee).div('1e' + tokenInfo.decimals).toNumber()
+                        chargeFeeList.push({ belong: item.token, fee, __raw__: feeRaw })
+                    })
+                }
+
+            } catch (reason) {
+                dumpError400(reason)
             }
-            myLog('response:', response)
 
-        } catch (reason) {
-            dumpError400(reason)
+            setChargeFeeList(chargeFeeList)
         }
+            , globalSetup.wait)
+    , [])
 
+    useCustomDCEffect(() => {
 
-        setChargeFeeList(chargeFeeList)
+        getFeeList(account.accountId, account.apiKey, tokenSymbol, requestType, tokenMap, amount)
 
-    }, [account.accountId, account.apiKey, LoopringAPI.userAPI, requestType, tokenSymbol, tokenMap])
+    }, [account.accountId, account.apiKey, tokenSymbol, requestType, tokenMap, amount])
 
     return {
         chargeFeeList,
