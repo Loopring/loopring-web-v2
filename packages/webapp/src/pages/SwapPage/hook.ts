b2dfa85e23a26e99700b24ac80280e1984a492c9
windatang <windatang@gmail.com>
2021-08-25 17:44:11 +0800

    Merge branch 'alpha-v2' of github.com:Loopring/loopring-web-v2 into alpha-v2

--- packages/webapp/src/pages/SwapPage/hook.ts
+++ packages/webapp/src/pages/SwapPage/hook.ts
@@ -322,30 +322,38 @@ export const useSwapPage = <C extends { [key: string]: any }>() => {
 
     //Btn related function
     const btnLabelAccountActive = React.useCallback((): string | undefined => {
-
         const validAmt = (output?.amountBOut && quoteMinAmt
             && sdk.toBig(output?.amountBOut).gte(sdk.toBig(quoteMinAmt))) ? true : false;
         if (isSwapLoading) {
             setSwapBtnStatus(TradeBtnStatus.LOADING)
             return undefined
         } else {
-            if (validAmt || quoteMinAmt === undefined) {
-                setSwapBtnStatus(TradeBtnStatus.AVAILABLE)
-                return undefined
+           if(account.readyState === AccountStatus.ACTIVATED){
+               if ( tradeData === undefined
+                   || tradeData?.sell.tradeValue === undefined
+                   || tradeData?.buy.tradeValue === undefined
+                   || tradeData?.sell.tradeValue === 0
+                   || tradeData?.buy.tradeValue === 0) {
+                   setSwapBtnStatus(TradeBtnStatus.DISABLED)
+                   return 'labelEnterAmount';
+               } else if (validAmt || quoteMinAmt === undefined) {
+                   setSwapBtnStatus(TradeBtnStatus.AVAILABLE)
+                   return undefined
 
-            } else if (tradeData === undefined || tradeData?.sell.tradeValue === undefined || tradeData?.buy.tradeValue === undefined) {
-                setSwapBtnStatus(TradeBtnStatus.DISABLED)
-                return 'labelEnterAmount';
-            } else {
-                const quote = tradeData?.buy.belong;
-                const minOrderSize = VolToNumberWithPrecision(quoteMinAmt, quote) + ' ' + tradeData?.buy.belong;
-                setSwapBtnStatus(TradeBtnStatus.DISABLED)
-                return `labelLimitMin, ${minOrderSize}`
+               } else {
+                   const quote = tradeData?.buy.belong;
+                   const minOrderSize = VolToNumberWithPrecision(quoteMinAmt, quote as any) + ' ' + tradeData?.buy.belong;
+                   setSwapBtnStatus(TradeBtnStatus.DISABLED)
+                   return `labelLimitMin, ${minOrderSize}`
 
-            }
+               }
+
+           }else{
+               setSwapBtnStatus(TradeBtnStatus.AVAILABLE)
+           }
 
         }
-    }, [quoteMinAmt, tradeData, isSwapLoading, setSwapBtnStatus])
+    }, [account.readyState, quoteMinAmt, tradeData, isSwapLoading, setSwapBtnStatus])
 
     const _btnLabel = Object.assign(deepClone(btnLabel), {
         [fnType.ACTIVATED]: [
@@ -354,10 +362,11 @@ export const useSwapPage = <C extends { [key: string]: any }>() => {
     });
 
     React.useEffect(() => {
-        if (accountStatus === SagaStatus.UNSET) {
+        if (accountStatus === SagaStatus.UNSET ) {
+            setSwapBtnStatus(TradeBtnStatus.AVAILABLE)
             setSwapBtnI18nKey(accountStaticCallBack(_btnLabel));
         }
-    }, [account.readyState, accountStatus, isSwapLoading, tradeData?.sell.tradeValue])
+    }, [accountStatus, isSwapLoading, tradeData?.sell.tradeValue])
 
     const swapCalculatorCallback = React.useCallback(async ({ sell, buy, slippage, ...rest }: any) => {
 
@@ -614,6 +623,9 @@ export const useSwapPage = <C extends { [key: string]: any }>() => {
                 coinBuy: coinB,
                 sellCoinInfoMap,
                 buyCoinInfoMap,
+                priceImpact: '',
+                priceImpactColor: 'inherit',
+                minimumReceived: '',
             }
 
             setTradeCalcData({ ...tradeCalcData, ..._tradeCalcData, } as TradeCalcData<C>)
@@ -724,6 +736,9 @@ export const useSwapPage = <C extends { [key: string]: any }>() => {
                     buyCoinInfoMap: tradeCalcData.sellCoinInfoMap,
                     StoB: tradeCalcData.BtoS,
                     BtoS: tradeCalcData.StoB,
+                    priceImpact: '',
+                    priceImpactColor: 'inherit',
+                    minimumReceived: '',
                 }
                 
                 myLog('Exchange,tradeCalcData', tradeCalcData);
