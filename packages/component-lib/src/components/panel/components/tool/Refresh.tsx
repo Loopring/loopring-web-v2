ae009f5ee400c8935ba3b36c3ce815b7e7ff29bf
windatang <windatang@me.com>
2021-08-26 00:06:49 +0800

    5

--- packages/component-lib/src/components/panel/components/tool/Refresh.tsx
+++ packages/component-lib/src/components/panel/components/tool/Refresh.tsx
@@ -4,32 +4,35 @@ import React from 'react';
 import { globalSetup, refreshTime } from '@loopring-web/common-resources';
 import * as _ from 'lodash'
 
+// @ts-ignore
 export const CountDownIcon = React.memo(React.forwardRef(({
                                              onRefreshData,
                                              wait = globalSetup.wait
-                                         }: { wait?: number, onRefreshData?: () => void },ref) => {
-    const countDownRef = React.useRef<any>(ref);
+                                         }: { wait?: number, onRefreshData?: () => void }, ref) => {
+    const countDownRef = React.useRef<any>();
+    // React.createRef
     const [refreshCount, setRefreshCount] = React.useState(0);
     const nodeTimer = React.useRef<NodeJS.Timeout | -1>(-1);
     const logoTimer = React.useRef<NodeJS.Timeout | -1>(-1);
     // const [triggerRefresh,setTriggerRefresh]  = React.useState(false);
-    const refresh = () => {_refresh()}
+    // const refresh = () => {_refresh()}
 
     React.useEffect(() => {
-
         if ((refreshCount === 0) && onRefreshData) {
             onRefreshData()
 
         }
-
     }, [refreshCount])
     // React.useEffect(()=>{
     //
     // },[shouldRefresh])
 
     const startCountDown = React.useCallback(() => {
+        //@ts-ignore
         if (countDownRef && countDownRef.current) {
+            //@ts-ignore
             countDownRef.current?.classList.add('countdown');
+            //@ts-ignore
             countDownRef.current?.classList?.remove('logo');
             // setRefreshCount(refreshTime-1);
             if (nodeTimer.current !== -1) {
@@ -38,7 +41,8 @@ export const CountDownIcon = React.memo(React.forwardRef(({
             nodeTimer.current = setInterval(decreaseNum, 1000)
         }
     }, [countDownRef, nodeTimer])
-    const _refresh = React.useCallback(_.debounce(() => {
+    const refresh = React.useCallback(_.debounce(() => {
+        //@ts-ignore
         if (countDownRef && countDownRef.current) {
             // setRefreshCount(0)
             if (nodeTimer.current !== -1) {
@@ -47,7 +51,9 @@ export const CountDownIcon = React.memo(React.forwardRef(({
             if (logoTimer.current !== -1) {
                 clearTimeout(logoTimer.current as NodeJS.Timeout);
             }
+            //@ts-ignore
             countDownRef.current?.classList?.remove('countdown');
+            //@ts-ignore
             countDownRef.current?.classList?.add('logo');
             setRefreshCount(0);
             logoTimer.current = setTimeout(() => {
@@ -80,15 +86,15 @@ export const CountDownIcon = React.memo(React.forwardRef(({
         clearTimeout(logoTimer.current as NodeJS.Timeout);
     }, [nodeTimer, logoTimer])
     React.useEffect(() => {
-        _refresh();
+        // _refresh();
         return cleanSubscribe;
     }, []);
-    return <CountDownStyled ref={countDownRef}
+    return <Box ref={ref}><CountDownStyled ref={countDownRef} component={'button'}
                             className={'clock-loading outline logo'}
                             onClick={refresh}>
         <Typography component={'span'} className={'text-count'}>
             {/*{refreshCount>0?refreshCount:''}*/}
         </Typography>
         <Box className={'circle'}/>
-    </CountDownStyled>
+    </CountDownStyled> </Box>
 }))
