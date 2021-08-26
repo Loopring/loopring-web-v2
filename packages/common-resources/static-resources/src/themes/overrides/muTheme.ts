e332baadd6b3caf60ba8bff4ea75dba70206fe0f
windatang <windatang@gmail.com>
2021-08-26 11:21:51 +0800

    Merge branch 'alpha-v2' of github.com:Loopring/loopring-web-v2 into alpha-v2

--- packages/common-resources/static-resources/src/themes/overrides/muTheme.ts
+++ packages/common-resources/static-resources/src/themes/overrides/muTheme.ts
@@ -166,7 +166,7 @@ export const getTheme = (themeMode: ThemeKeys): LoopringTheme => {
             MuiInputBase: MuiInputBase({colorBase,themeMode}),
             MuiMenu: MuiMenu({colorBase}),
             MuiMenuItem: MuiMenuItem({colorBase,themeMode}),
-            MuiList: MuiList(),
+            MuiList: MuiList({colorBase}),
             MuiListItem: MuiListItem({colorBase}),
             MuiListItemAvatar: MuiListItemAvatar(),
             MuiInputLabel: MuiInputLabel({colorBase}),
