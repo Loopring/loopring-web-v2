b5aba0d5a7b81294424094593f5a904e97dc2bf6
windatang <windatang@gmail.com>
2021-08-27 11:41:19 +0800

    Merge branch 'alpha-v2' of github.com:Loopring/loopring-web-v2 into alpha-v2

--- packages/webapp/src/stores/token/interface.ts
+++ packages/webapp/src/stores/token/interface.ts
@@ -1,6 +1,7 @@
 import { CoinKey, CoinMap, StateBase } from '@loopring-web/common-resources';
 import { MarketInfo, TokenInfo, TokenRelatedInfo } from 'loopring-sdk';
 import { LoopringMap } from 'loopring-sdk';
+import * as loopring_defs from 'loopring-sdk/dist/defs/loopring_defs';
 
 export type TokenMap<R extends { [ key: string ]: any }> = LoopringMap<TokenInfo & { tradePairs: Array<CoinKey<R>> }>
 export  type GetTokenMapParams<R extends { [ key: string ]: any }> = { tokensMap: TokenMap<R>, marketMap: LoopringMap<MarketInfo>, pairs: LoopringMap<TokenRelatedInfo>, marketArr: string[], tokenArr: string[] }
@@ -12,7 +13,6 @@ export type IdMap = {
     [ key: number ]: string
 }
 
-// export type TokenPairMap<R extends {[key:string]:any}> ={ [key in keyof R]: Array<keyof R>}
 export type TokenMapStates<R extends { [ key: string ]: any }> = {
     coinMap?: CoinMap<R> | undefined,
     totalCoinMap?: CoinMap<R> | undefined,
