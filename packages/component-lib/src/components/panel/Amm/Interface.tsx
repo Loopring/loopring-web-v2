7ffd6bc76d7ec7546a31a7be9746121f95e0507c
leo chan <leochan007@163.com>
2021-08-28 19:52:29 +0800

    update amm panel

--- packages/component-lib/src/components/panel/Amm/Interface.tsx
+++ packages/component-lib/src/components/panel/Amm/Interface.tsx
@@ -3,8 +3,8 @@ import { AmmData, CoinInfo, IBData } from '@loopring-web/common-resources';
 import { AmmChgData, AmmDepositBaseProps, AmmWithdrawBaseProps } from '../components';
 
 export enum AmmPanelType {
-    Join = 'Join',
-    Exit = 'Exit',
+    Join = 0,
+    Exit = 1
 }
 
 /**
@@ -14,7 +14,7 @@ export type AmmPanelBaseProps<T, I, ACD, C> = {
 
     ammDepositData: T,
     ammWithdrawData: T,
-    tabSelected?: keyof typeof AmmPanelType,
+    tabSelected?: AmmPanelType,
     disableDeposit?: boolean,
     disableWithdraw?: boolean,
     ammCalcData: ACD,
@@ -31,12 +31,12 @@ export type AmmPanelBaseProps<T, I, ACD, C> = {
 export type AmmProps<T extends AmmData<C extends IBData<I> ? C : IBData<I>>, I, ACD, C = IBData<I>> =
     AmmPanelBaseProps<T, I, ACD, C>
     & {
-    handleAmmAddChangeEvent: (data: T, focusOn: 'coinA' | 'coinB') => void,
-    handleAmmRemoveChangeEvent: (data: T, focusOn: 'coinA' | 'coinB') => void,
-    onChangeEvent?: (data: AmmChgData<T>) => AmmChgData<T>,
-    refreshRef:React.Ref<any>,
-    onRefreshData?:()=>void,
-}
+        handleAmmAddChangeEvent: (data: T, focusOn: 'coinA' | 'coinB') => void,
+        handleAmmRemoveChangeEvent: (data: T, focusOn: 'coinA' | 'coinB') => void,
+        onChangeEvent?: (data: AmmChgData<T>) => AmmChgData<T>,
+        refreshRef: React.Ref<any>,
+        onRefreshData?: () => void,
+    }
     & AmmWithdrawBaseProps<T, I>
     & AmmDepositBaseProps<T, I>
 
