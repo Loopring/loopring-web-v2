daf525106c286677dd9f92ed5af770a05fb65903
leo chan <leochan007@163.com>
2021-08-30 03:00:19 +0800

    remove fee when exit.

--- packages/webapp/src/hooks/help/ammPairDetail.ts
+++ packages/webapp/src/hooks/help/ammPairDetail.ts
@@ -1,10 +1,7 @@
 import { AmmDetailBase } from '@loopring-web/common-resources';
-import { AmmPanelType } from '@loopring-web/component-lib';
-import { myLog } from 'utils/log_tools';
 import { volumeToCountAsBigNumber } from './volumeToCount';
 
 export const ammPairInit = ({
-    type,
     fee,
     pair,
     _ammCalcData,
@@ -26,42 +23,33 @@ export const ammPairInit = ({
     if (pair.coinAInfo) {
         const feeReal = fee ? fee : 0
 
-        // myLog('........feeReal:', feeReal)
+        _ammCalcData.myCoinA = {
+            belong: pair.coinAInfo.simpleName,
+            balance: walletMap ? walletMap[pair.coinAInfo.simpleName]?.count : 0,
+        }
+        _ammCalcData.myCoinB = {
+            belong: pair.coinBInfo.simpleName,
+            balance: walletMap ? walletMap[pair.coinBInfo.simpleName]?.count - feeReal : 0,
+        }
 
-        switch (type) {
-            case AmmPanelType.Join:
-                _ammCalcData.myCoinA = {
-                    belong: pair.coinAInfo.simpleName,
-                    balance: walletMap ? walletMap[pair.coinAInfo.simpleName]?.count : 0,
-                }
-                _ammCalcData.myCoinB = {
-                    belong: pair.coinBInfo.simpleName,
-                    balance: walletMap ? walletMap[pair.coinBInfo.simpleName]?.count - feeReal: 0,
-                }
-                break
-            case AmmPanelType.Exit:
-                const key = `${pair.coinAInfo.simpleName}-${pair.coinBInfo.simpleName}`;
-                let coinACount = 0, coinBCount = 0;
-                if (walletMap) {
-                    const balance = walletMap ? walletMap['LP-' + key]?.count : 0;
-                    const { totalLPToken, totalA, totalB }: AmmDetailBase<any> = ammMap['AMM-' + key];
-                    if (totalA && totalLPToken && totalB) {
-                        coinACount = totalA / totalLPToken * balance;
-                        coinBCount = totalB / totalLPToken * balance - feeReal;
-                    }
-                    _ammCalcData.lpCoin = balance;
-                }
-                _ammCalcData.lpCoinA = {
-                    belong: pair.coinAInfo.simpleName,
-                    balance: coinACount,
-                }
-                _ammCalcData.lpCoinB = {
-                    belong: pair.coinBInfo.simpleName,
-                    balance: coinBCount,
-                }
-                break
-            default:
-                break
+        const key = `${pair.coinAInfo.simpleName}-${pair.coinBInfo.simpleName}`;
+        let coinACount = 0, coinBCount = 0;
+        if (walletMap) {
+            const balance = walletMap ? walletMap['LP-' + key]?.count : 0;
+            const { totalLPToken, totalA, totalB }: AmmDetailBase<any> = ammMap['AMM-' + key];
+            if (totalA && totalLPToken && totalB) {
+                coinACount = totalA / totalLPToken * balance;
+                coinBCount = totalB / totalLPToken * balance - feeReal;
+            }
+            _ammCalcData.lpCoin = balance;
+        }
+        _ammCalcData.lpCoinA = {
+            belong: pair.coinAInfo.simpleName,
+            balance: coinACount,
+        }
+        _ammCalcData.lpCoinB = {
+            belong: pair.coinBInfo.simpleName,
+            balance: coinBCount,
         }
     }
 
