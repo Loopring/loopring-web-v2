1d5cb6cf04c834c64be014bf1a883f591f404c0b
leo chan <leochan007@163.com>
2021-09-01 22:54:21 +0800

    update sdk fix.

--- packages/webapp/src/pages/SwapPage/hookSwap.ts
+++ packages/webapp/src/pages/SwapPage/hookSwap.ts
@@ -623,8 +623,8 @@ export const useSwap = <C extends { [ key: string ]: any }>() => {
             }
             const calcTradeParams = sdk.getOutputAmount({
                 input: input.toString(),
-                base: coinA,
-                quote: coinB,
+                sell: coinA,
+                buy: coinB,
                 isAtoB,
                 marketArr: marketArray as string[],
                 tokenMap: tokenMap as any,
