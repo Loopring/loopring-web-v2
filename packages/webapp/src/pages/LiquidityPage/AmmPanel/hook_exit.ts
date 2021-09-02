928c1b32e592a8e1c959e40dfc790b2b8abd32e0
leo chan <leochan007@163.com>
2021-09-02 14:18:18 +0800

    sep amm hook

--- packages/webapp/src/pages/LiquidityPage/AmmPanel/hook_exit.ts
+++ packages/webapp/src/pages/LiquidityPage/AmmPanel/hook_exit.ts
@@ -26,7 +26,6 @@ import {
     ChainId,
     dumpError400,
     ExitAmmPoolRequest,
-    GetAmmPoolSnapshotRequest,
     getExistedMarket,
     GetNextStorageIdRequest,
     GetOffchainFeeAmtRequest,
@@ -50,109 +49,10 @@ import { myLog } from "utils/log_tools";
 import { useTranslation } from "react-i18next";
 
 import { useWalletLayer2Socket, walletLayer2Service } from 'services/socket';
-import { useSocket, } from "stores/socket";
-import { useToast } from "hooks/common/useToast";
-
-export const useAmmCommon = ({pair, snapShotData,}: {
-    pair: {
-        coinAInfo: CoinInfo<string> | undefined,
-        coinBInfo: CoinInfo<string> | undefined
-    },
-    snapShotData: any,
-}) => {
-
-    const {toastOpen, setToastOpen, closeToast,} = useToast()
-
-    const {sendSocketTopic, socketEnd} = useSocket()
-
-    const {account} = useAccount()
-
-    const [ammPoolSnapshot, setAmmPoolSnapShot] = React.useState<AmmPoolSnapshot>()
-
-    const {marketArray, marketMap,} = useTokenMap();
-    const {ammMap} = useAmmMap();
-
-    const updateAmmPoolSnapshot = React.useCallback(async () => {
-
-        if (!pair?.coinAInfo?.simpleName || !pair?.coinBInfo?.simpleName || !LoopringAPI.ammpoolAPI) {
-            return
-        }
-
-        const {market, amm} = getExistedMarket(marketArray, pair.coinAInfo.simpleName as string,
-            pair.coinBInfo.simpleName as string)
-
-        if (!market || !amm || !marketMap || !ammMap || !ammMap[ amm as string ]) {
-            return
-        }
-
-        const ammInfo: any = ammMap[ amm as string ]
-
-        const request1: GetAmmPoolSnapshotRequest = {
-            poolAddress: ammInfo.address
-        }
-
-        const response = await LoopringAPI.ammpoolAPI.getAmmPoolSnapshot(request1)
-
-        if (!response) {
-            myLog('err res:', response)
-            return
-        }
-
-        const {ammPoolSnapshot} = response
-
-        setAmmPoolSnapShot(ammPoolSnapshot)
-
-    }, [pair, marketArray, ammMap, setAmmPoolSnapShot])
-
-    React.useEffect(() => {
-        if (account.readyState === AccountStatus.ACTIVATED) {
-            sendSocketTopic({[ WsTopicType.account ]: true});
-        } else {
-            socketEnd()
-        }
-        return () => {
-            socketEnd()
-        }
-    }, [account.readyState]);
-
-    const refreshRef = React.createRef()
-
-    React.useEffect(() => {
-        if (refreshRef.current && pair) {
-            // @ts-ignore
-            refreshRef.current.firstElementChild.click();
-        }
-
-    }, []);
-
-    const walletLayer2Callback = React.useCallback(() => {
-
-        if (snapShotData) {
-            setAmmPoolSnapShot(snapShotData.ammPoolSnapshot)
-        }
-
-    }, [snapShotData, setAmmPoolSnapShot])
-
-    React.useEffect(() => {
-        walletLayer2Callback()
-    }, [snapShotData])
-
-    return {
-        toastOpen,
-        setToastOpen,
-        closeToast,
-        refreshRef,
-        ammPoolSnapshot,
-        updateAmmPoolSnapshot,
-    }
-
-}
-
-// ----------calc hook -------
 
 const initSlippage = 0.5
 
-export const useAmmCalc = <C extends { [ key: string ]: any }>({
+export const useAmmExit = <C extends { [ key: string ]: any }>({
                                                                    setToastOpen,
                                                                    type,
                                                                    pair,
