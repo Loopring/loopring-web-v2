04d3e20ba7233a89a7f59b4016f0daa41ec693fb
leo chan <leochan007@163.com>
2021-09-05 23:57:05 +0800

    refine codes

--- packages/webapp/src/utils/formatter_tool.ts
+++ packages/webapp/src/utils/formatter_tool.ts
@@ -1,25 +1,28 @@
 import store from 'stores'
 
 import * as sdk from 'loopring-sdk'
+import { Side, toBig } from 'loopring-sdk'
+import { getShowStr, TradeTypes } from '@loopring-web/common-resources'
+import { volumeToCountAsBigNumber } from 'hooks/help'
 
 const getTokenInfo = (symbol: string) => {
     const tokenMap = store.getState().tokenMap.tokenMap
 
-    if (!tokenMap || !tokenMap[ symbol ]) {
+    if (!tokenMap || !tokenMap[symbol]) {
         return undefined
     }
 
-    return tokenMap[ symbol ]
+    return tokenMap[symbol]
 }
 
 const getMarketInfo = (symbol: string) => {
     const marketMap = store.getState().tokenMap.marketMap
 
-    if (!marketMap || !marketMap[ symbol ]) {
+    if (!marketMap || !marketMap[symbol]) {
         return undefined
     }
 
-    return marketMap[ symbol ]
+    return marketMap[symbol]
 }
 
 export function StringToNumberWithPrecision(rawVal: string, symbol: string) {
@@ -74,7 +77,7 @@ export function FormatValWithPrecision(rawVal: string, symbol: string) {
 * format order price with precision
 */
 export function formatPriceWithPrecision(rawVal: string,
-                                         symbol: string) {
+    symbol: string) {
 
     const marketInfo = getMarketInfo(symbol)
     if (!rawVal || !marketInfo || !symbol) {
@@ -83,4 +86,46 @@ export function formatPriceWithPrecision(rawVal: string,
 
     return sdk.toBig(rawVal).toFixed(marketInfo.precisionForPrice)
 
+}
+
+export function tradeItemToTableDataItem(tradeItem: any) {
+    const marketList = tradeItem.market.split('-')
+    // due to AMM case, we cannot use first index
+    const side = tradeItem.side === Side.Buy ? TradeTypes.Buy : TradeTypes.Sell
+    const isBuy = side === TradeTypes.Buy
+
+    const base = marketList[marketList.length - 2]
+    const quote = marketList[marketList.length - 1]
+    const baseValue = volumeToCountAsBigNumber(base, tradeItem.volume)
+    const quoteValue = baseValue?.times(tradeItem.price)
+    const sellToken = isBuy ? quote : base
+    const buyToken = isBuy ? base : quote
+    const sellValue = getShowStr((isBuy ? quoteValue : baseValue)?.toNumber())
+    const buyValue = getShowStr((isBuy ? baseValue : quoteValue)?.toNumber())
+
+    const feeKey = buyToken
+    const feeValue = getShowStr(volumeToCountAsBigNumber(feeKey, tradeItem.fee)?.toString())
+
+    return ({
+        side,
+        price: {
+            key: sellToken,
+            value: getShowStr(toBig(tradeItem.price).toString()),
+        },
+        fee: {
+            key: feeKey,
+            value: feeKey && feeValue ? feeValue : '--'
+        },
+        time: Number(tradeItem.tradeTime),
+        amount: {
+            from: {
+                key: sellToken,
+                value: sellToken ? sellValue : undefined
+            },
+            to: {
+                key: buyToken,
+                value: buyValue ? buyValue : undefined
+            }
+        }
+    })
 }
