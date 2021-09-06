d9ec5641a2bee8f03f88c30b02bfb17df1f16b75
leo chan <leochan007@163.com>
2021-09-06 17:17:59 +0800

    update amm redux.

--- packages/webapp/src/stores/router/ammPage/hook.ts
+++ packages/webapp/src/stores/router/ammPage/hook.ts
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { updatePageAmmExit, updatePageAmmJoin, } from './reducer';
-import { PageAmmExit, PageAmmJoin, PageAmmPoolStatus } from './interface';
+import { updatePageAmmExit, updatePageAmmJoin, updatePageAmmCommon, } from './reducer';
+import { PageAmmExit, PageAmmJoin, PageAmmCommon, PageAmmPoolStatus } from './interface';
 import React from 'react';
 import { RequireOne } from '@loopring-web/common-resources';
 import { RootState } from 'stores';
@@ -8,11 +8,15 @@ import { RootState } from 'stores';
 export function usePageAmmPool(): PageAmmPoolStatus & {
     updatePageAmmJoin: (pageAmmPool: RequireOne<PageAmmJoin, never>) => void,
     updatePageAmmExit: (pageAmmPool: RequireOne<PageAmmExit, never>) => void,
+    updatePageAmmCommon: (pageAmmPool: RequireOne<PageAmmCommon, never>) => void,
 } {
     const pageAmmPoolStatus: PageAmmPoolStatus = useSelector((state: RootState) => state._router_pageAmmPool)
     const dispatch = useDispatch()
     return {
         ...pageAmmPoolStatus,
+        updatePageAmmCommon: React.useCallback((pageAmmJoin: RequireOne<PageAmmCommon, never>) => {
+            dispatch(updatePageAmmCommon(pageAmmJoin))
+        }, [dispatch]),
         updatePageAmmJoin: React.useCallback((pageAmmJoin: RequireOne<PageAmmJoin, never>) => {
             dispatch(updatePageAmmJoin(pageAmmJoin))
         }, [dispatch]),
