599db3bf88006ef92eb9e8fed94f0ff4f4426059
leo chan <leochan007@163.com>
2021-09-06 11:29:27 +0800

    add timeout check

--- packages/webapp/src/App.tsx
+++ packages/webapp/src/App.tsx
@@ -24,19 +24,6 @@ const App = () => {
     }, [])
 
     const {state} = useInit();
-    // const [status, setStatus] = React.useState<keyof typeof SagaStatus>('PENDING');
-    // check all status be
-    //TODO  demo if  tokenMapStatus is unset and tokenMap is empty. show error
-    //TODO tokenMapObj.status is pending, show global loading
-    //console.log(tokenMapObj.tokenMap && Object.keys(tokenMapObj.tokenMap).length>0,tokenMapObj.status, tokenMapObj.errorMessage)
-
-    // useEffect(() => {
-    //     if (state === SagaStatus.PENDING || state === SagaStatus.ERROR) {
-    //         setStatus(state)
-    //     } else {
-    //         setStatus('DONE')
-    //     }
-    // }, [state, setStatus])
 
 
     return <><GlobalStyles styles={css`
@@ -60,15 +47,10 @@ const App = () => {
       //}
     }`}></GlobalStyles>
 
-        {/*<Web3ReactManager>*/}
         {state === 'PENDING' ?
             <LoadingPage/>
-            // <ErrorPage {...ErrorMap.LOADING_WHOLE_SITE}/>
-            // <Avatar src={loadingSvg}/>
             : state === 'ERROR' ? <ErrorPage {...ErrorMap.NO_NETWORK_ERROR} /> : <>
                 <RouterView/>
-
-                {/*    <ErrorPage {...ErrorMap.LOADING_WHOLE_SITE}/>*/}
             </>}
     </>
 
