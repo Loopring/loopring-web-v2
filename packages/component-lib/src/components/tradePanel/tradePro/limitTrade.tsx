015acda7df029847b841933dd5ad4a0c0e3e4a37
windatang <windatang@gmail.com>
2021-09-16 18:09:14 +0800

    orderbook init

--- packages/component-lib/src/components/tradePanel/tradePro/limitTrade.tsx
+++ packages/component-lib/src/components/tradePanel/tradePro/limitTrade.tsx
@@ -1,12 +1,11 @@
 import { WithTranslation, withTranslation } from 'react-i18next';
-import { InputButton, InputCoin } from '../../basic-lib';
-import { LimitTradeData, TradeLimitProps } from '../Interface';
-import { CoinInfo, CoinKey, CoinMap, ExchangeIcon, IBData, TradeCalcData } from '@loopring-web/common-resources';
+import {  InputCoin } from '../../basic-lib';
+import { LimitTradeData, TradeBtnStatus, TradeLimitProps } from '../Interface';
+import { CoinInfo, CoinKey, CoinMap, IBData, TradeCalcData } from '@loopring-web/common-resources';
 import { Box, Tab, Tabs } from '@mui/material';
-import { TabContext } from '@mui/lab';
 import { TradeProType } from './Interface';
-import React from 'react';
-import { IconButtonStyled } from '../components/Styled';
+import { ButtonStyle } from '../components/Styled';
+import { useCommon } from './hookCommon';
 
 // const tradeDataInit:LimitTradeData<IBData<any>> = {
 //     sell: {belong:'',tradeValue:undefined,balance} as IBData<any>,
@@ -14,76 +13,90 @@ import { IconButtonStyled } from '../components/Styled';
 //     slippage: 0.5,
 // }
 
-export const limitTrade = withTranslation('common', {withRef: true})(<L extends LimitTradeData<T>,
+export const LimitTrade = withTranslation('common', {withRef: true})(<L extends LimitTradeData<T>,
     T extends IBData<I>,
     TCD extends TradeCalcData<I>, I = CoinKey<any>>(
-    {
+    {tradeData = {} as L,...props}:TradeLimitProps<L,T,TCD, I> & WithTranslation
+) => {
+    const  {
         t,
         // disabled,
-        // tradeLimitI18nKey,
+        tradeLimitI18nKey,
         // tradeCalcData,
-        // TradeLimitBtnStatus,
-        // tokenPriceProps,
+        tradeLimitBtnStatus,
         // tokenBuyProps,
         // tokenSellProps,
-        // tradeData = {},
+        // tradeData,
+        // handleError,
         // handleSubmitEvent,
-        // onChangeEvent,
         // handleChangeIndex,
-    }: TradeLimitProps<L,T,TCD, I> & WithTranslation
-) => {
-    // const _handleChangeIndex = React.useCallback((index:TradeProType)=>{
-    //     if(handleChangeIndex){
-    //         tradeData =  handleChangeIndex(index)
-    //     }else{
-    //         tradeData.type = index
-    //     }
-    //     onChangeEvent(tradeData)
-    // },[tradeData])
-    // const _onChangeEvent = React.useCallback((_tradeData)=>{
-    //     if(onChangeEvent) {
-    //         onChangeEvent({
-    //            ...tradeData,
-    //             _tradeData,
-    //         })
-    //     }
-    // },[tradeData])
-    return <Box flex={1} display={'flex'} flexDirection={'row'} alignItems={'stretch'}>
-           {/*<Box className={'tool-bar'} paddingTop={2} paddingX={2} display={'flex'}  alignItems={'stretch'}>*/}
-           {/*    <Tabs value={tradeData?.type??TradeProType.buy}*/}
-           {/*          onChange={(e, index) => _handleChangeIndex(index)} className={'pro-tabs'}  >*/}
-           {/*      <Tab value={TradeProType.buy} label={t('labelProBuy')}/>*/}
+        // onChangeEvent,
+        // ...rest
+    } =  props
+    const {
+        sellRef,
+        buyRef,
+        btnLabel,
+        getDisabled,
+        _handleChangeIndex,
+        inputError,
+        // i18nKey : tradeLimitI18nKey,
+        tradeCalcData,
+        tradeBtnBaseStatus,
+        propsBuy,
+        propsSell,
+    } = useCommon({
+        ...props as any,
+        tradeData,
+        i18nKey: tradeLimitI18nKey?tradeLimitI18nKey:'labelTrade',
+        tradeBtnBaseStatus: tradeLimitBtnStatus
+    })
+    return <Box flex={1} display={'flex'} flexDirection={'column'} alignItems={'stretch'}>
+        <Box className={'tool-bar'} paddingTop={2} paddingX={2} display={'flex'} alignItems={'stretch'}>
+            <Tabs value={tradeData?.type ?? TradeProType.buy}
+                  onChange={(_e, index) => _handleChangeIndex(index)} className={'pro-tabs'}>
+                <Tab value={TradeProType.buy} label={t('labelProBuy')}/>
 
-           {/*      <Tab value={TradeProType.sell} label={t('labelProSell')}/>*/}
-           {/*    </Tabs>*/}
-           {/*</Box>*/}
-        <Box className={'trade-panel'} >
+                <Tab value={TradeProType.sell} label={t('labelProSell')}/>
+            </Tabs>
+        </Box>
+        <Box className={'trade-panel'} paddingX={2} paddingTop={2}>
 
-            {/*<InputCoin<any, I, CoinInfo<I>> ref={buyRef} disabled={getDisabled()} {...{*/}
-            {/*    ...propsBuy,*/}
-            {/*    // maxAllow:false,*/}
-            {/*    isHideError: true,*/}
-            {/*    inputData: tradeData ? tradeData.buy : {} as any,*/}
-            {/*    coinMap: tradeCalcData && tradeCalcData.coinInfoMap ? tradeCalcData.coinInfoMap : {} as CoinMap<I, CoinInfo<I>>*/}
-            {/*}} />*/}
-            {/*/!*</Grid>*!/*/}
-            {/*/!*<Grid item>*!/*/}
-            {/*<Box alignSelf={"center"} marginY={1}>*/}
+            <InputCoin<any, I, CoinInfo<I>> ref={buyRef as any} disabled={getDisabled()} {...{
+                ...propsBuy,
+                // maxAllow:false,
+                isHideError: true,
+                inputData: tradeData ? tradeData.buy : {} as any,
+                coinMap: tradeCalcData && tradeCalcData.coinInfoMap ? tradeCalcData.coinInfoMap : {} as CoinMap<I, CoinInfo<I>>
+            }} />
+            {/*</Grid>*/}
+            {/*<Grid item>*/}
+            <Box alignSelf={"center"} marginY={1}>
 
-            {/*</Box>*/}
-            {/*<InputButton<any, I, CoinInfo<I>> ref={sellRef} disabled={getDisabled()}  {...{*/}
-            {/*    ...propsSell,*/}
-            {/*    isHideError: true,*/}
-            {/*    inputData: tradeData ? tradeData.sell : {} as any,*/}
-            {/*    coinMap: tradeCalcData && tradeCalcData.coinInfoMap ? tradeCalcData.coinInfoMap : {} as CoinMap<I, CoinInfo<I>>*/}
-            {/*}} />*/}
+            </Box>
+            <InputCoin<any, I, CoinInfo<I>> ref={sellRef} disabled={getDisabled()}  {...{
+                ...propsSell,
+                isHideError: true,
+                inputData: tradeData ? tradeData.sell : {} as any,
+                coinMap: tradeCalcData && tradeCalcData.coinInfoMap ? tradeCalcData.coinInfoMap : {} as CoinMap<I, CoinInfo<I>>
+            }} />
             {/*</Grid>*/}
             {/*<Grid item>*/}
 
             {/*< label={tradeCalcData.buyToken} coinMap={tradeCalcData.coinMap} />*/}
         </Box>
+        <Box paddingX={2} paddingTop={2}>
+            <ButtonStyle variant={'contained'} size={'large'} color={'primary'} onClick={() => {
+                // onSwapClick(swapData.tradeData)
+            }}
+                         loading={!getDisabled() && tradeBtnBaseStatus === TradeBtnStatus.LOADING ? 'true' : 'false'}
+                         disabled={getDisabled() || tradeBtnBaseStatus === TradeBtnStatus.DISABLED || tradeBtnBaseStatus === TradeBtnStatus.LOADING || inputError.error}
+                         fullWidth={true}>{btnLabel}
+            </ButtonStyle>
+        </Box>
     </Box>
-    // <InputCoin/>
-})
+}) as <L extends LimitTradeData<T>,
+    T extends IBData<I>,
+    TCD extends TradeCalcData<I>, I = CoinKey<any>>(props: TradeLimitProps<L,T,TCD,I>) => JSX.Element
 
 
--- packages/webapp/src/pages/ProTradePage/panel/spot/hookLimit.ts
+++ packages/webapp/src/pages/ProTradePage/panel/spot/hookLimit.ts
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useToast } from 'hooks/common/useToast';
-import { MarketType } from '@loopring-web/common-resources';
+import { IBData, MarketType } from '@loopring-web/common-resources';
+import { LimitTradeData, MarketTradeData, TradeBaseType, TradeProType } from '@loopring-web/component-lib';
 
 
 export const useLimit = <C extends { [ key: string ]: any }>(market:MarketType):{
@@ -8,14 +9,32 @@ export const useLimit = <C extends { [ key: string ]: any }>(market:MarketType):
     // market: MarketType|undefined;
     // marketTicker: MarketBlockProps<C> |undefined,
 } =>{
-
+    const limitTradeData:LimitTradeData<IBData<any>> = {
+        sell: {belong: 'ETH'} as IBData<any>,
+        buy: {belong: 'LRC'} as IBData<any>,
+        price: {count: 0},
+        type: TradeProType.sell
+    }
     const {toastOpen, setToastOpen, closeToast} = useToast();
+    const limitSubmit = ()=> {
+        return
+    }
+    const onChangeLimitEvent  = async (tradeData: LimitTradeData<IBData<any>>, formType: TradeBaseType): Promise<void> => {
 
+        // myLog('handleSwapPanelEvent...')
+
+        // const {tradeData} = swapData
+        // resetSwap(swapType, tradeData)
+
+    }
     return {
         // alertOpen,
         // confirmOpen,
         toastOpen,
-        closeToast
+        closeToast,
+        limitSubmit,
+        limitTradeData,
+        onChangeLimitEvent
         // marketTicker,
     }
 }
