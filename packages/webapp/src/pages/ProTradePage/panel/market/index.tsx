974cee70f396d4650202a2e5e5620e73cbbb655d
windatang <windatang@me.com>
2021-09-17 23:23:12 +0800

    Amm data

--- packages/webapp/src/pages/ProTradePage/panel/market/index.tsx
+++ packages/webapp/src/pages/ProTradePage/panel/market/index.tsx
@@ -1,10 +1,19 @@
 import { WithTranslation, withTranslation } from 'react-i18next';
-import { DepthBlock } from '@loopring-web/component-lib';
-import { LoadingIcon, MarketType } from '@loopring-web/common-resources';
-import { Box, Tab, Tabs } from '@mui/material';
+import { DepthBlock, DepthType, useSettings } from '@loopring-web/component-lib';
+import {
+    Currency,
+    EmptyValueTag,
+    getValuePrecisionThousand,
+    LoadingIcon,
+    MarketType,
+    PriceTag
+} from '@loopring-web/common-resources';
+import { Box, Tab, Tabs, Typography } from '@mui/material';
 import React from 'react';
 import { usePageTradePro } from 'stores/router';
 import { useTokenMap } from 'stores/token';
+import { useTokenPrices } from '../../../../stores/tokenPrices';
+import { useSystem } from '../../../../stores/system';
 
 enum TabIndex {
     orderbook = 'orderbook',
@@ -17,9 +26,32 @@ export  const MarketView = withTranslation('common')(({
     market: MarketType,
     // marketTicker:  MarketBlockProps<C>
 } & WithTranslation)=>{
+    // @ts-ignore
+    const [, baseSymbol, quoteSymbol] = market.match(/(\w+)-(\w+)/i);
     const [tabIndex, setTabIndex] = React.useState<TabIndex>(TabIndex.orderbook)
     const {pageTradePro} = usePageTradePro();
-    const { marketMap }  = useTokenMap()
+
+    const {tickMap,depth} = pageTradePro;
+    const { marketMap,tokenMap }  = useTokenMap();
+    const {upColor, currency } = useSettings();
+    const {tokenPrices} = useTokenPrices();
+    // @ts-ignore
+    const basePrice = tokenPrices[baseSymbol];
+    const {forex} = useSystem();
+
+
+    // const                          {pageTradePro.tickMap[market].close + '\u2248' + pageTradePro.tickMap[market].priceDollar }
+    // `1${tradeData.sell?.belong} \u2248 ${tradeCalcData?.StoB ? tradeCalcData.StoB : EmptyValueTag} ${tradeData.buy?.belong}`
+
+    // if (tickMap) {
+    const value = currency === Currency.dollar ? '\u2248 ' + PriceTag.Dollar
+        + getValuePrecisionThousand((
+            (tickMap && tickMap[ market ])? tickMap[ market ].close * basePrice
+                : 0), undefined, undefined, undefined, true, {isFait: true})
+        : '\u2248 ' + PriceTag.Yuan
+        + getValuePrecisionThousand(( (tickMap && tickMap[ market ])?
+            tickMap[ market ].close * basePrice / forex: 0), undefined, undefined, undefined, true, {isFait: true})
+     // }
     return <>
         <Box display={'flex'} flexDirection={'column'} alignItems={'stretch'}>
             <Box component={'header'} width={'100%'}>
@@ -30,15 +62,26 @@ export  const MarketView = withTranslation('common')(({
                     {/*<Tab value={TabIndex.market} label={t('labelProMarket')}/>*/}
                 </Tabs>
             </Box>
-            {pageTradePro.depth && marketMap && market == pageTradePro.market ?<>
+            {pageTradePro.depth && pageTradePro.tickMap && tokenMap && marketMap && market == pageTradePro.market ?
+                <Box display={'flex'} flexDirection={'column'} alignItems={'stretch'} paddingX={2}>
                     <DepthBlock marketInfo={marketMap[market]}
-                                depths={pageTradePro.depth.asks}
+                                type={DepthType.ask}
+                                tokenBaseInfo={tokenMap[baseSymbol]}
+                                tokenQuoteInfo={tokenMap[quoteSymbol]}
+                                depths={pageTradePro.depth[ `${DepthType.ask}s`].slice(0,8)}
                                 showTitle ={true}  />
-                    <></>
+                    <Box paddingY={1/2} display={'flex'} flexDirection={'column'}  alignItems={'center'}>
+                         <Typography color={'var(--color-text-third)'} variant={'body2'} component={'p'} textAlign={'center'} >
+                             {pageTradePro.tickMap[market].close} {value}
+                         </Typography>
+                    </Box>
                     <DepthBlock marketInfo={marketMap[market]}
-                                depths={pageTradePro.depth.bids}
-                                showTitle ={true}  />
-                </>
+                                type={DepthType.bid}
+                                tokenBaseInfo={tokenMap[baseSymbol]}
+                                tokenQuoteInfo={tokenMap[quoteSymbol]}
+                                depths={pageTradePro.depth[ `${DepthType.bid}s`].slice(0,8)}
+                                showTitle ={false}  />
+                </Box>
                 :<LoadingIcon/>
             }
 
