2ff5e7a9c2424eeb1a0d65c2d3cd56343f1538dc
leo chan <leochan007@163.com>
2021-09-19 05:03:52 +0800

    fix mining url cn bug.

--- packages/common-resources/static-resources/src/constant/miningOuterLinks.ts
+++ packages/common-resources/static-resources/src/constant/miningOuterLinks.ts
@@ -1,4 +1,7 @@
-export const getMiningLinkList = (lan: 'cn' | 'en') => {
+export const getMiningLinkList = (lan: string) => {
+    if (lan === 'cn') {
+        lan = 'zh'
+    }
     return {
         'BCDT-ETH': `https://loopring.pro/#/embed/amm_mining_14_${lan}`,
         'USDC-USDT': `https://loopring.pro/#/embed/orderbook_mining_24_${lan}`,
