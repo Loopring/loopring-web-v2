3b75651bce30e6d7bbd2f6f566463c9959547733
windatang <windatang@gmail.com>
2021-09-29 20:33:32 +0800

    percentage step 1

--- packages/webapp/src/stores/socket/hook.ts
+++ packages/webapp/src/stores/socket/hook.ts
@@ -6,6 +6,7 @@ import { StateBase } from '@loopring-web/common-resources';
 
 export function useSocket(): StateBase & {
     // socketStart:()=>void,
+    socket: SocketMap,
     statusUnset: () => void,
     sendSocketTopic: (socket: SocketMap) => void,
     socketEnd: () => void
