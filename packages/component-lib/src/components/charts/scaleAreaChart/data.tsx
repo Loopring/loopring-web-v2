6f722c1c35975930bb67e1c7f8d6a0f2539fe875
SylvanasGone <397009765@qq.com>
2021-10-11 03:07:40 +0800

    perf: add fake zero data for depth chart

--- packages/component-lib/src/components/charts/scaleAreaChart/data.tsx
+++ packages/component-lib/src/components/charts/scaleAreaChart/data.tsx
@@ -109,15 +109,30 @@ export const getDepthData = (data?: IGetDepthDataParams | any) => {
     const jointPrices = formattedBidsPrices.concat(formattedAsksPrices)
     const jointAmtTotals = bidsAmtTotals.concat(asksAmtTotals)
 
-    return jointAmtTotals.map((amount: number, index: number) =>
+    const fakeData = [{
+        price: formattedBidsPrices[formattedBidsPrices.length - 1]?.price,
+        bids: 0
+    }, {
+        price: formattedAsksPrices[0]?.price,
+        asks: 0
+    }]
+
+    const rawAmtTotals = jointAmtTotals.map((amount: number, index: number) =>
         jointPrices[ index ].type === 'bids'
             ? {
                 price: jointPrices[ index ].price,
                 bids: amount,
             }
             : {
-                price: jointPrices[ index ].price,
+                price: jointPrices[ index ].price ,
                 asks: amount,
             }
     )
+
+    const filteredBidsList = rawAmtTotals.filter((o: any) => o.bids)
+    const filteredAsksList = rawAmtTotals.filter((o: any) => o.asks)
+
+    const newData = [...filteredBidsList, ...fakeData, ...filteredAsksList]
+
+    return newData
 }
