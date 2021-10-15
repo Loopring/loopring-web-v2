f9cd9c3d167dbc89288644c2aa45432dca6206c6
windatang <windatang@gmail.com>
2021-10-15 18:10:37 +0800

    Merge remote-tracking branch 'origin/alpha-v2' into alpha-v2

--- packages/webapp/src/stores/localStore/onchainHashInfo/interface.ts
+++ packages/webapp/src/stores/localStore/onchainHashInfo/interface.ts
@@ -1,8 +1,10 @@
 export interface TxInfo {
     hash: string
 }
-
-export interface OnchainHashInfo {
+export interface accountHashInfo {
     depositHashes: { [ key: string ]: TxInfo }
     withdrawHashes: { [ key: string ]: TxInfo }
+}
+export interface OnchainHashInfo {
+    [key:string]: accountHashInfo
 }
--- packages/webapp/src/stores/localStore/onchainHashInfo/reducer.ts
+++ packages/webapp/src/stores/localStore/onchainHashInfo/reducer.ts
@@ -2,10 +2,7 @@ import { createSlice, PayloadAction, Slice } from '@reduxjs/toolkit'
 import { SliceCaseReducers } from '@reduxjs/toolkit/src/createSlice'
 import { OnchainHashInfo, TxInfo, } from './interface'
 
-const initialState: OnchainHashInfo = {
-    depositHashes: {},
-    withdrawHashes: {},
-}
+const initialState: OnchainHashInfo = {}
 
 const onchainHashInfoSlice: Slice<OnchainHashInfo> = createSlice<OnchainHashInfo, SliceCaseReducers<OnchainHashInfo>, 'onchainHashInfo'>({
     name: 'onchainHashInfo',
@@ -15,10 +12,10 @@ const onchainHashInfoSlice: Slice<OnchainHashInfo> = createSlice<OnchainHashInfo
             state = initialState
         },
         clearDepositHash(state: OnchainHashInfo) {
-            state.depositHashes = {}
+            // state.depositHashes = {}
         },
         clearWithdrawHash(state: OnchainHashInfo) {
-            state.withdrawHashes = {}
+            // state.withdrawHashes = {}
         },
         updateDepositHash(state: OnchainHashInfo, action: PayloadAction<TxInfo>) {
             const txInfo = action.payload
