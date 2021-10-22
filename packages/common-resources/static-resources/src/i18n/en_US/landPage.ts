7aca97b17cd4c6efc739db04f66e1d30639bd8cf
SylvanasGone <397009765@qq.com>
2021-10-22 12:03:33 +0800

    update landing page developer docs

--- packages/common-resources/static-resources/src/i18n/en_US/landPage.ts
+++ packages/common-resources/static-resources/src/i18n/en_US/landPage.ts
@@ -16,5 +16,6 @@ export default {
     describeLowCost:'The cost of a transaction on Layer2 is \n less than 0.1% what you would pay on Ethereum L1. For the first time, \n market makers and professional trading \n firms can be free from the unfair and \n prohibitive restrictions of centralised trading platforms.',
     describeFastTransfer:'2,025 transactions per second.\n Now, decentralized exchanges can be \n a serious business model that can compete with centralized counterparts.',
     describeSuperpowers :'Build DApps and tools on one of the largest crypto projects on Ethereum. Get started with quick-start guides, protocol documentation, a Javascript SDK, and fully open source code.',
-    labelBtnStart:'Start Layer2 app'
+    labelBtnStart:'Start Layer2 app',
+    labelBtnDeveloper: 'Developer Documentation',
 }
