d241b778f30b8917192a09c4884734e8a0f94f2c
SylvanasGone <397009765@qq.com>
2021-11-15 17:04:42 +0800

    update wallet connect server

--- packages/web3-provider/src/walletConnect/provider.ts
+++ packages/web3-provider/src/walletConnect/provider.ts
@@ -17,7 +17,7 @@ const POLLING_INTERVAL = 12000
 
 export const WalletConnectProvide = async (account?: string): Promise<{ provider?: WalletConnectProvider, web3?: Web3, } | undefined> => {
     try {
-        const BRIDGE_URL = await (fetch('https://wcbridge.loopring.network/hello').then(({status}) => {
+        const BRIDGE_URL = await (fetch('https://connect.loopring.io/hello').then(({status}) => {
             return status === 200 ? process.env.REACT_APP_WALLET_CONNECT_BRIDGE : 'https://bridge.walletconnect.org'
         }).catch(() => {
             return 'https://bridge.walletconnect.org';
