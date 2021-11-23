82488e10f0387103bfbef181b9dca27255899a5a
SylvanasGone <397009765@qq.com>
2021-11-23 21:51:24 +0800

    update bridge url

--- packages/web3-provider/src/walletConnect/provider.ts
+++ packages/web3-provider/src/walletConnect/provider.ts
@@ -18,13 +18,11 @@ const POLLING_INTERVAL = 12000
 
 export const WalletConnectProvide = async (account?: string): Promise<{ provider?: WalletConnectProvider, web3?: Web3, } | undefined> => {
     try {
-        // const BRIDGE_URL = await (fetch('https://connect.loopring.io/hello').then(({status}) => {
-        //     return status === 200 ? process.env.REACT_APP_WALLET_CONNECT_BRIDGE : 'https://bridge.walletconnect.org'
-        // }).catch(() => {
-        //     return 'https://bridge.walletconnect.org';
-        // }))
-
-        const BRIDGE_URL = 'https://bridge.walletconnect.org'
+        const BRIDGE_URL = await (fetch('https://wcbridge.loopring.network/hello').then(({status}) => {
+            return status === 200 ? process.env.REACT_APP_WALLET_CONNECT_BRIDGE : 'https://bridge.walletconnect.org'
+        }).catch(() => {
+            return 'https://bridge.walletconnect.org';
+        }))
 
         const provider: WalletConnectProvider = new WalletConnectProvider({
             rpc: RPC_URLS,
