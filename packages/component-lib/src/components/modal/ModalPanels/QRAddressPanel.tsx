cc6ef82cee1848f49459346aff86d9b384839ae4
SylvanasGone <397009765@qq.com>
2021-11-26 19:54:30 +0800

    change qrcode scanning result to app's transfer

--- packages/component-lib/src/components/modal/ModalPanels/QRAddressPanel.tsx
+++ packages/component-lib/src/components/modal/ModalPanels/QRAddressPanel.tsx
@@ -7,13 +7,13 @@ import { Account } from '@loopring-web/common-resources';
 
 export const QRAddressPanel = withTranslation('common')(({
                                                    accAddress,
-                                                   etherscanUrl,
+                                                //    etherscanUrl,
                                                }: WithTranslation & { etherscanUrl: string} & Account) => {
-    const etherscanLink = etherscanUrl + 'address/' + accAddress;
+//     const etherscanLink = etherscanUrl + 'address/' + accAddress;
     return <Box flex={1} paddingY={2} paddingX={2}  display={'flex'} flexDirection={'column'}
                 alignItems={'center'} justifyContent={'center'} >
-        <QRCode value={etherscanLink} size={240} style={{padding: 8,backgroundColor: '#fff'}}
-                aria-label={`link:${etherscanLink}`}/>
+        <QRCode value={accAddress} size={240} style={{padding: 8,backgroundColor: '#fff'}}
+                aria-label={`address:${accAddress}`}/>
         <Typography marginTop={3} variant={'body2'} color={'textSecondary'}
                     style={{wordBreak: 'break-all'}}>{accAddress}</Typography>
     </Box>
