eae3eafa76ce2d8d4c21a212354e10eef2f61a64
SylvanasGone <397009765@qq.com>
2021-12-07 10:18:12 +0800

    Feat ramp (#221)
    * [WIP]feat: RAMP for fait
    
    * [WIP]feat: Ramp
    
    :#

--- packages/webapp/src/pages/Layer2Page/AssetPanel/index.tsx
+++ packages/webapp/src/pages/Layer2Page/AssetPanel/index.tsx
@@ -1,12 +1,10 @@
 import { useCallback, useRef, useEffect, useState } from 'react'
 import { useDeepCompareEffect } from 'react-use';
 import { WithTranslation, withTranslation } from 'react-i18next'
-import ReactEcharts from 'echarts-for-react'
 import { getValuePrecisionThousand, myLog, PriceTag } from '@loopring-web/common-resources'
 import { Box, Grid, Typography } from '@mui/material'
 import styled from '@emotion/styled'
 import { useHistory } from 'react-router-dom'
-import moment from 'moment'
 import {
     AssetsTable,
     AssetTitle,
@@ -28,9 +26,6 @@ import { useSystem } from 'stores/system';
 import { useAccount } from 'stores/account';
 import { useTokenPrices } from 'stores/tokenPrices'
 import { RampInstantSDK } from '@ramp-network/ramp-instant-sdk'
-
-const UP_COLOR = '#00BBA8'
-const DOWN_COLOR = '#fb3838'
 
 const StyledChartWrapper = styled(Box)`
     height: 225px;
@@ -163,8 +158,6 @@ const AssetPanel = withTranslation('common')(({t, ...rest}: WithTranslation) => 
 
     let history = useHistory();
 
-    const { upColor } = useSettings()
-
     const onShowDeposit = useCallback((token?: any) => {
         showDeposit({isShow: true, symbol: token})
     }, [showDeposit])
@@ -187,14 +180,13 @@ const AssetPanel = withTranslation('common')(({t, ...rest}: WithTranslation) => 
         const widget = new RampInstantSDK({
             hostAppName: 'Loopring',
             hostLogoUrl: 'https://ramp.network/assets/images/Logo.svg',
-            // url: 'https://ri-widget-staging.web.app/', // main staging
+            url: 'https://ri-widget-staging.web.app/', // main staging
             swapAsset: 'LOOPRING_*',
             userAddress: accAddress,
-            hostApiKey: 'syxdszpr5q6c9vcnuz8sanr77ammsph59umop68d',
+            hostApiKey: '7v6thd8gprbgac3mupxdkatvotq97mzy62x4g6ga',
         }).show();
-        
         if (widget && widget.domNodes) {
-            (widget as any).domNodes.shadowHost.style.position = 'absolute';
+            (widget as any).domNodes.overlay.style.zIndex = 10000;
         }
     }, [accAddress])
 
@@ -221,81 +213,18 @@ const AssetPanel = withTranslation('common')(({t, ...rest}: WithTranslation) => 
     const currAssetsEthDollar = getValuePrecisionThousand((ethFaitPriceDollar || 0) * (currAssetsEth || 0), undefined, undefined, undefined, false, { isFait: true, floor: true })
     const currAssetsEthYuan = getValuePrecisionThousand((ethFaitPriceYuan || 0) * (currAssetsEth || 0), undefined, undefined, undefined, false, { isFait: true, floor: true })
 
-    const getSign = useCallback((close: string, dataIndex: number) => {
-        let sign
-        const closeLastDay = dataIndex !== 0 && userAssets[ dataIndex - 1 ].close
-        sign = dataIndex > 0 && closeLastDay
-            ? closeLastDay < close 
-                ? 1
-                : -1
-            : 1
-        return sign
-    }, [userAssets])
+    const handleAssetsTrendMove = useCallback(({close}) => {
+        setCurrAssetsEth(close)
+    }, [])
 
-    const isThemeDark = themeMode === 'dark'
-
-    const getAssetsTrendChartOption = useCallback(() => {
-        const option = {
-            grid: { top: 8, right: 8, bottom: 24, left: 0 },
-            xAxis: {
-                show: false,
-                type: 'category',
-                // data: userAssets.map(o => moment(o.timeStamp).format('MMM DD')),
-                data: userAssets.map(o => o.timeStamp),
-                // axisLabel: {
-                //     interval: 7,
-                //     align: 'left',
-                // }
-            },
-            yAxis: {
-                type: 'value',
-                show: false,
-            },
-            series: [
-                {
-                    data: userAssets.map(o => o.close),
-                    type: 'line',
-                    smooth: true,
-                    showSymbol: false,
-                },
-            ],
-            tooltip: {
-                trigger: 'axis',
-                axisPointer: {
-                    type: 'shadow',
-                },
-                backgroundColor: 'var(--color-pop-bg)',
-                borderColor: 'var(--color-border)',
-                textStyle: {
-                    color: 'var(--color-text-primary)',
-                    fontFamily: 'Roboto'
-                },
-                padding: 16,
-                formatter: (params: any) => {
-                    const {name, data, dataIndex} = params[0]
-                    const change = dataIndex === 0 ? '--' : (((data - userAssets[dataIndex - 1].close) / userAssets[dataIndex - 1].close) * 100).toFixed(2)
-                    const sign = getSign(data, dataIndex)
-                    const renderColor = sign !== 1 
-                        ? upColor === 'green' 
-                            ? DOWN_COLOR 
-                            : UP_COLOR 
-                        : upColor === 'green' 
-                            ? UP_COLOR
-                            : DOWN_COLOR
-                    let renderHtml = `<div>
-                        <div>${moment(name).format('YYYY-MM-DD')}</div>
-                        <div>
-                            <span>${Number(data).toFixed(8)} ETH</span>
-                            <span style="color: ${renderColor}">${Number(change || 0) > 0 ? `+${change}` : change} %</span>
-                        </div>
-                    </div>`
-                    return renderHtml
-                }
-            },
+    const handleAssetsTrendMoveOut = useCallback(() => {
+        if (!!userAssets.length) {
+            setCurrAssetsEth(userAssets[userAssets.length - 1].close)
+        } else {
+            setCurrAssetsEth(0)
         }
-        return option
-    }, [userAssets, getSign, upColor])
-
+    }, [userAssets])
+    
     return (
         <>
             <StyleTitlePaper paddingX={3} paddingY={5/2} className={'MuiPaper-elevation2'} >
@@ -323,15 +252,19 @@ const AssetPanel = withTranslation('common')(({t, ...rest}: WithTranslation) => 
                         <Typography component={'span'} variant={'h5'}>{getCurrAssetsEth()} ETH </Typography>
                         <Typography component={'span'} variant={'body2'} color={'var(--color-text-third)'}>&nbsp;&#8776;&nbsp;{currency === Currency.usd ? PriceTag.Dollar + currAssetsEthDollar : PriceTag.Yuan + currAssetsEthYuan}</Typography>
                     </Box>
-                    {!!userAssets.length ?
-                        (
-                            <ReactEcharts
-                                notMerge={true}
-                                lazyUpdate={true}
-                                option={getAssetsTrendChartOption()} />
-                        )
-                    : (
-                        <ChartWrapper marginTop={2} dark={isThemeDark ? 'true' : 'false'} flex={1} component={'div'}/>
+                    {!!userAssets.length ? (
+                        <ScaleAreaChart 
+                        type={ChartType.Trend} 
+                        isDailyTrend 
+                        showArea={false} 
+                        handleMove={handleAssetsTrendMove} 
+                        handleMoveOut={handleAssetsTrendMoveOut}
+                        data={userAssets.map(o => ({
+                            close: o.close,
+                            timeStamp: o.timeStamp,
+                        }))} />
+                    ) : (
+                        <ChartWrapper marginTop={2} dark={themeMode === 'dark' ? 'true' : 'false'} flex={1} component={'div'}/>
                     )}
                 </Box>
             </StyledChartWrapper>
