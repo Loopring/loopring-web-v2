e776630e6af4ae5beb7f42a09b36daef73f2b685
SylvanasGone <397009765@qq.com>
2021-12-14 02:55:53 +0800

    feat: add Ramp access for deposit

--- packages/component-lib/src/components/tradePanel/components/DepositWrap.tsx
+++ packages/component-lib/src/components/tradePanel/components/DepositWrap.tsx
@@ -10,8 +10,9 @@ import { Button, IconClearStyled, TextField } from '../../../index';
 import { DepositViewProps } from './Interface';
 import { BasicACoinTrade } from './BasicACoinTrade';
 import * as _ from 'lodash'
-import { HelpIcon,NFTWholeINFO } from '@loopring-web/common-resources'
+import { HelpIcon,NFTWholeINFO, RampIcon } from '@loopring-web/common-resources'
 import { BasicANFTTrade } from './BasicANFTTrade';
+import { RampInstantSDK } from '@ramp-network/ramp-instant-sdk'
 
 //SelectReceiveCoin
 export const DepositWrap = <T extends IBData<I> & Partial<NFTWholeINFO>,
@@ -38,7 +39,7 @@ export const DepositWrap = <T extends IBData<I> & Partial<NFTWholeINFO>,
             return false
         }
     };
-    const [address, setAddress] = React.useState<string | undefined>(addressDefault ? addressDefault : '');
+    const [address, setAddress] = React.useState<string | undefined>(addressDefault || '');
     const [addressError, setAddressError] = React.useState<{ error: boolean, message?: string | React.ElementType<HTMLElement> } | undefined>();
     // const [error, setError] = React.useState<{ error: boolean, message?: string | React.ElementType }>({
     //     error: false,
@@ -65,6 +66,21 @@ export const DepositWrap = <T extends IBData<I> & Partial<NFTWholeINFO>,
         setAddress(address);
         debounceAddress({address})
     }
+
+    const showRamp = React.useCallback(() => {
+        const widget = new RampInstantSDK({
+            hostAppName: 'Loopring',
+            hostLogoUrl: 'https://ramp.network/assets/images/Logo.svg',
+            swapAsset: 'LOOPRING_*',
+            userAddress: addressDefault,
+            hostApiKey: 'syxdszpr5q6c9vcnuz8sanr77ammsph59umop68d',
+        }).show();
+        
+        if (widget && widget.domNodes) {
+            (widget as any).domNodes.shadowHost.style.position = 'absolute';
+            (widget as any).domNodes.overlay.style.zIndex = 10000;
+        }
+    }, [addressDefault])
     
     const inputButtonDefaultProps = {
         label: t('depositLabelEnterToken'),
@@ -103,6 +119,17 @@ export const DepositWrap = <T extends IBData<I> & Partial<NFTWholeINFO>,
                     </Trans>
                 </Typography>
             </PopoverPure>
+        </Grid>
+        <Grid item alignSelf={"flex-end"} display={'flex'} alignItems={'center'}>
+            <Button
+                variant={'text'} 
+                style={{ textTransform: 'none', paddingRight: 0 }}
+                onClick={showRamp}
+            >
+                {t('labelDepositRamp')}
+                <RampIcon fontSize={'large'} style={{ marginLeft: 4, marginRight: 4 }} />
+                <Typography>Ramp</Typography>
+            </Button>
         </Grid>
         <Grid item marginTop={2} alignSelf={"stretch"}>
             {type === 'NFT'?<Box display={'inline-flex'} alignItems={'center'} justifyContent={'space-between'}>
