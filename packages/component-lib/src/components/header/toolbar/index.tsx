cd6f863a5b0772d47a92debb96380930bb6bc703
Winda <windatang@gmail.com>
2021-12-16 12:56:44 +0800

    Code clean (#264)
    * code clean
    
    * pull/261
    
    * pull/263
    
    * code clean

--- packages/component-lib/src/components/header/toolbar/index.tsx
+++ packages/component-lib/src/components/header/toolbar/index.tsx
@@ -1,94 +1,67 @@
 import { Box, IconButton, Link, } from '@mui/material';
-import {
-    DownloadIcon,
-    // DropDownIcon,
-    // i18n,
-    // LanguageType,
-    NotificationIcon, SettingIcon,
-    // ThemeDarkIcon,
-    // ThemeLightIcon,
-    // ThemeType,
-} from '@loopring-web/common-resources';
-// import React from 'react';
+import { DownloadIcon, NotificationIcon, SettingIcon, } from '@loopring-web/common-resources';
 import { WithTranslation } from 'react-i18next';
 import { bindHover, usePopupState } from 'material-ui-popup-state/hooks';
 import { bindPopper } from 'material-ui-popup-state/es';
-import {
-    // OutlineSelect,
-    // OutlineSelectItem,
-    PopoverPure } from '../../basic-lib';
+import { PopoverPure } from '../../basic-lib';
 import { SettingPanel } from '../../block/SettingPanel';
 import { QRCodePanel } from '../../modal';
 
 export const BtnDownload = ({
-                                t,
-                                url,
-                                i18nTitle,
-                                i18nDescription,
-                                ...rest
+                              t,
+                              url,
+                              i18nTitle,
+                              i18nDescription,
+                              ...rest
                             }: & { i18nTitle: string, i18nDescription: string, url: string } & WithTranslation) => {
-    const popupState = usePopupState({variant: 'popover', popupId: 'download-QRcode'});
-    const Description = () => <Link target={'_blank'} href='https://loopring.io'>{t(i18nDescription)}</Link>
+  const popupState = usePopupState({variant: 'popover', popupId: 'download-QRcode'});
+  const Description = () => <Link target={'_blank'} href='https://loopring.io'>{t(i18nDescription)}</Link>
 
-    return <Box>
-        <IconButton aria-label={t('labeldownloadApp')} {...bindHover(popupState)}><DownloadIcon/></IconButton>
-        <PopoverPure
-            // type={PopoverType.hover}
-            {...bindPopper(popupState)}
-            //popupId="wallet-connect-notification"
-            // popoverContent={}
-            //arrowHorizon={{right: 98}}
-            // className={'arrow-center'}
-            anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'center',
-            }}
-            transformOrigin={{
-                vertical: 'top',
-                horizontal: 'center',
-            }}
-        >
-            <Box margin={3}><QRCodePanel {...{
-                ...rest, title: t(i18nTitle), description: <Description/>,
-                url, t
-            }} /></Box>
-        </PopoverPure>
-    </Box>
+  return <Box>
+    <IconButton aria-label={t('labeldownloadApp')} {...bindHover(popupState)}><DownloadIcon/></IconButton>
+    <PopoverPure
+      {...bindPopper(popupState)}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'center',
+      }}
+    >
+      <Box margin={3}><QRCodePanel {...{
+        ...rest, title: t(i18nTitle), description: <Description/>,
+        url, t
+      }} /></Box>
+    </PopoverPure>
+  </Box>
 }
 
 
 export const BtnNotification = ({t}: any) => <IconButton
-    aria-label={t('alert message')}><NotificationIcon/></IconButton>
+  aria-label={t('alert message')}><NotificationIcon/></IconButton>
 
 
 export const BtnSetting = ({t, label}: any) => {
-    const popupState = usePopupState({variant: 'popover', popupId: 'settingPop'});
-    return<Box>
-            <IconButton aria-label={t(label)} {...bindHover(popupState)}><SettingIcon/></IconButton>
-            <PopoverPure
-                // type={PopoverType.hover}
-                {...bindPopper(popupState)}
-
-                //popupId="wallet-connect-notification"
-                // popoverContent={}
-                //arrowHorizon={{right: 98}}
-                // className={'arrow-center'}
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'center',
-                }}
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'center',
-                }}
-            >
-                {/*<ClickAwayListener onClickAway={()=>{popupState.setOpen(false)}}>*/}
-                   <Box margin={2}> <SettingPanel/>  </Box>
-                {/*</ClickAwayListener>*/}
-            </PopoverPure>
-        </Box>
+  const popupState = usePopupState({variant: 'popover', popupId: 'settingPop'});
+  return <Box>
+    <IconButton aria-label={t(label)} {...bindHover(popupState)}><SettingIcon/></IconButton>
+    <PopoverPure
+      {...bindPopper(popupState)}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'center',
+      }}
+    >
+      <Box margin={2}> <SettingPanel/> </Box>
+    </PopoverPure>
+  </Box>
 }
-
 
 export * from './Interface';
 export * from './WalletConnect';
--- static/documents/markdown/whyamIblocked_en.md
+++ static/documents/markdown/whyamIblocked_en.md
@@ -1,7 +1,7 @@
-#  Blacklist
+# Blacklist
 
-Loopring Technology will backlist users who violate our **TERMS of USE**.
+Loopring Technology will blacklist users who violate our [**TERMS OF USE**](./terms_en.md).
 
-Once a user is blacklisted, the user will not be able to access certain services we provide, such as but not limited to wallet lock, recovery, upgrade, red packet, claiming and using Points, etc. However, the user can still withdraw funds from our exchange and his/her Loopring Wallet at any time.
+Once a user is blacklisted, the user will not be able to access certain services we provide, such as but not limited to: wallet lock, recovery, upgrade, red packet functionality, claiming and using Points, etc. However, the user can still withdraw funds from our exchange and his/her Loopring Wallet at any time.
 
 
