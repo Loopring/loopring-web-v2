46c8d926f0e641cb66c3691dcdfded861291cf35
windatang <windatang@gmail.com>
2021-12-20 00:35:20 +0800

    event page for cross-platform

--- packages/component-lib/src/components/block/TradeRace.tsx
+++ packages/component-lib/src/components/block/TradeRace.tsx
@@ -1,34 +1,43 @@
-import React from 'react'
-import { Column, Table } from '../basic-lib';
-import { WithTranslation, withTranslation } from 'react-i18next';
-import styled from '@emotion/styled';
-import { Box, Typography } from '@mui/material'
-import { getValuePrecisionThousand } from '@loopring-web/common-resources';
+import React from "react";
+import { Column, Table } from "../basic-lib";
+import { WithTranslation, withTranslation } from "react-i18next";
+import styled from "@emotion/styled";
+import { Box, Typography } from "@mui/material";
+import { getValuePrecisionThousand } from "@loopring-web/common-resources";
 
 export interface TradeRaceRow {
   project: string;
   pair: string;
   reward: {
-      count: number,
-      token: string };
+    count: number;
+    token: string;
+  };
 }
-
+const rowHeight = 44;
 const TableStyle = styled(Table)`
   &.rdg {
-    min-Height: 380px;
+    box-sizing: border-box;
     border-top: 1px solid var(--color-divide);
     text-align: center;
-
+    min-height: initial;
+    height: ${(props: any) => {
+      if (props.currentheight) {
+        return props.currentheight + 2 + "px";
+      } else {
+        return "100%";
+      }
+    }};
     .rdg-header-row {
       // background:var(--color-table-header-bg);
     }
 
-    .rdg-row, .rdg-header-row {
+    .rdg-row,
+    .rdg-header-row {
       border-right: 1px solid var(--color-divide);
-
     }
 
     .rdg-cell {
+      box-sizing: border-box;
       border-left-width: 1px;
       border-bottom-width: 1px;
       border-left-color: var(--color-divide);
@@ -43,47 +52,54 @@ const TableStyle = styled(Table)`
       text-align: right;
     }
   }
-` as typeof Table
+` as typeof Table;
 
-export const TradeRacePanel = withTranslation(['tables'])(({
-                                                             t,
-                                                             rawData,
-                                                             ...rest
-                                                           }: & WithTranslation & { rawData: TradeRaceRow[] }) => {
-  const getColumnMode = React.useCallback(({t}: WithTranslation): Column<any, unknown>[] => [
-    {
-      key: 'project',
-      name: t('labelTradeRaceProject'),
-      formatter: ({row}) => {
-        const value = row[ 'project' ]
+export const TradeRacePanel = withTranslation(["tables"])(
+  ({ t, rawData, ...rest }: WithTranslation & { rawData: TradeRaceRow[] }) => {
+    const getColumnMode = React.useCallback(
+      ({ t }: WithTranslation): Column<any, unknown>[] => [
+        {
+          key: "project",
+          name: t("labelTradeRaceProject"),
+          formatter: ({ row }) => {
+            const value = row["project"];
 
-        return (
-          <Box className="rdg-cell-value">
-            {value}
-          </Box>
-        )
-      }
-    },
-    {key: 'pair', name: t('labelTradeRacePair')},
-    {
-      key: 'reward',
-      name: t('labelTradeRaceReward'),
-      formatter: ({row}) => {
-        return <>
-          <Typography variant={'body1'} component={'span'}>{
-              getValuePrecisionThousand(row.reward.count)}</Typography>
-         <Typography variant={'body1'} component={'span'} marginLeft={1}>{row.reward.token}</Typography>
-        </>
-      }
-    }
-  ], [])
+            return <Box className="rdg-cell-value">{value}</Box>;
+          },
+        },
+        { key: "pair", name: t("labelTradeRacePair") },
+        {
+          key: "reward",
+          name: t("labelTradeRaceReward"),
+          formatter: ({ row }) => {
+            return (
+              <>
+                <Typography variant={"body1"} component={"span"}>
+                  {getValuePrecisionThousand(row.reward.count)}
+                </Typography>
+                <Typography variant={"body1"} component={"span"} marginLeft={1}>
+                  {row.reward.token}
+                </Typography>
+              </>
+            );
+          },
+        },
+      ],
+      []
+    );
 
-  const defaultArgs: any = {
-    columnMode: getColumnMode({t, ...rest}),
-    generateRows: (rawData: any) => rawData,
-    generateColumns: ({columnsRaw}: any) => columnsRaw as Column<any, unknown>[],
+    const defaultArgs: any = {
+      columnMode: getColumnMode({ t, ...rest }),
+      currentheight: (rawData.length + 1) * rowHeight,
+      generateRows: (rawData: any) => rawData,
+      generateColumns: ({ columnsRaw }: any) =>
+        columnsRaw as Column<any, unknown>[],
+    };
+
+    return (
+      <TableStyle<TradeRaceRow, unknown>
+        {...{ ...defaultArgs, t, ...rest, rawData }}
+      />
+    );
   }
-
-  return <TableStyle<TradeRaceRow, unknown> {...{...defaultArgs, t, ...rest, rawData}}  />
-
-})
+);
--- packages/landing/src/routers/index.tsx
+++ packages/landing/src/routers/index.tsx
@@ -1,24 +1,21 @@
-import { Redirect, Route, Switch } from 'react-router-dom'
-import React from 'react';
-
-import { LandPage } from 'LandPage/LandPage';
-import { Footer } from '@loopring-web/component-lib/src/components/footer';
-import Header from '../header';
-
+import { Route, Switch } from "react-router-dom";
+import React from "react";
+import { LandPage } from "LandPage/LandPage";
+import { Footer } from "@loopring-web/component-lib/src/components/footer";
+import Header from "../header";
 
 const RouterView = () => {
-
-
-    return <>
-        <Switch>
-            <Route exact path='/'>
-                <Header isHideOnScroll={true} isLandPage />
-                <LandPage/>
-            </Route>
-        </Switch>
-        <
-            Footer/>
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Header isHideOnScroll={true} isLandPage />
+          <LandPage />
+        </Route>
+      </Switch>
+      <Footer />
     </>
-}
+  );
+};
 
-export default RouterView
+export default RouterView;
