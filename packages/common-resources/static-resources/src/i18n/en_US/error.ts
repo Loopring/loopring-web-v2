58e93d4d10545bf14d45a6562bcbd3d1137f9b9a
windatang <windatang@gmail.com>
2021-12-19 22:28:37 +0800

    hack!!! relay mantain notifaction

--- packages/common-resources/static-resources/src/i18n/en_US/error.ts
+++ packages/common-resources/static-resources/src/i18n/en_US/error.ts
@@ -1,6 +1,8 @@
 export default {
-    labelConnectUs: 'If you believe this is indeed a bug, please <1>contact us</1> <2/> We would appreciate your feedback',
-    errorBase: 'Oops! Something went wrong.',
-    errorLoading: 'Loading! Please wait for a while.',
-    error404: '404! page can\'t be reached',
-}
+  labelConnectUs:
+    "If you believe this is indeed a bug, please <1>contact us</1> <2/> We would appreciate your feedback",
+  errorBase: "Oops! Something went wrong.",
+  errorLoading: "Loading! Please wait for a while.",
+  error404: "404! page can't be reached",
+  errorMaintain: "System update! Please wait for a while.",
+};
