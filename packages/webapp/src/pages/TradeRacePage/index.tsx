280aea02fe7b8b0f5d6fa3985b756c19c4b78ea2
SylvanasGone <397009765@qq.com>
2021-12-20 14:44:08 +0800

    fix: correct event during period (#285)

--- packages/webapp/src/pages/TradeRacePage/index.tsx
+++ packages/webapp/src/pages/TradeRacePage/index.tsx
@@ -14,11 +14,13 @@ import { useAmmMiningUI } from "../MiningPage/hook";
 import {
   CURRENT_EVENT_DATE,
   DropDownIcon,
+  getValuePrecisionThousand,
 } from "@loopring-web/common-resources";
 import { LoadingBlock } from "../LoadingPage";
 //@ts-ignore
 import cssStyle from "./snow.css";
 import moment from "moment";
+import { volumeToCount } from "../../hooks/help";
 
 const LayoutStyled = styled(Box)`
   width: 100%;
@@ -53,7 +55,6 @@ const LayoutStyled = styled(Box)`
       top: 0;
     }
   }
-
   //
   ${cssStyle}
 `;
@@ -88,6 +89,7 @@ export const TradeRacePage = withTranslation("common")(
   ({ t }: WithTranslation) => {
     const { search } = useLocation();
     const [currMarketPair, setCurrMarketPair] = React.useState("");
+    const volumeToken = currMarketPair ? currMarketPair.split("-")[1] : "";
     const { ammActivityMap } = useAmmPool();
     const {
       eventData,
@@ -95,11 +97,15 @@ export const TradeRacePage = withTranslation("common")(
       countDown,
       currPairUserRank,
       currPairRankData,
+      rewardToken,
       getAmmGameRank,
       getAmmGameUserRank,
       eventStatus,
     } = useTradeRace();
     const { volume, rank } = currPairUserRank || {};
+    const userVolume = volume
+      ? getValuePrecisionThousand(volumeToCount(volumeToken, volume))
+      : "--";
     const { ammActivityViewMap } = useAmmMiningUI({ ammActivityMap });
     const filteredAmmViewMap = ammActivityViewMap
       .filter((o) => o.activity.ruleType === "SWAP_VOLUME_RANKING")
@@ -361,7 +367,7 @@ export const TradeRacePage = withTranslation("common")(
                   alignItems={"center"}
                 >
                   <Typography fontSize={16} marginRight={2}>
-                    {t("labelTradeRaceYourVolume")}: {volume || "--"}
+                    {t("labelTradeRaceYourVolume")} ({volumeToken}): {userVolume}
                   </Typography>
                   <Typography fontSize={16}>
                     {t("labelTradeRaceYourRanking")}: {rank || "--"}
@@ -376,9 +382,14 @@ export const TradeRacePage = withTranslation("common")(
                     {t("labelTradeRaceGoTrading")} &gt;&gt;
                   </Button>
                 </Box>
-                {/*{eventStatus !== EVENT_STATUS.EVENT_READY && (*/}
-                <TradeRaceTable {...{ t, rawData: currPairRankData }} />
-                {/*)}*/}
+                <TradeRaceTable
+                  {...{
+                    t,
+                    rawData: currPairRankData,
+                    volumeToken,
+                    rewardToken,
+                  }}
+                />
               </TableWrapperStyled>
             </Box>
             <Box
