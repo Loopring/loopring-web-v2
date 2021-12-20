7c056ea5e19e73de4abfed0b075b6b10ff8a2158
Winda <windatang@gmail.com>
2021-12-20 23:06:24 +0800

    Revert "token price issue update (#293)"
    This reverts commit c841f380dc6b6530751b873edfa1e6e4fc2e6a6d.

--- packages/webapp/src/pages/QuotePage/hook.ts
+++ packages/webapp/src/pages/QuotePage/hook.ts
@@ -1,155 +1,128 @@
-import React from "react";
-import store from "stores";
-import {
-  MarketBlockProps,
-  QuoteTableRawDataItem,
-} from "@loopring-web/component-lib";
-import { TradingInterval, WsTopicType } from "@loopring-web/loopring-sdk";
-import { LoopringAPI } from "api_wrapper";
-import { tickerService } from "services/socket";
-import { myError, SagaStatus } from "@loopring-web/common-resources";
-import _ from "lodash";
-import { TickerMap, useTicker } from "stores/ticker";
-import { useSocket } from "../../stores/socket";
+import React from "react"
+import store from 'stores';
+import { MarketBlockProps, QuoteTableRawDataItem, } from '@loopring-web/component-lib';
+import { TradingInterval, WsTopicType } from '@loopring-web/loopring-sdk';
+import { LoopringAPI } from 'api_wrapper'
+import { tickerService } from 'services/socket';
+import { myError, SagaStatus, } from "@loopring-web/common-resources";
+import _ from 'lodash'
+import { TickerMap, useTicker } from 'stores/ticker';
+import { useSocket } from '../../stores/socket';
 
-export function useTickList<C extends { [key: string]: string }>() {
+export function useTickList<C extends { [ key: string ]: string }>() {
   const [tickList, setTickList] = React.useState<any>([]);
   const [recommendedPairs, setRecommendedPairs] = React.useState<string[]>([]);
-  const { marketArray, coinMap, marketMap, tokenMap } =
-    store.getState().tokenMap;
-  const [recommendations, setRecommendations] = React.useState<
-    MarketBlockProps<C>[]
-  >([]);
-  const { tickerMap, status: tickerStatus } = useTicker();
-  const { forex } = store.getState().system;
-  const { tokenPrices } = store.getState().tokenPrices;
-  console.log(tokenPrices);
-  console.log(tokenMap);
+  const {marketArray, coinMap, marketMap, tokenMap} = store.getState().tokenMap;
+  const [recommendations, setRecommendations] = React.useState<MarketBlockProps<C>[]>([]);
+  const {tickerMap, status: tickerStatus} = useTicker();
+  const {forex} = store.getState().system
+  const {tokenPrices} = store.getState().tokenPrices
   const getRecommendPairs = React.useCallback(async () => {
     if (LoopringAPI.exchangeAPI) {
       try {
-        const { recommended } =
-          await LoopringAPI.exchangeAPI.getRecommendedMarkets();
-        setRecommendedPairs(recommended);
-        return recommended || [];
+        const {recommended} = await LoopringAPI.exchangeAPI.getRecommendedMarkets()
+        setRecommendedPairs(recommended)
+        return recommended || []
       } catch (e) {
-        myError(e);
+        myError(e)
       }
-      return [];
+      return []
+
     }
-  }, [setRecommendedPairs]);
-  const updateRecommendation = React.useCallback(
-    (recommendationIndex, ticker) => {
-      if (recommendations.length) {
-        recommendations[recommendationIndex].tradeFloat = ticker;
-        setRecommendations(recommendations);
+  }, [setRecommendedPairs])
+  const updateRecommendation = React.useCallback((recommendationIndex, ticker) => {
+    if (recommendations.length) {
+      recommendations[ recommendationIndex ].tradeFloat = ticker
+      setRecommendations(recommendations)
+    }
+  }, [recommendations, setRecommendations]);
+  const updateRawData = React.useCallback(async (tickerMap: TickerMap<C>) => {
+    const marketPairs: string[] = await getRecommendPairs()
+    let _recommendationsFloat: QuoteTableRawDataItem[] = [];
+    let defaultRecommendationsFloat: QuoteTableRawDataItem[] = []
+    const _tickList = tickerMap && Object.keys(tickerMap) ? Reflect.ownKeys(tickerMap).reduce((prev, key) => {
+      // @ts-ignore
+      const [, coinA, coinB] = key.match(/(\w+)-(\w+)/i);
+      let _item = {
+        ...tickerMap[ key as string ],
+        pair: {
+          coinA,
+          coinB,
+        },
+        coinAPriceDollar: tokenPrices[ coinA ] || 0,
+        coinAPriceYuan: (tokenPrices[ coinA ] || 0) * (forex || 6.5),
+      } as QuoteTableRawDataItem;
+
+      if (marketPairs.findIndex(m => m === key) !== -1) {
+        _recommendationsFloat.push(_.cloneDeep(_item))
       }
-    },
-    [recommendations, setRecommendations]
-  );
-  const updateRawData = React.useCallback(
-    async (tickerMap: TickerMap<C>) => {
-      const marketPairs: string[] = await getRecommendPairs();
-      let _recommendationsFloat: QuoteTableRawDataItem[] = [];
-      let defaultRecommendationsFloat: QuoteTableRawDataItem[] = [];
-      const _tickList =
-        tickerMap && Object.keys(tickerMap)
-          ? Reflect.ownKeys(tickerMap).reduce((prev, key) => {
-              // @ts-ignore
-              const [, coinA, coinB] = key.match(/(\w+)-(\w+)/i);
-              let _item = {
-                ...tickerMap[key as string],
-                pair: {
-                  coinA,
-                  coinB,
-                },
-                coinAPriceDollar: tokenPrices[coinA] || 0,
-                coinAPriceYuan: (tokenPrices[coinA] || 0) * (forex || 6.5),
-              } as QuoteTableRawDataItem;
-
-              if (marketPairs.findIndex((m) => m === key) !== -1) {
-                _recommendationsFloat.push(_.cloneDeep(_item));
-              }
-              if (
-                marketArray &&
-                marketArray.findIndex((m) => m === key) !== -1
-              ) {
-                defaultRecommendationsFloat.push(_.cloneDeep(_item));
-              }
-              prev.push(_item);
-              return prev;
-            }, [] as QuoteTableRawDataItem[])
-          : [];
-
-      const newTickListWithPrecision = _tickList.map((o: any) => {
-        const pair = o.__rawTicker__.symbol;
-        const precision = marketMap
-          ? marketMap[pair]?.precisionForPrice
-          : undefined;
-        return {
-          ...o,
-          precision,
-        };
-      });
-
-      setTickList(newTickListWithPrecision);
-      _recommendationsFloat = _recommendationsFloat.filter((o) => {
-        const { coinA, coinB } = o.pair;
-        return coinMap && coinMap[coinA] && coinMap[coinB];
-      });
-
-      if (_recommendationsFloat.length < 4) {
-        const filteredFloat = defaultRecommendationsFloat.filter((o) => {
-          const pair = `${o.pair.coinA}-${o.pair.coinB}`;
-          return !marketPairs.includes(pair);
-        });
-        _recommendationsFloat = _recommendationsFloat.concat(
-          filteredFloat.slice(0, 4 - _recommendationsFloat.length)
-        );
+      if (marketArray && marketArray.findIndex(m => m === key) !== -1) {
+        defaultRecommendationsFloat.push(_.cloneDeep(_item))
       }
+      prev.push(_item);
+      return prev
+    }, [] as QuoteTableRawDataItem[]) : []
 
-      while (_recommendationsFloat.length < 4) {
-        _recommendationsFloat.push(_.cloneDeep(_recommendationsFloat[0]));
+    const newTickListWithPrecision = _tickList.map((o: any) => {
+      const pair = o.__rawTicker__.symbol
+      const precision = marketMap ? marketMap[ pair ]?.precisionForPrice : undefined
+      return ({
+        ...o,
+        precision,
+      })
+    })
+
+    setTickList(newTickListWithPrecision)
+    _recommendationsFloat = _recommendationsFloat.filter(o => {
+      const {coinA, coinB} = o.pair
+      return (coinMap && coinMap[ coinA ] && coinMap[ coinB ])
+    })
+
+    if (_recommendationsFloat.length < 4) {
+      const filteredFloat = defaultRecommendationsFloat.filter(o => {
+        const pair = `${o.pair.coinA}-${o.pair.coinB}`
+        return !marketPairs.includes(pair)
+      })
+      _recommendationsFloat = _recommendationsFloat.concat(filteredFloat.slice(0, 4 - _recommendationsFloat.length));
+    }
+
+    while (_recommendationsFloat.length < 4) {
+      _recommendationsFloat.push(_.cloneDeep(_recommendationsFloat[ 0 ]))
+    }
+
+    const _recommendations = _recommendationsFloat.reduce((prev, item) => {
+      if (coinMap && item) {
+        const {coinA, coinB} = item.pair;
+        const _item: MarketBlockProps<C> = {
+          tradeFloat: item as any,
+          // @ts-ignore
+          coinAInfo: coinMap[ coinA ],
+          // @ts-ignore
+          coinBInfo: coinMap[ coinB ]
+        } as MarketBlockProps<C>
+        prev.push(_item)
       }
+      return prev
+    }, [] as MarketBlockProps<C>[])
 
-      const _recommendations = _recommendationsFloat.reduce((prev, item) => {
-        if (coinMap && item) {
-          const { coinA, coinB } = item.pair;
-          const _item: MarketBlockProps<C> = {
-            tradeFloat: item as any,
-            // @ts-ignore
-            coinAInfo: coinMap[coinA],
-            // @ts-ignore
-            coinBInfo: coinMap[coinB],
-          } as MarketBlockProps<C>;
-          prev.push(_item);
+    const _recommendationsWithPrecision = _recommendations.map(o => {
+      const pair = o.tradeFloat[ '__rawTicker__' ]?.symbol
+      const coinB = o.tradeFloat[ 'pair' ]?.coinB
+      const marketPrecision = marketMap ? marketMap[ pair ]?.precisionForPrice : undefined
+      const coinBPrecision = tokenMap ? tokenMap[ coinB ]?.precision : undefined
+      return ({
+        ...o,
+        tradeFloat: {
+          ...o.tradeFloat,
+          marketPrecision,
+          coinBPrecision,
         }
-        return prev;
-      }, [] as MarketBlockProps<C>[]);
+      })
+    })
 
-      const _recommendationsWithPrecision = _recommendations.map((o) => {
-        const pair = o.tradeFloat["__rawTicker__"]?.symbol;
-        const coinB = o.tradeFloat["pair"]?.coinB;
-        const marketPrecision = marketMap
-          ? marketMap[pair]?.precisionForPrice
-          : undefined;
-        const coinBPrecision = tokenMap
-          ? tokenMap[coinB]?.precision
-          : undefined;
-        return {
-          ...o,
-          tradeFloat: {
-            ...o.tradeFloat,
-            marketPrecision,
-            coinBPrecision,
-          },
-        };
-      });
 
-      setRecommendations(_recommendationsWithPrecision);
-    },
-    [tickList]
-  );
+    setRecommendations(_recommendationsWithPrecision)
+  }, [tickList])
 
   React.useEffect(() => {
     if (tickerStatus === SagaStatus.UNSET) {
@@ -163,26 +136,27 @@ export function useTickList<C extends { [key: string]: string }>() {
     updateRecommendation,
     recommendations,
     updateRawData,
-    getRecommendPairs,
-  };
+    getRecommendPairs
+  }
+
 }
 
-export function useQuote<C extends { [key: string]: string }>() {
-  const { sendSocketTopic, socketEnd } = useSocket();
-  const { marketArray } = store.getState().tokenMap;
+export function useQuote<C extends { [ key: string ]: string }>() {
+  const {sendSocketTopic, socketEnd} = useSocket();
+  const {marketArray} = store.getState().tokenMap;
   const {
     recommendedPairs,
     tickList,
     recommendations,
     tickerMap,
     getRecommendPairs,
-    updateRecommendation,
-  } = useTickList();
+    updateRecommendation
+  } = useTickList()
   const subject = React.useMemo(() => tickerService.onSocket(), []);
 
   React.useEffect(() => {
-    const subscription = subject.subscribe(({ tickerMap }) => {
-      socketCallback();
+    const subscription = subject.subscribe(({tickerMap}) => {
+      socketCallback()
     });
     return () => subscription.unsubscribe();
   }, [subject]);
@@ -190,69 +164,68 @@ export function useQuote<C extends { [key: string]: string }>() {
   const socketCallback = React.useCallback(() => {
     if (tickerMap && recommendedPairs) {
       Reflect.ownKeys(tickerMap).forEach((key) => {
-        let recommendationIndex = recommendedPairs.findIndex(
-          (ele) => ele === key
-        );
+        let recommendationIndex = recommendedPairs.findIndex(ele => ele === key)
         if (recommendationIndex !== -1) {
-          updateRecommendation(recommendationIndex, tickerMap[key as string]);
+          updateRecommendation(recommendationIndex, tickerMap[ key as string ])
         }
         //TODO update related row. use socket return
-      });
+      })
     }
-  }, [recommendedPairs, updateRecommendation, tickerMap]);
+  }, [recommendedPairs, updateRecommendation, tickerMap])
+
 
   React.useEffect(() => {
-    getRecommendPairs();
-  }, [getRecommendPairs]);
+    getRecommendPairs()
+  }, [getRecommendPairs])
 
   React.useEffect(() => {
     socketSendTicker();
     return () => {
-      socketEnd();
-    };
+      socketEnd()
+    }
   }, []);
 
+
   const socketSendTicker = React.useCallback(() => {
-    sendSocketTopic({ [WsTopicType.ticker]: marketArray });
-  }, []);
+    sendSocketTopic({[ WsTopicType.ticker ]: marketArray});
+  }, [])
 
   return {
     tickList,
     recommendations,
-  };
+  }
 }
+
 
 export type CandlestickItem = {
   close: number;
   timeStamp: number;
-};
+}
 
 export const useCandlestickList = (market: string) => {
-  const [candlestickList, setCandlestickList] = React.useState<
-    CandlestickItem[]
-  >([]);
+  const [candlestickList, setCandlestickList] = React.useState<CandlestickItem[]>([])
   const getCandlestick = React.useCallback(async (market: string) => {
     if (LoopringAPI.exchangeAPI) {
       const res = await LoopringAPI.exchangeAPI.getMixCandlestick({
         market: market,
         interval: TradingInterval.d1,
-        limit: 7,
-      });
+        limit: 7
+      })
       if (res && res.candlesticks && !!res.candlesticks.length) {
-        const data = res.candlesticks.map((o) => ({
+        const data = res.candlesticks.map(o => ({
           close: o.close,
-          timeStamp: o.timestamp,
-        }));
-        setCandlestickList(data);
+          timeStamp: o.timestamp
+        }))
+        setCandlestickList(data)
       }
-      setCandlestickList([]);
+      setCandlestickList([])
     }
-    setCandlestickList([]);
-  }, []);
+    setCandlestickList([])
+  }, [])
 
   React.useEffect(() => {
-    getCandlestick(market);
-  }, [getCandlestick, market]);
+    getCandlestick(market)
+  }, [getCandlestick, market])
 
-  return candlestickList;
-};
+  return candlestickList
+}
--- packages/webapp/src/stores/token/saga.ts
+++ packages/webapp/src/stores/token/saga.ts
@@ -1,89 +1,83 @@
 import { all, call, fork, put, takeLatest } from "redux-saga/effects";
-import { getTokenMap, getTokenMapStatus } from "./reducer";
-import { getIcon } from "../../utils/swap_utils";
-import { CoinInfo, CoinMap } from "@loopring-web/common-resources";
-import { AddressMap, GetTokenMapParams, IdMap } from "./interface";
-import { PayloadAction } from "@reduxjs/toolkit";
+import { getTokenMap, getTokenMapStatus } from './reducer';
+import { getIcon } from '../../utils/swap_utils';
+import { CoinInfo, CoinMap } from '@loopring-web/common-resources';
+import { AddressMap, GetTokenMapParams, IdMap } from './interface';
+import { PayloadAction } from '@reduxjs/toolkit';
 
-const getTokenMapApi = async <R extends { [key: string]: any }>({
-  tokensMap,
-  pairs,
-  marketArr,
-  tokenArr,
-}: GetTokenMapParams<R>) => {
-  let coinMap: CoinMap<any, CoinInfo<any>> = {};
-  let totalCoinMap: CoinMap<any, CoinInfo<any>> = {};
-  let tokenMap: any = tokensMap;
-  let addressIndex: AddressMap = {};
-  let idIndex: IdMap = {};
-  Reflect.ownKeys(tokensMap).forEach((key) => {
-    const coinInfo = {
-      icon: getIcon(key as string, tokensMap),
-      name: key as string,
-      simpleName: key as string,
-      description: "",
-      company: "",
-    };
-    if (!(key as string).startsWith("LP-")) {
-      coinMap[key as string] = coinInfo;
-    }
-    totalCoinMap[key as string] = coinInfo;
+const getTokenMapApi = async <R extends { [ key: string ]: any }>({
+                                                                      tokensMap,
+                                                                      pairs,
+                                                                      marketArr,
+                                                                      tokenArr
+                                                                  }: GetTokenMapParams<R>) => {
+    let coinMap: CoinMap<any, CoinInfo<any>> = {};
+    let totalCoinMap: CoinMap<any, CoinInfo<any>> = {};
+    let tokenMap: any = tokensMap;
+    let addressIndex: AddressMap = {};
+    let idIndex: IdMap = {};
+    Reflect.ownKeys(tokensMap).forEach((key) => {
+        const coinInfo = {
+            icon: getIcon(key as string, tokensMap),
+            name: key as string,
+            simpleName: key as string,
+            description: '',
+            company: '',
+        }
+        if (!(key as string).startsWith('LP-')) {
+            coinMap[ key as string ] = coinInfo
+        }
+        totalCoinMap[ key as string ] = coinInfo
 
-    if (pairs[key as string] && pairs[key as string].tokenList) {
-      // @ts-ignore
-      tokensMap[key].tradePairs = pairs[key as string].tokenList;
+        if (pairs[ key as string ] && pairs[ key as string ].tokenList) {
+            // @ts-ignore
+            tokensMap[ key ].tradePairs = pairs[ key as string ].tokenList;
+        }
+        addressIndex = {
+            ...addressIndex,
+            // @ts-ignore
+            [ tokensMap[ key ].address ]: key as string
+        }
+        idIndex = {
+            ...idIndex,
+            // @ts-ignore
+            [ tokensMap[ key ].tokenId ]: key as string
+        }
+    })
+    return {
+        data: {
+            coinMap,
+            totalCoinMap,
+            addressIndex,
+            idIndex,
+            tokenMap,
+            marketArray: marketArr,
+            marketCoins: tokenArr
+        }
     }
-    addressIndex = {
-      ...addressIndex,
-      // @ts-ignore
-      [tokensMap[key].address.toLowerCase()]: key as string,
-    };
-    idIndex = {
-      ...idIndex,
-      // @ts-ignore
-      [tokensMap[key].tokenId]: key as string,
-    };
-  });
-  return {
-    data: {
-      coinMap,
-      totalCoinMap,
-      addressIndex,
-      idIndex,
-      tokenMap,
-      marketArray: marketArr,
-      marketCoins: tokenArr,
-    },
-  };
 };
 
-export function* getPostsSaga<R extends { [key: string]: any }>({
-  payload,
-}: PayloadAction<GetTokenMapParams<R>>) {
-  try {
-    const { tokensMap, marketMap, pairs, marketArr, tokenArr } = payload;
-    // @ts-ignore
-    const { data } = yield call(getTokenMapApi, {
-      tokensMap,
-      pairs,
-      marketArr,
-      tokenArr,
-    });
+export function* getPostsSaga<R extends { [ key: string ]: any }>({payload}: PayloadAction<GetTokenMapParams<R>>) {
+    try {
+        const {tokensMap, marketMap, pairs, marketArr, tokenArr} = payload
+        // @ts-ignore
+        const {data} = yield call(getTokenMapApi, {tokensMap, pairs, marketArr, tokenArr});
 
-    yield put(getTokenMapStatus({ ...data, marketMap }));
-  } catch (err) {
-    yield put(getTokenMapStatus(err));
-  }
+        yield put(getTokenMapStatus({...data, marketMap}));
+    } catch (err) {
+        yield put(getTokenMapStatus(err));
+    }
 }
 
 export function* tokenInitSaga() {
-  yield all([takeLatest(getTokenMap, getPostsSaga)]);
+    yield all([takeLatest(getTokenMap, getPostsSaga)]);
 }
 
+
 export const tokenSaga = [
-  fork(tokenInitSaga),
-  // fork(tokenPairsSaga),
-];
+    fork(tokenInitSaga),
+    // fork(tokenPairsSaga),
+]
 
 // export function* getPairsSaga({payload}:PayloadAction<{tokenPairs: TokenPairs }>) {
 //     try {
@@ -101,3 +95,5 @@ export const tokenSaga = [
 // export function* tokenPairsSaga() {
 //     yield all([takeLatest(getTokenPairMap, getPairsSaga)]);
 // }
+
+
