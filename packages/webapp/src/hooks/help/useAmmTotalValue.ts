27976f04bfb8ec81eae8e0e00a460d17d547fba9
windatang <windatang@gmail.com>
2021-12-20 21:17:12 +0800

    token price issue update

--- packages/webapp/src/hooks/help/useAmmTotalValue.ts
+++ packages/webapp/src/hooks/help/useAmmTotalValue.ts
@@ -1,53 +1,45 @@
-import React from 'react'
-import { LoopringAPI } from 'api_wrapper'
-import { useTokenMap, } from 'stores/token'
-import { useWalletLayer2 } from 'stores/walletLayer2'
-import { volumeToCount } from 'hooks/help'
-import store from 'stores'
-import { Currency } from '@loopring-web/loopring-sdk';
+import React from "react";
+import { LoopringAPI } from "api_wrapper";
+import { useTokenMap } from "stores/token";
+import { useWalletLayer2 } from "stores/walletLayer2";
+import { volumeToCount } from "hooks/help";
+import store from "stores";
+import { Currency } from "@loopring-web/loopring-sdk";
+import { useTokenPrices } from "../../stores/tokenPrices";
 
 export const useAmmTotalValue = () => {
-    const {addressIndex} = useTokenMap();
-    const {walletLayer2} = useWalletLayer2();
-    const {forex} = store.getState().system
+  const { walletLayer2 } = useWalletLayer2();
+  const { tokenPrices } = useTokenPrices();
+  const { forex } = store.getState().system;
 
-    const getLpTokenPrice = React.useCallback(async (market: string) => {
-        if (addressIndex && LoopringAPI.walletAPI) {
-            const result = await LoopringAPI.walletAPI.getLatestTokenPrices()
-            const list = Object.entries(result.tokenPrices).map(([addr, price]) => ({
-                addr,
-                price,
-            }))
-            const address = Object.entries(addressIndex).find(([_, token]) => token === market)?.[ 0 ]
-            if (address && !!list.length) {
-                return list.find((o) => o.addr === address)?.price
-            }
-            return undefined
-        }
-        return undefined
-    }, [addressIndex])
+  type GetAmmLiquidityProps = {
+    market: string;
+    balance?: number;
+    currency?: Currency;
+  };
 
-    type GetAmmLiquidityProps = {
-        market: string;
-        balance?: number;
-        currency?: Currency
-    }
+  const getAmmLiquidity = React.useCallback(
+    ({ market, balance, currency = Currency.usd }: GetAmmLiquidityProps) => {
+      const price = tokenPrices && tokenPrices[market];
+      let curBalance = 0;
+      if (balance) {
+        curBalance = balance;
+      } else {
+        // if balance is not given, use walletl2 total lp token balance instead
+        curBalance = Number(
+          Object.entries(walletLayer2 || {}).find(
+            ([token]) => token === market
+          )?.[1].total || 0
+        );
+      }
+      const formattedBalance = volumeToCount(market, curBalance);
+      const unit = currency && currency === Currency.cny ? forex : 1;
+      return (price || 0) * (formattedBalance || 0) * (unit as number);
+    },
+    [walletLayer2, forex]
+  );
 
-    const getAmmLiquidity = React.useCallback(async ({market, balance, currency =  Currency.usd}: GetAmmLiquidityProps) => {
-        const price = await getLpTokenPrice(market)
-        let curBalance = 0
-        if (balance) {
-            curBalance = balance
-        } else {
-            // if balance is not given, use walletl2 total lp token balance instead
-            curBalance = Number(Object.entries(walletLayer2 || {}).find(([token]) => token === market)?.[ 1 ].total || 0)
-        }
-        const formattedBalance = volumeToCount(market, curBalance)
-        const unit = currency && currency === Currency.cny ? forex : 1
-        return (price || 0) * (formattedBalance || 0) * (unit as number)
-    }, [walletLayer2, getLpTokenPrice, forex])
-
-    return {
-        getAmmLiquidity
-    }
-}
+  return {
+    getAmmLiquidity,
+  };
+};
--- packages/webapp/src/stores/tokenPrices/saga.ts
+++ packages/webapp/src/stores/tokenPrices/saga.ts
@@ -1,52 +1,50 @@
 import { all, call, fork, put, takeLatest } from "redux-saga/effects";
-import { getTokenPrices, getTokenPricesStatus } from './reducer';
-import { LoopringAPI } from 'api_wrapper'
-import store from '../index';
-import { TokenPrices } from './interface';
-import { PayloadAction } from '@reduxjs/toolkit';
+import { getTokenPrices, getTokenPricesStatus } from "./reducer";
+import { LoopringAPI } from "api_wrapper";
+import store from "../index";
+import { TokenPrices } from "./interface";
+import { PayloadAction } from "@reduxjs/toolkit";
 
+const getTokenPricesApi = async <R extends { [key: string]: any }>() => {
+  const { addressIndex } = store.getState().tokenMap;
+  if (LoopringAPI?.walletAPI && addressIndex) {
+    const { tokenPrices: _tokenPrices, raw_data } =
+      await LoopringAPI?.walletAPI.getLatestTokenPrices();
+    let { __timer__ } = store.getState().tokenPrices;
+    const tokenPrices: { [key in keyof R]: number } = Reflect.ownKeys(
+      _tokenPrices
+    ).reduce((prev, address) => {
+      const symbol = addressIndex[address.toString().toLowerCase()];
+      return { ...prev, [symbol]: _tokenPrices[address as keyof R] };
+    }, {} as { [key in keyof R]: number });
+    __timer__ = (() => {
+      if (__timer__ && __timer__ !== -1) {
+        clearInterval(__timer__);
+      }
+      return setInterval(async () => {
+        if (!LoopringAPI.walletAPI) {
+          return undefined;
+        }
+        store.dispatch(getTokenPrices({}));
+      }, 900000); //15*60*1000 //900000
+    })();
+    return { tokenPrices, __timer__, __rawConfig__: raw_data };
+  }
+};
 
-const getTokenPricesApi = async <R extends { [ key: string ]: any }>() => {
-    const {addressIndex} = store.getState().tokenMap;
-    if(LoopringAPI?.walletAPI && addressIndex) {
-        const {tokenPrices:_tokenPrices,raw_data} = await LoopringAPI?.walletAPI.getLatestTokenPrices()
-        let {__timer__} = store.getState().tokenPrices;
-        const tokenPrices:{[key in keyof R]:number} = Reflect.ownKeys(_tokenPrices).reduce((prev, address) => {
-            const symbol = addressIndex[ address as string ];
-            return {...prev, [ symbol ]: _tokenPrices[ address as keyof R]}
-        }, {} as {[key in keyof R]:number})
-        __timer__ = (() => {
-            if (__timer__ && __timer__ !== -1) {
-                clearInterval(__timer__)
-            }
-            return setInterval(async () => {
-
-                if (!LoopringAPI.walletAPI) {
-                    return undefined
-                }
-                store.dispatch(getTokenPrices({}))
-            }, 900000)    //15*60*1000 //900000
-        })()
-        return {tokenPrices, __timer__,__rawConfig__:raw_data}
-    }
+export function* getPostsSaga({ payload }: PayloadAction<undefined>) {
+  try {
+    const { tokenPrices, __timer__, __rawConfig__ } = yield call(
+      getTokenPricesApi
+    );
+    yield put(getTokenPricesStatus({ tokenPrices, __timer__, __rawConfig__ }));
+  } catch (err) {
+    yield put(getTokenPricesStatus(err));
+  }
 }
-
-
-export function* getPostsSaga({payload}: PayloadAction<undefined>) {
-    try {
-        const {tokenPrices, __timer__,__rawConfig__} = yield call(getTokenPricesApi);
-        yield put(getTokenPricesStatus({tokenPrices, __timer__,__rawConfig__}));
-    } catch (err) {
-        yield put(getTokenPricesStatus(err));
-    }
-}
-
 
 export function* tokenPricesInitSaga() {
-    yield all([takeLatest(getTokenPrices, getPostsSaga)]);
+  yield all([takeLatest(getTokenPrices, getPostsSaga)]);
 }
 
-export const tokenPricesSaga = [
-    fork(tokenPricesInitSaga),
-]
-
+export const tokenPricesSaga = [fork(tokenPricesInitSaga)];
