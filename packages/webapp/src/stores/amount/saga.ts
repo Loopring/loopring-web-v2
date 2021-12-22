304bc8e7881b5c3b43c17fa118b68c48b4266ea3
Winda <windatang@gmail.com>
2021-12-22 13:57:01 +0800

    api loop log (#314)
    monitor api

--- packages/webapp/src/stores/amount/saga.ts
+++ packages/webapp/src/stores/amount/saga.ts
@@ -1,101 +1,105 @@
-import { all, call, fork, put, takeLatest } from "redux-saga/effects"
-import { getAmount, getAmountStatus, resetAmount } from './reducer'
+import { all, call, fork, put, takeLatest } from "redux-saga/effects";
+import { getAmount, getAmountStatus, resetAmount } from "./reducer";
 
-import store from '../index';
-import { LoopringAPI } from 'api_wrapper';
-import * as sdk from '@loopring-web/loopring-sdk';
+import store from "../index";
+import { LoopringAPI } from "api_wrapper";
+import * as sdk from "@loopring-web/loopring-sdk";
+import { myLog } from "@loopring-web/common-resources";
 
-const getAmountApi = async <R extends { [ key: string ]: any }>(market: string): Promise<{
-    newAmountMap: object | undefined,
-    __timer__: NodeJS.Timer | -1
+const getAmountApi = async <R extends { [key: string]: any }>(
+  market: string
+): Promise<{
+  newAmountMap: object | undefined;
+  __timer__: NodeJS.Timer | -1;
 }> => {
+  const { accountId, apiKey } = store.getState().account;
+  let { __timerMap__ } = store.getState().amountMap;
+  let __timer__ =
+    __timerMap__ && __timerMap__[market] ? __timerMap__[market] : -1;
 
-    const {accountId, apiKey} = store.getState().account
-    let {__timerMap__} = store.getState().amountMap;
-    let __timer__ = (__timerMap__ && __timerMap__[ market ]) ? __timerMap__[ market ] : -1;
+  if (LoopringAPI.userAPI && accountId && apiKey) {
+    if (__timer__ && __timer__ !== -1) {
+      clearInterval(__timer__);
+    }
+    __timer__ = setInterval(async () => {
+      store.dispatch(getAmount(market));
+    }, 1000 * 15 * 60); //   //
 
-    if (LoopringAPI.userAPI && accountId && apiKey) {
-        // __timer__ = ((__timer__, market) => {
-        if (__timer__ && __timer__ !== -1) {
-            clearInterval(__timer__);
-        }
-        __timer__ = setInterval(async () => {
-            store.dispatch(getAmount(market))
-        }, 1000 * 15 * 60)//   //
+    const req: sdk.GetMinimumTokenAmtRequest = {
+      accountId: accountId,
+      market: market,
+    };
+    myLog("loop get getMinimumTokenAmt");
 
-        // })(__timer__, market);
-        const req: sdk.GetMinimumTokenAmtRequest = {
-            accountId: accountId,
-            market: market,
-        }
-        const {amountMap: _pairMap} = await LoopringAPI.userAPI.getMinimumTokenAmt(req, apiKey)
-        const reqAmm: sdk.GetMinimumTokenAmtRequest = {
-            accountId: accountId,
-            market: 'AMM-' + market,
-        }
-        const {amountMap: _pairMapAmm} = await LoopringAPI.userAPI.getMinimumTokenAmt(reqAmm, apiKey)
-        return {newAmountMap: {[ market ]: _pairMap, [ 'AMM-' + market ]: _pairMapAmm}, __timer__}
+    const { amountMap: _pairMap } =
+      await LoopringAPI.userAPI.getMinimumTokenAmt(req, apiKey);
+    const reqAmm: sdk.GetMinimumTokenAmtRequest = {
+      accountId: accountId,
+      market: "AMM-" + market,
+    };
+    const { amountMap: _pairMapAmm } =
+      await LoopringAPI.userAPI.getMinimumTokenAmt(reqAmm, apiKey);
+    return {
+      newAmountMap: { [market]: _pairMap, ["AMM-" + market]: _pairMapAmm },
+      __timer__,
+    };
+  } else {
+    if (__timer__ && __timer__ !== -1) {
+      clearInterval(__timer__);
+    }
+    return Promise.resolve({ newAmountMap: {}, __timer__: -1 });
+  }
+};
 
+export function* getPostsSaga({ payload: { market } }: any) {
+  try {
+    const { amountMap, __timerMap__ } = store.getState().amountMap;
+    if (market) {
+      const { newAmountMap, __timer__ } = yield call(getAmountApi, market);
+      yield put(
+        getAmountStatus({
+          amountMap: { ...amountMap, ...newAmountMap },
+          __timerMap__: { ...__timerMap__, [market]: __timer__ },
+        })
+      );
     } else {
+      yield put(getAmountStatus({ amountMap, __timerMap__ }));
+    }
+  } catch (err) {
+    yield put(getAmountStatus(err));
+  }
+}
+
+export function* getResetsSaga({ payload }: any) {
+  try {
+    let { __timerMap__ } = store.getState().amountMap;
+    if (__timerMap__) {
+      Reflect.ownKeys(__timerMap__).map((market) => {
+        let __timer__ = __timerMap__ && __timerMap__[market as string];
         if (__timer__ && __timer__ !== -1) {
-            clearInterval(__timer__);
+          clearInterval(__timer__);
         }
-        return Promise.resolve({newAmountMap: {}, __timer__: -1})
+      });
     }
 
-}
-
-export function* getPostsSaga({payload: {market}}: any) {
-    try {
-
-        const {amountMap, __timerMap__} = store.getState().amountMap;
-        if (market) {
-            const {newAmountMap, __timer__} = yield call(getAmountApi, market)
-            yield put(getAmountStatus({
-                amountMap:
-                    {...amountMap, ...newAmountMap},
-                __timerMap__:
-                    {...__timerMap__, [ market ]: __timer__}
-            }));
-        } else {
-            yield put(getAmountStatus({amountMap, __timerMap__}))
-        }
-
-    } catch (err) {
-        yield put(getAmountStatus(err));
-    }
-}
-
-export function* getResetsSaga({payload}: any) {
-    try {
-        let {__timerMap__} = store.getState().amountMap;
-        if (__timerMap__) {
-            Reflect.ownKeys(__timerMap__).map((market) => {
-                let __timer__ = __timerMap__ && __timerMap__[ market as string ];
-                if (__timer__ && __timer__ !== -1) {
-                    clearInterval(__timer__);
-                }
-            })
-        }
-
-
-        yield put(getAmountStatus({amountMap: undefined, __timerMap__: undefined}));
-    } catch (err) {
-        yield put(getAmountStatus(err));
-    }
+    yield put(
+      getAmountStatus({ amountMap: undefined, __timerMap__: undefined })
+    );
+  } catch (err) {
+    yield put(getAmountStatus(err));
+  }
 }
 
 function* amountSaga() {
-    yield all([takeLatest(getAmount, getPostsSaga)]);
+  yield all([takeLatest(getAmount, getPostsSaga)]);
 }
 
 function* restAmountSaga() {
-    yield all([takeLatest(resetAmount, getResetsSaga)]);
+  yield all([takeLatest(resetAmount, getResetsSaga)]);
 }
 
 export const amountForks = [
-    fork(amountSaga),
-    fork(restAmountSaga),
-    // fork(amountsSaga),
-]
- 
+  fork(amountSaga),
+  fork(restAmountSaga),
+  // fork(amountsSaga),
+];
