0ab3c7314d3b628d677be977b1b5330e6d0d6643
windatang <windatang@gmail.com>
2021-12-30 19:53:30 +0800

    #312 CF

--- packages/webapp/src/services/account/activateAccount.ts
+++ packages/webapp/src/services/account/activateAccount.ts
@@ -1,160 +1,186 @@
-import { updateAccountStatus } from '../../stores/account';
-import { AccountStep, setShowAccount, setShowConnect } from '@loopring-web/component-lib';
-import store from '../../stores';
-import { AccountStatus, FeeInfo } from '@loopring-web/common-resources';
+import { updateAccountStatus } from "../../stores/account";
+import {
+  AccountStep,
+  setShowAccount,
+  setShowConnect,
+} from "@loopring-web/component-lib";
+import store from "../../stores";
+import { AccountStatus, FeeInfo } from "@loopring-web/common-resources";
 import { myLog } from "@loopring-web/common-resources";
-import { LoopringAPI } from 'api_wrapper';
-import { connectProvides } from '@loopring-web/web3-provider';
-import * as sdk from '@loopring-web/loopring-sdk'
-import { ActionResult, ActionResultCode, DAYS, } from 'defs/common_defs';
-import { getTimestampDaysLater } from 'utils/dt_tools';
+import { LoopringAPI } from "api_wrapper";
+import { connectProvides } from "@loopring-web/web3-provider";
+import * as sdk from "@loopring-web/loopring-sdk";
+import { ActionResult, ActionResultCode, DAYS } from "defs/common_defs";
+import { getTimestampDaysLater } from "utils/dt_tools";
 
-export async function activeAccount({reason, shouldShow}: { reason: any, shouldShow: boolean }) {
-    const account = store.getState().account;
-    // const {exchangeInfo} = store.getState().system;
-    if (reason?.response?.data?.resultInfo?.code === 100001) {
-        // deposited, but need update account
-        myLog('SignAccount')
-        store.dispatch(setShowConnect({isShow: false}));
-        store.dispatch(setShowAccount({isShow: true, step: AccountStep.UpdateAccount}));
-        store.dispatch(updateAccountStatus({readyState: AccountStatus.NOT_ACTIVE}));
-
-    } else {
-        // need to deposit.
-        let activeDeposit = localStorage.getItem('activeDeposit');
-        if (activeDeposit) {
-            activeDeposit = JSON.stringify(activeDeposit);
-        }
-        if (activeDeposit && activeDeposit[ account.accAddress ]) {
-            myLog('DEPOSITING')
-            store.dispatch(setShowConnect({isShow: false}));
-            store.dispatch(setShowAccount({isShow: shouldShow, step: AccountStep.Deposit_Submit}));
-            store.dispatch(updateAccountStatus({readyState: AccountStatus.DEPOSITING}));
-            // store.dispatch(statusAccountUnset(undefined))
-        } else {
-            myLog('NO_ACCOUNT')
-            setShowConnect({isShow: false});
-            setShowAccount({isShow: shouldShow, step: AccountStep.NoAccount});
-            store.dispatch(updateAccountStatus({readyState: AccountStatus.NO_ACCOUNT}));
-            // store.dispatch(statusAccountUnset(undefined));
-        }
+export async function activeAccount({
+  reason,
+  shouldShow,
+}: {
+  reason: any;
+  shouldShow: boolean;
+}) {
+  const account = store.getState().account;
+  // const {exchangeInfo} = store.getState().system;
+  if (reason?.response?.data?.resultInfo?.code === 100001) {
+    // deposited, but need update account
+    myLog("SignAccount");
+    store.dispatch(setShowConnect({ isShow: false }));
+    store.dispatch(
+      setShowAccount({ isShow: true, step: AccountStep.UpdateAccount })
+    );
+    store.dispatch(
+      updateAccountStatus({ readyState: AccountStatus.NOT_ACTIVE })
+    );
+  } else {
+    // need to deposit.
+    let activeDeposit = localStorage.getItem("activeDeposit");
+    if (activeDeposit) {
+      activeDeposit = JSON.stringify(activeDeposit);
     }
+    if (activeDeposit && activeDeposit[account.accAddress]) {
+      myLog("DEPOSITING");
+      store.dispatch(setShowConnect({ isShow: false }));
+      store.dispatch(
+        setShowAccount({ isShow: shouldShow, step: AccountStep.Deposit_Submit })
+      );
+      store.dispatch(
+        updateAccountStatus({ readyState: AccountStatus.DEPOSITING })
+      );
+      // store.dispatch(statusAccountUnset(undefined))
+    } else {
+      myLog("NO_ACCOUNT");
+      setShowConnect({ isShow: false });
+      setShowAccount({ isShow: shouldShow, step: AccountStep.NoAccount });
+      store.dispatch(
+        updateAccountStatus({ readyState: AccountStatus.NO_ACCOUNT })
+      );
+      // store.dispatch(statusAccountUnset(undefined));
+    }
+  }
 }
 
-export async function updateAccountFromServer({isHWAddr, feeInfo, isReset, }: { isHWAddr: boolean, feeInfo?: FeeInfo, isReset?: boolean }) {
+export async function updateAccountFromServer({
+  isHWAddr,
+  feeInfo,
+  isReset,
+}: {
+  isHWAddr: boolean;
+  feeInfo?: FeeInfo;
+  isReset?: boolean;
+}) {
+  const system = store.getState().system;
+  const account = store.getState().account;
 
-    const system = store.getState().system
-    const account = store.getState().account
+  let eddsaKey = isReset ? undefined : account.eddsaKey;
 
-    let eddsaKey = isReset ? undefined : account.eddsaKey
+  myLog("before check!", account);
 
-    myLog('before check!', account)
+  let result: ActionResult = { code: ActionResultCode.NoError };
 
-    let result: ActionResult = {code: ActionResultCode.NoError,}
+  try {
+    if (
+      LoopringAPI.userAPI &&
+      LoopringAPI.exchangeAPI &&
+      system.exchangeInfo &&
+      connectProvides.usedWeb3 &&
+      account &&
+      system.chainId !== "unknown" &&
+      account.connectName !== "unknown"
+    ) {
+      const { accInfo } = await LoopringAPI.exchangeAPI.getAccount({
+        owner: account.accAddress,
+      });
 
-    try {
+      if (accInfo?.owner && accInfo?.accountId) {
+        const connectName = account.connectName as sdk.ConnectorNames;
 
-        if (LoopringAPI.userAPI && LoopringAPI.exchangeAPI && system.exchangeInfo && connectProvides.usedWeb3 && account
-            && system.chainId !== 'unknown' && account.connectName !== 'unknown') {
+        try {
+          if (!eddsaKey) {
+            myLog("no eddsaKey ！!");
+            eddsaKey = await sdk.generateKeyPair({
+              web3: connectProvides.usedWeb3,
+              address: accInfo.owner,
+              exchangeAddress: system.exchangeInfo.exchangeAddress,
+              keyNonce: accInfo.nonce,
+              walletType: connectName,
+              chainId: system.chainId as any,
+            });
+            myLog("no eddsaKey! after generateKeyPair");
+          }
 
-            const {accInfo} = (await LoopringAPI.exchangeAPI.getAccount({
-                owner: account.accAddress
-            }))
+          try {
+            let tokenId = 0;
 
-            if (accInfo?.owner && accInfo?.accountId) {
+            let feeVol = "0";
 
-                const connectName = account.connectName as sdk.ConnectorNames
-
-                try {
-                    if (!eddsaKey) {
-                        myLog('no eddsaKey ！!')
-                        eddsaKey = await sdk
-                            .generateKeyPair({
-                                web3: connectProvides.usedWeb3,
-                                address: accInfo.owner,
-                                exchangeAddress: system.exchangeInfo.exchangeAddress,
-                                keyNonce: accInfo.nonce,
-                                walletType: connectName,
-                            }
-                            )
-                        myLog('no eddsaKey! after generateKeyPair')
-                    }
-
-                    try {
-
-                        let tokenId = 0
-
-                        let feeVol = '0'
-
-                        if (feeInfo) {
-                            tokenId = feeInfo.__raw__.tokenId
-                            feeVol = feeInfo.__raw__.feeRaw
-                        }
-
-                        const request: sdk.UpdateAccountRequestV3 = {
-                            exchange: system.exchangeInfo.exchangeAddress,
-                            owner: accInfo.owner,
-                            accountId: accInfo.accountId,
-                            publicKey: {x: eddsaKey.formatedPx, y: eddsaKey.formatedPy,},
-                            maxFee: {tokenId, volume: feeVol},
-                            validUntil: getTimestampDaysLater(DAYS),
-                            nonce: accInfo.nonce as number,
-                        }
-
-                        myLog('updateAccountFromServer req:', request)
-
-                        const response = await LoopringAPI.userAPI.updateAccount({
-                            request,
-                            web3: connectProvides.usedWeb3,
-                            chainId: system.chainId,
-                            walletType: connectName,
-                            isHWAddr,
-                        })
-
-                        myLog('updateAccountResponse:', response)
-
-                        if ((response as sdk.ErrorMsg)?.errMsg) {
-                            result.code = ActionResultCode.UpdateAccoutError
-                            result.data = {
-                                eddsaKey,
-                                ...response,
-                            }
-                        } else if ((response as sdk.TX_HASH_RESULT<sdk.TX_HASH_API>)?.resultInfo ) {
-                            result.code = ActionResultCode.UpdateAccoutError
-                            result.data = {
-                                eddsaKey,
-                                ...response,
-                            }
-                        } else {
-                            result.data = {
-                                response,
-                                eddsaKey,
-                            }
-                        }
-
-                    } catch (reason) {
-                        result.code = ActionResultCode.UpdateAccoutError
-                        result.data = reason
-                        sdk.dumpError400(reason)
-                    }
-
-                } catch (reason) {
-
-                    myLog('GenEddsaKeyError!!!!!! ')
-
-                    result.code = ActionResultCode.GenEddsaKeyError
-                    result.data = reason
-                    sdk.dumpError400(reason)
-                }
+            if (feeInfo) {
+              tokenId = feeInfo.__raw__.tokenId;
+              feeVol = feeInfo.__raw__.feeRaw;
             }
+
+            const request: sdk.UpdateAccountRequestV3 = {
+              exchange: system.exchangeInfo.exchangeAddress,
+              owner: accInfo.owner,
+              accountId: accInfo.accountId,
+              publicKey: { x: eddsaKey.formatedPx, y: eddsaKey.formatedPy },
+              maxFee: { tokenId, volume: feeVol },
+              validUntil: getTimestampDaysLater(DAYS),
+              nonce: accInfo.nonce as number,
+            };
+
+            myLog("updateAccountFromServer req:", request);
+
+            const response = await LoopringAPI.userAPI.updateAccount({
+              request,
+              web3: connectProvides.usedWeb3,
+              chainId: system.chainId,
+              walletType: connectName,
+              isHWAddr,
+            });
+
+            myLog("updateAccountResponse:", response);
+
+            if ((response as sdk.ErrorMsg)?.errMsg) {
+              result.code = ActionResultCode.UpdateAccoutError;
+              result.data = {
+                eddsaKey,
+                ...response,
+              };
+            } else if (
+              (response as sdk.TX_HASH_RESULT<sdk.TX_HASH_API>)?.resultInfo
+            ) {
+              result.code = ActionResultCode.UpdateAccoutError;
+              result.data = {
+                eddsaKey,
+                ...response,
+              };
+            } else {
+              result.data = {
+                response,
+                eddsaKey,
+              };
+            }
+          } catch (reason) {
+            result.code = ActionResultCode.UpdateAccoutError;
+            result.data = reason;
+            sdk.dumpError400(reason);
+          }
+        } catch (reason) {
+          myLog("GenEddsaKeyError!!!!!! ");
+
+          result.code = ActionResultCode.GenEddsaKeyError;
+          result.data = reason;
+          sdk.dumpError400(reason);
         }
-    } catch (reason) {
-
-        myLog('other error!!!!!!! ')
-        result.code = ActionResultCode.GetAccError
-        result.data = reason
-        sdk.dumpError400(reason)
+      }
     }
+  } catch (reason) {
+    myLog("other error!!!!!!! ");
+    result.code = ActionResultCode.GetAccError;
+    result.data = reason;
+    sdk.dumpError400(reason);
+  }
 
-    return result
+  return result;
 }
--- packages/webapp/src/services/account/unlockAccount.ts
+++ packages/webapp/src/services/account/unlockAccount.ts
@@ -1,23 +1,22 @@
-import { connectProvides } from '@loopring-web/web3-provider';
-import { LoopringAPI } from 'api_wrapper';
-import store from 'stores';
-import { accountServices } from './accountServices';
+import { connectProvides } from "@loopring-web/web3-provider";
+import { LoopringAPI } from "api_wrapper";
+import store from "stores";
+import { accountServices } from "./accountServices";
 import { myLog } from "@loopring-web/common-resources";
-import { checkErrorInfo } from 'hooks/useractions/utils';
+import { checkErrorInfo } from "hooks/useractions/utils";
 
-import * as sdk from '@loopring-web/loopring-sdk'
+import * as sdk from "@loopring-web/loopring-sdk";
 
 export async function unlockAccount() {
   const account = store.getState().account;
-  const {exchangeInfo} = store.getState().system;
-  accountServices.sendSign()
+  const { exchangeInfo, chainId } = store.getState().system;
+  accountServices.sendSign();
 
-  myLog('unlockAccount account:', account)
+  myLog("unlockAccount account:", account);
 
   if (exchangeInfo && LoopringAPI.userAPI && account.nonce !== undefined) {
     try {
-
-      const connectName = account.connectName as sdk.ConnectorNames
+      const connectName = account.connectName as sdk.ConnectorNames;
 
       const eddsaKey = await sdk.generateKeyPair({
         web3: connectProvides.usedWeb3,
@@ -25,36 +24,45 @@ export async function unlockAccount() {
         exchangeAddress: exchangeInfo.exchangeAddress,
         keyNonce: account.nonce - 1,
         walletType: connectName,
-      })
+        chainId: chainId as any,
+        accountId: account.accountId,
+      });
 
-      // myLog('unlockAccount eddsaKey:', eddsaKey)
+      myLog("unlockAccount eddsaKey:", eddsaKey);
 
-      const {apiKey, raw_data,} = (await LoopringAPI.userAPI.getUserApiKey({
-        accountId: account.accountId
-      }, eddsaKey.sk))
+      const { apiKey, raw_data } = await LoopringAPI.userAPI.getUserApiKey(
+        {
+          accountId: account.accountId,
+        },
+        eddsaKey.sk
+      );
 
-      myLog('unlockAccount raw_data:', raw_data)
+      myLog("unlockAccount raw_data:", raw_data);
 
       if (!apiKey && raw_data.resultInfo) {
-        myLog('try to sendErrorUnlock....')
-        accountServices.sendErrorUnlock()
+        myLog("try to sendErrorUnlock....");
+        accountServices.sendErrorUnlock();
       } else {
-        myLog('try to sendAccountSigned....')
-        accountServices.sendAccountSigned({accountId: account.accountId, nonce: account.nonce, apiKey, eddsaKey})
+        myLog("try to sendAccountSigned....");
+        accountServices.sendAccountSigned({
+          accountId: account.accountId,
+          nonce: account.nonce,
+          apiKey,
+          eddsaKey,
+        });
       }
     } catch (e) {
-      myLog('unlockAccount e:', e)
+      myLog("unlockAccount e:", e);
 
-      const errType = checkErrorInfo(e, true)
+      const errType = checkErrorInfo(e, true);
       switch (errType) {
         case sdk.ConnectorError.USER_DENIED:
-          accountServices.sendSignDeniedByUser()
-          return
+          accountServices.sendSignDeniedByUser();
+          return;
         default:
-          break
+          break;
       }
-      accountServices.sendErrorUnlock()
+      accountServices.sendErrorUnlock();
     }
-
   }
 }
