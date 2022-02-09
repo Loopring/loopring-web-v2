978aff5a29858a0e5b7ea6355f61b162db138874
windatang <windatang@gmail.com>
2022-02-10 03:59:49 +0800

    ThirdPart Plugin( Chrome extension for partner ) #422

--- packages/common-resources/static-resources/src/i18n/zh_CN/error.ts
+++ packages/common-resources/static-resources/src/i18n/zh_CN/error.ts
@@ -4,7 +4,12 @@ export default {
   errorLoading: "不要着急, 不要着急马上就好",
   error404: "四〇四! 这是页面不存在呢.",
   errorMaintain: "系统维护中...",
-  errorMessageNoNetwork: "Oops! Something went wrong at service.",
+  errorUnknown: "Unknown Error",
+  errorOnFromSubmit: "Error on submit Info!",
+  errorWrongAccount: "Error on account Info!",
+  errorWrongApikey: "Error on signature or no Apikey",
+  errorAboutFee: "Fee token is not Enough or wrong",
   errorDisableOtherWalletForCurrent:
     "Global ethereum is not {{name}}, Please disable other Wallet Plugin",
+  errorNotInstallGME: "GameStop Plugin is not install or disabled",
 };
