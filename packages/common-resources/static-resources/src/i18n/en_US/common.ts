c65809f0d7b4481119d6ae7f887e05c44da0b82b
windatang <windatang@gmail.com>
2022-02-27 00:21:18 +0800

    Guardian

--- packages/common-resources/static-resources/src/i18n/en_US/common.ts
+++ packages/common-resources/static-resources/src/i18n/en_US/common.ts
@@ -573,15 +573,15 @@ export default {
   labelTxGuardianGUARDIAN_CONFIRM_ADDITION: "GUARDIAN CONFIRM ADDITION",
   labelTxGuardianGUARDIAN_REJECT_ADDITION: "GUARDIAN REJECT ADDITION",
   labelTxGuardianGUARDIAN_APPROVE: "GUARDIAN APPROVE",
-  labelTxGuardianAPPROVE_RECOVER: "APPROVE RECOVER", // RECOVER  16
-  labelTxGuardianAPPROVE_TRANSFER: "APPROVE TRANSFER", // APPROVE TRANSFER 18
-  labelTxGuardianAPPROVE_TOKEN_APPROVE: "APPROVE TOKEN APPROVE", // 23
-  labelTxGuardianADD_GUARDIAN_WA: "ADD GUARDIAN WA", // 34
-  labelTxGuardianREMOVE_GUARDIAN_WA: "REMOVE GUARDIAN WA", // 35
-  labelTxGuardianUNLOCK_WALLET_WA: "UNLOCK WALLET WA", // 37
-  labelTxGuardianRESET_GUARDIANS_WA: "RESET GUARDIANS WA", // 200
-  labelTxGuardian_recovery: "recovery",
-  labelTxGuardian_transfer: "transfer",
+  labelTxGuardianAPPROVE_RECOVER: "RECOVER WALLET", // RECOVER  16
+  labelTxGuardianAPPROVE_TRANSFER: "OVER DAILY QUOTA TRANSFER", // APPROVE TRANSFER 18
+  labelTxGuardianAPPROVE_TOKEN_APPROVE: "TOKEN ACCESS", // 23
+  labelTxGuardianADD_GUARDIAN_WA: "ADD GUARDIAN", // 34
+  labelTxGuardianREMOVE_GUARDIAN_WA: "REMOVE GUARDIAN", // 35
+  labelTxGuardianUNLOCK_WALLET_WA: "UNLOCK WALLET", // 37
+  labelTxGuardianRESET_GUARDIANS_WA: "RESET GUARDIANS", // 200
+  labelTxGuardian_recovery: "recovery wallet",
+  labelTxGuardian_transfer: "over daily quota transfer",
   labelTxGuardian_add_guardian: "add guardian",
   labelTxGuardian_remove_guardian: "remove guardian",
   labelTxGuardian_unlock_wallet: "unlock wallet",
