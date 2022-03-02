28c97a81272531f6f2bf75c940a398590a00cadf
windatang <windatang@gmail.com>
2022-03-03 00:10:38 +0800

    imtoken dapp #343

--- packages/component-lib/src/components/tableList/transactionsTable/TransactionTable.tsx
+++ packages/component-lib/src/components/tableList/transactionsTable/TransactionTable.tsx
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 import { Box, Modal, Typography } from "@mui/material";
-import { TFunction, WithTranslation, withTranslation } from "react-i18next";
+import { WithTranslation, withTranslation } from "react-i18next";
 import moment from "moment";
 import { Column, Table, TablePagination } from "../../basic-lib";
 import {
@@ -12,8 +12,10 @@ import {
   getShortAddr,
   getValuePrecisionThousand,
   TableType,
+  TransferIcon,
   WaitingIcon,
   WarningIcon,
+  WithdrawIcon,
 } from "@loopring-web/common-resources";
 import { Filter } from "./components/Filter";
 import { TxnDetailPanel, TxnDetailProps } from "./components/modal";
@@ -86,7 +88,9 @@ const TableStyled = styled(Box)`
 
   .rdg {
     --template-columns: 120px auto auto auto 120px 150px !important;
-
+    @media only screen and (max-width: 768px) {
+      --template-columns: 60% 40% !important;
+    }
     .rdgCellCenter {
       height: 100%;
       justify-content: center;
@@ -155,6 +159,7 @@ export const TransactionTable = withTranslation(["tables", "common"])(
       showloading,
       etherscanBaseUrl,
       accAddress,
+      t,
     } = props;
     const { isMobile } = useSettings();
     const [page, setPage] = React.useState(1);
@@ -435,51 +440,43 @@ export const TransactionTable = withTranslation(["tables", "common"])(
           },
         },
       ],
-      [handleTxnDetail, etherscanBaseUrl]
+      [handleTxnDetail, etherscanBaseUrl, t]
     );
 
     const getColumnMobileTransaction = React.useCallback(
-      (t: TFunction): Column<any, unknown>[] => [
-        // {
-        //   key: "side",
-        //   name: t("labelTxSide"),
-        //   formatter: ({ row }) => {
-        //     const value = row["side"];
-        //     const renderValue =
-        //       value === TransactionTradeTypes.deposit
-        //         ? t("labelDeposit")
-        //         : value === TransactionTradeTypes.transfer
-        //         ? t("labelTransfer")
-        //         : t("labelWithdraw");
-        //     return <Box className="rdg-cell-value">{renderValue}</Box>;
-        //   },
-        // },
+      (): Column<any, unknown>[] => [
         {
           key: "amount",
-          name: t("labelTxAmount"),
-          headerCellClass: "textAlignRight",
+          name: t("labelTxAmount") + " / " + t("labelTxFee"),
+          cellClass: "textAlignRight",
+          headerCellClass: "textAlignLeft",
           formatter: ({ row }) => {
             const { unit, value } = row["amount"];
             const hasValue = Number.isFinite(value);
+            const side =
+              row.side === TransactionTradeTypes.deposit
+                ? t("labelDeposit")
+                : row.side === TransactionTradeTypes.transfer
+                ? t("labelTransfer")
+                : t("labelWithdraw");
             const hasSymbol =
-              row["side"] === "TRANSFER"
+              row.side === "TRANSFER"
                 ? row["receiverAddress"]?.toUpperCase() ===
                   accAddress?.toUpperCase()
                   ? "+"
                   : "-"
-                : row["side"] === "DEPOSIT"
+                : row.side === "DEPOSIT"
                 ? "+"
-                : row["side"] === "OFFCHAIN_WITHDRAWAL"
+                : row.side === "OFFCHAIN_WITHDRAWAL"
                 ? "-"
                 : "";
-            const value = row["side"];
-            const renderValue =
-              value === TransactionTradeTypes.deposit ? (
-                <DepositIcon />
-              ) : value === TransactionTradeTypes.transfer ? (
-                <Tr
+            const sideIcon =
+              row.side === TransactionTradeTypes.deposit ? (
+                <DepositIcon fontSize={"inherit"} />
+              ) : row.side === TransactionTradeTypes.transfer ? (
+                <TransferIcon fontSize={"inherit"} />
               ) : (
-                t("labelWithdraw")
+                <WithdrawIcon fontSize={"inherit"} />
               );
             const renderValue = hasValue
               ? `${getValuePrecisionThousand(
@@ -491,22 +488,9 @@ export const TransactionTable = withTranslation(["tables", "common"])(
                   { isTrade: true }
                 )}`
               : EmptyValueTag;
-            return (
-              <Box className="rdg-cell-value textAlignRight">
-                {hasSymbol}
-                {renderValue} {unit || ""}
-              </Box>
-            );
-          },
-        },
-        {
-          key: "fee",
-          name: t("labelTxFee"),
-          headerCellClass: "textAlignRight",
-          formatter: ({ row }) => {
-            const fee = row["fee"];
-            const renderValue = `${getValuePrecisionThousand(
-              fee.value,
+
+            const renderFee = `Fee: ${getValuePrecisionThousand(
+              row.fee.value,
               undefined,
               undefined,
               undefined,
@@ -515,20 +499,49 @@ export const TransactionTable = withTranslation(["tables", "common"])(
                 floor: false,
                 isTrade: true,
               }
-            )} ${fee.unit}`;
+            )} ${row.fee.unit}`;
             return (
-              <Box className="rdg-cell-value textAlignRight">{renderValue}</Box>
+              <Box
+                flex={1}
+                display={"flex"}
+                alignItems={"center"}
+                justifyContent={"flex-start"}
+                title={side}
+              >
+                {/*{side + " "}*/}
+                <Typography
+                  display={"inline-flex"}
+                  marginRight={1}
+                  variant={"h2"}
+                  alignItems={"center"}
+                >
+                  {sideIcon}
+                </Typography>
+                <Box display={"flex"} flex={1} flexDirection={"column"}>
+                  <Typography
+                    display={"inline-flex"}
+                    justifyContent={"flex-end"}
+                    alignItems={"center"}
+                  >
+                    {hasSymbol}
+                    {renderValue} {unit || ""}
+                  </Typography>
+                  <Typography color={"textSecondary"} variant={"body2"}>
+                    {renderFee}
+                  </Typography>
+                </Box>
+              </Box>
             );
           },
         },
         {
           key: "from",
-          name: t("labelTxFrom"),
+          name: t("labelTxFrom") + " / " + t("labelTxTime"),
           headerCellClass: "textAlignRight",
           cellClass: "textAlignRight",
           formatter: ({ row }) => {
-            const receiverAddress = getShortAddr(row.receiverAddress);
-            const senderAddress = getShortAddr(row.senderAddress);
+            const receiverAddress = getShortAddr(row.receiverAddress, isMobile);
+            const senderAddress = getShortAddr(row.senderAddress, isMobile);
             const [from, to] =
               row["side"] === "TRANSFER"
                 ? row["receiverAddress"]?.toUpperCase() ===
@@ -547,80 +560,46 @@ export const TransactionTable = withTranslation(["tables", "common"])(
                 : Explorer +
                   `tx/${row.hash}-${EXPLORE_TYPE[row.txType.toUpperCase()]}`;
 
-            // if (
-            //   row.txHash ||
-            //   (row.blockIdInfo.blockId &&
-            //     row.storageInfo &&
-            //     (row.storageInfo.tokenId || row.storageInfo.storageId))
-            // ) {
-            // const path =
-            //   row.txHash !== ""
-            //     ? etherscanBaseUrl + `/tx/${row.txHash}`
-            //     : Explorer +
-            //       `tx/${row.hash}-${EXPLORE_TYPE[row.txType.toUpperCase()]}`;
-            // const path =
-            //   row.txHash !== ""
-            //     ? etherscanBaseUrl + `/tx/${row.txHash}`
-            //     : row.storageInfo.tokenId || row.storageInfo.storageId
-            //     ? Explorer +
-            //       `tx/${row.storageInfo.accountId}-${row.storageInfo.tokenId}-${row.storageInfo.storageId}`
-            //     : Explorer +
-            //       `tx/${row.hash}-${EXPLORE_TYPE[row.txType.toUpperCase()]}`;
+            const hasValue = Number.isFinite(row.time);
+            const renderTime = hasValue
+              ? moment(new Date(row.time), "YYYYMMDDHHMM").fromNow()
+              : EmptyValueTag;
+
             return (
               <Box
-                className="rdg-cell-value textAlignRight"
-                display={"inline-flex"}
-                justifyContent={"flex-end"}
-                alignItems={"center"}
+                display={"flex"}
+                flex={1}
+                flexDirection={"column"}
+                onClick={() => window.open(path, "_blank")}
               >
                 <Typography
-                  style={{
-                    cursor: "pointer",
-                  }}
-                  color={"var(--color-primary)"}
-                  onClick={() => window.open(path, "_blank")}
-                  title={hash}
+                  display={"inline-flex"}
+                  justifyContent={"flex-end"}
+                  alignItems={"center"}
                 >
-                  {from + " -> " + to}
-                  {/*{hash ? getFormattedHash(hash) : EmptyValueTag}*/}
+                  <Typography
+                    style={{
+                      cursor: "pointer",
+                    }}
+                    color={"var(--color-primary)"}
+                    title={hash}
+                  >
+                    {from + " -> " + to}
+                    {/*{hash ? getFormattedHash(hash) : EmptyValueTag}*/}
+                  </Typography>
+                  <Typography marginLeft={1}>
+                    <CellStatus {...{ row }} />
+                  </Typography>
                 </Typography>
-                <Box marginLeft={1}>
-                  <CellStatus {...{ row }} />
-                </Box>
+                <Typography color={"textSecondary"} variant={"body2"}>
+                  {renderTime}
+                </Typography>
               </Box>
             );
           },
         },
-        {
-          key: "status",
-          name: t("labelTxMemo"),
-          headerCellClass: "textAlignCenter",
-          formatter: ({ row }) => (
-            <MemoCellStyled
-              title={row["memo"]}
-              className="rdg-cell-value textAlignLeft"
-            >
-              {row["memo"] || EmptyValueTag}
-            </MemoCellStyled>
-          ),
-        },
-        {
-          key: "time",
-          name: t("labelTxTime"),
-          headerCellClass: "textAlignRight",
-          formatter: ({ row }) => {
-            const value = row["time"];
-            const hasValue = Number.isFinite(value);
-            const renderValue = hasValue
-              ? moment(new Date(row["time"]), "YYYYMMDDHHMM").fromNow()
-              : EmptyValueTag;
-            return (
-              <Box className="rdg-cell-value textAlignRight">{renderValue}</Box>
-            );
-          },
-        },
       ],
-      [handleTxnDetail, etherscanBaseUrl]
+      [handleTxnDetail, etherscanBaseUrl, isMobile, t]
     );
 
     const defaultArgs: any = {
