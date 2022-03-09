8f8d339bd2faae5066f1fb5d91476f0470501df0
windatang <windatang@gmail.com>
2022-03-09 21:14:25 +0800

    Merge remote-tracking branch 'origin/LeilaLin123-patch-2' into released

--- packages/common-resources/static-resources/src/i18n/zh_CN/common.ts
+++ packages/common-resources/static-resources/src/i18n/zh_CN/common.ts
@@ -477,7 +477,7 @@ export default {
   labelTradeRaceYourVolume: "Your trading volume",
   labelTradeRaceYourRanking: "Your ranking",
   labelTradeRaceGoTrading: "Go to trade",
-  labelTradeReadRule: "Read Rule",
+  labelTradeReadRule: "Read Rules",
   labelTradeRaceRewards: "Rewards",
   labelTradeRaceRules: "Activity Rules",
   labelTradeRaceStart: "Activity ends in:",
