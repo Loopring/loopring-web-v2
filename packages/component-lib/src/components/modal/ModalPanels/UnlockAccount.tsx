f755ab46c1698915f847cca54e4869617706e15d
windatang <windatang@gmail.com>
2022-04-20 00:49:47 +0800

    ipfs

--- packages/component-lib/src/components/modal/ModalPanels/UnlockAccount.tsx
+++ packages/component-lib/src/components/modal/ModalPanels/UnlockAccount.tsx
@@ -1,6 +1,8 @@
 import { Trans, WithTranslation } from "react-i18next";
 import { UnlockAccountBase, IconType, PanelProps } from "./BasicPanel";
 import { Link, Typography } from "@mui/material";
+import { WalletType } from "@loopring-web/loopring-sdk";
+import { FEED_BACK_LINK } from "@loopring-web/common-resources";
 
 // symbol
 export const UnlockAccount_WaitForAuth = (
@@ -18,7 +20,7 @@ export const UnlockAccount_User_Denied = (
 ) => {
   const propsPatch = {
     iconType: IconType.RefuseIcon,
-    describe1: <Trans i18nKey={"labelSignDenied"}></Trans>,
+    describe1: <Trans i18nKey={"labelSignDenied"} />,
   };
   return <UnlockAccountBase {...propsPatch} {...props} />;
 };
@@ -27,23 +29,42 @@ export const UnlockAccount_User_Denied = (
 export const UnlockAccount_Success = (props: PanelProps & WithTranslation) => {
   const propsPatch = {
     iconType: IconType.DoneIcon,
-    describe1: <Trans i18nKey={"labelUnlockAccountSuccess"}></Trans>,
+    describe1: <Trans i18nKey={"labelUnlockAccountSuccess"} />,
   };
   return <UnlockAccountBase {...propsPatch} {...props} />;
 };
 
 // value symbol
 export const UnlockAccount_Failed = (
-  props: PanelProps & WithTranslation & { resetAccount: () => void }
+  props: PanelProps &
+    WithTranslation & { walletType?: WalletType; resetAccount: () => void }
 ) => {
   const propsPatch = {
     iconType: IconType.FailedIcon,
     describe1: (
       <Typography component={"div"} display={"inline-flex"}>
         <Trans i18nKey={"labelUnlockAccountFailed"} />
-        <Link onClick={props.resetAccount} paddingLeft={2}>
-          {<Trans i18nKey={"labelReActiveAccount"} />}
-        </Link>
+        {props.walletType &&
+        (props.walletType.isContract ||
+          props.walletType.isInCounterFactualStatus) ? (
+          <Typography color={"textSecondary"} paddingLeft={2}>
+            <Trans i18nKey={"labelConnectUsSimple"} ns={"error"}>
+              Please
+              <Link
+                target="_blank"
+                rel="noopener noreferrer"
+                href={FEED_BACK_LINK}
+              >
+                contact us
+              </Link>
+              .
+            </Trans>
+          </Typography>
+        ) : (
+          <Link onClick={props.resetAccount} paddingLeft={2}>
+            {<Trans i18nKey={"labelReActiveAccount"} />}
+          </Link>
+        )}
       </Typography>
     ),
   };
