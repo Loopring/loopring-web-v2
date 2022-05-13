e5affa6004781cf20db70bfda0e2b255a892c9e7
windatang <windatang@gmail.com>
2022-05-13 10:17:20 +0800

    L2 send

--- packages/core/src/modal/WalletModal/index.tsx
+++ packages/core/src/modal/WalletModal/index.tsx
@@ -264,22 +264,22 @@ export const ModalWalletConnectPanel = withTranslation("common")(
               [account]
             ),
           },
-          {
-            ...DefaultGatewayList[2],
-            handleSelect: React.useCallback(
-              async (event, flag?) => {
-                walletServices.sendDisconnect("", "should new provider");
-                setShowConnect({
-                  isShow: true,
-                  step: WalletConnectStep.CommonProcessing,
-                });
-                setConnectProvider(DefaultGatewayList[2].key);
-                setProcessingCallback({ callback: gameStopCallback });
-                setStateCheck(true);
-              },
-              [account]
-            ),
-          },
+          // {
+          //   ...DefaultGatewayList[2],
+          //   handleSelect: React.useCallback(
+          //     async (event, flag?) => {
+          //       walletServices.sendDisconnect("", "should new provider");
+          //       setShowConnect({
+          //         isShow: true,
+          //         step: WalletConnectStep.CommonProcessing,
+          //       });
+          //       setConnectProvider(DefaultGatewayList[2].key);
+          //       setProcessingCallback({ callback: gameStopCallback });
+          //       setStateCheck(true);
+          //     },
+          //     [account]
+          //   ),
+          // },
           // {
           //   ...DefaultGatewayList[3],
           //   handleSelect: React.useCallback(
