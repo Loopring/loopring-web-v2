4f5ac03666d46d5e75b37fce43a622c3c4718a61
windatang <windatang@gmail.com>
2022-06-30 14:48:23 +0800

    NFT send & NFT MP3/4

--- packages/webapp/src/pages/Layer2Page/ForcewithdrawPanel/index.tsx
+++ packages/webapp/src/pages/Layer2Page/ForcewithdrawPanel/index.tsx
@@ -1,9 +1,14 @@
 import styled from "@emotion/styled";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Link, Typography } from "@mui/material";
 import React from "react";
 import { WithTranslation, withTranslation } from "react-i18next";
-import { ForceWithdrawPanel } from "@loopring-web/component-lib";
+import {
+  ForceWithdrawPanel,
+  TransactionTradeTypes,
+} from "@loopring-web/component-lib";
 import { useForceWithdraw } from "@loopring-web/core";
+import { useTheme } from "@emotion/react";
+import { UserTxTypes } from "@loopring-web/loopring-sdk";
 
 const StylePaper = styled(Box)`
   background: var(--color-box);
@@ -13,6 +18,7 @@ const StylePaper = styled(Box)`
 export const ForcewithdrawPanel = withTranslation(["common", "layout"])(
   ({ t }: WithTranslation) => {
     const { forceWithdrawProps } = useForceWithdraw();
+    const theme = useTheme();
     return (
       <StylePaper
         flex={1}
@@ -21,7 +27,21 @@ export const ForcewithdrawPanel = withTranslation(["common", "layout"])(
         justifyContent={"center"}
         className={"MuiPaper-elevation2"}
         marginBottom={2}
+        position={"relative"}
       >
+        <Link
+          position={"absolute"}
+          variant={"body1"}
+          sx={{
+            right: 2 * theme.unit,
+            top: 2 * theme.unit,
+          }}
+          target="_self"
+          rel="noopener noreferrer"
+          href={`./#/layer2/history/transactions?types=${TransactionTradeTypes.forceWithdraw}`}
+        >
+          {t("labelTransactionsLink")}
+        </Link>
         <Box display={"flex"} alignItems={"center"} justifyContent={"center"}>
           <ForceWithdrawPanel {...forceWithdrawProps} _width={420} />
         </Box>
