ec8a5a983845ef3ae5eef91f879aafbf7ad44cfe
windatang <windatang@me.com>
2022-08-09 23:37:16 +0800

    NFT collection & mint

--- packages/webapp/src/pages/NFTPage/components/CollectionItem.tsx
+++ packages/webapp/src/pages/NFTPage/components/CollectionItem.tsx
@@ -191,7 +191,7 @@ export const CollectionItem = React.memo(React.forwardRef(({
 
                   }}
                 >
-                  {t((item.name && item.tileUr) ? "labelNFTMintBtn" : "Fix Collection")}
+                  {t((item.name && item.tileUri) ? "labelNFTMintBtn" : "Fix Collection")}
                 </Button>
               </Box> :
               <Box className={isMobile ? "isMobile" : ""} width={"48%"} marginLeft={'4%'}>
