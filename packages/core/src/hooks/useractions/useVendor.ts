e4662e9d9cd468a2cea6317fd8ea29d8b2106f01
windatang <windatang@me.com>
2022-09-02 10:45:56 +0800

    dual

--- packages/core/src/hooks/useractions/useVendor.ts
+++ packages/core/src/hooks/useractions/useVendor.ts
@@ -138,11 +138,11 @@ export const useVendor = () => {
                 userAddress: account.accAddress,
                 defaultFlow: "OFFRAMP",
                 enabledFlows: ["OFFRAMP"],
-                url: "https://ri-widget-dev-5.firebaseapp.com",
+                url: "https://ri-widget-dev-5.web.app", //"https://ri-widget-dev-5.firebaseapp.com",
               };
               config = {
                 ...config,
-                hostApiKey: "vy8uc7pm9wdeqvcge4jwxtcyurj6h9nbx6qsuu48",
+                hostApiKey: "3qncr4yvxfpro6endeaeu6npkh8qc23e9uadtazq",
               };
               window.rampInstance = new RampInstantSDK({
                 ...config,
