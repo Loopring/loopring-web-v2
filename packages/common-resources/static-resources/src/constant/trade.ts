21a12868d7ea0289144388fa3d1144d95a182702
windatang <windatang@me.com>
2022-09-02 14:36:07 +0800

    fee

--- packages/common-resources/static-resources/src/constant/trade.ts
+++ packages/common-resources/static-resources/src/constant/trade.ts
@@ -285,7 +285,8 @@ export type AddressItemType<T> = {
   maxWidth?: string | number;
 };
 
-export const useAddressTypeLists = <T extends WALLET_TYPE | EXCHANGE_TYPE
+export const useAddressTypeLists = <
+  T extends WALLET_TYPE | EXCHANGE_TYPE
 >() => {
   const { t } = useTranslation("common");
   const walletList: AddressItemType<T>[] = [
@@ -431,4 +432,4 @@ export type L2CollectionFilter = {
   owner?: string;
 };
 
-export const LIVE_FEE_TIMES = 60000; // todo
+export const LIVE_FEE_TIMES = 60000;
