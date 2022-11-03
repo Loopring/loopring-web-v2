6aee5e699daf6d592eec877d4c8b117196ceac64
windatang <windatang@gmail.com>
2022-11-04 00:37:44 +0800

    nftDetail

--- packages/common-resources/static-resources/src/constant/setting.ts
+++ packages/common-resources/static-resources/src/constant/setting.ts
@@ -1,4 +1,3 @@
-import moment from "moment";
 import { IsMobile } from "../utils";
 
 export enum UpColor {
@@ -75,21 +74,3 @@ export const sizeNFTConfig = (size: "large" | "medium" | "small") => {
       break;
   }
 };
-moment.defineLocale("en-gb", {
-  relativeTime: {
-    future: "in %s",
-    past: "%s ago",
-    s: "Just now",
-    ss: "%d seconds",
-    m: "a minute",
-    mm: "%d minutes",
-    h: "an hour",
-    hh: "%d hours",
-    d: "a day",
-    dd: "%d days",
-    M: "a month",
-    MM: "%d months",
-    y: "a year",
-    yy: "%d years",
-  },
-});
--- packages/component-lib/src/stores/reducer/settings/reducer.ts
+++ packages/component-lib/src/stores/reducer/settings/reducer.ts
@@ -46,22 +46,10 @@ export const settingsSlice: Slice<SettingsState> = createSlice({
         action.payload === "en_US"
           ? moment.updateLocale("en", {
               relativeTime: {
-                future: "in %s",
-                past: "%s ago",
-                s: "a few seconds",
-                ss: "%d seconds",
-                m: "a minute",
-                mm: "%d minutes",
-                h: "an hour",
-                hh: "%d hours",
-                d: "a day",
-                dd: "%d days",
-                w: "a week",
-                ww: "%d weeks",
-                M: "a month",
-                MM: "%d months",
-                y: "a year",
-                yy: "%d years",
+                future: (diff) => (diff == "just now" ? diff : `in ${diff}`),
+                past: (diff) => (diff == "just now" ? diff : `${diff} ago`),
+                s: "just now",
+                ss: "just now",
               },
             })
           : moment.updateLocale("zh-cn", {
