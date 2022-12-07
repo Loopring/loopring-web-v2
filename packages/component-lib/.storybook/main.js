4b52aa9fab04413fa8edf2ff7091c620d7fffa53
windatang <windatang@gmail.com>
2022-12-07 19:55:34 +0800

    ACTIVE ACCOUNT

--- packages/component-lib/.storybook/main.js
+++ packages/component-lib/.storybook/main.js
@@ -93,69 +93,6 @@ module.exports = {
     ];
     console.log("rule.plugins:", rule.options.plugins);
 
-    // config.module.rules.push({
-    //   test: /\.(mjs|js|jsx|tsx|ts)$/,
-    //   // exclude: [/node_modules/, /dist/],
-    //   include: [
-    //     path.resolve(
-    //       __dirname,
-    //       "..",
-    //       "..",
-    //       "common-resources",
-    //       "static-resources"
-    //     ),
-    //   ],
-    //
-    //   // resolve: { fullySpecified: false },
-    //   loader: "babel-loader",
-    //   // loader: require.resolve('babel-loader'),
-    //   options: {
-    //     customize: require.resolve("babel-preset-react-app/webpack-overrides"),
-    //     presets: [
-    //       [
-    //         require.resolve("babel-preset-react-app"),
-    //         {
-    //           runtime: hasJsxRuntime ? "automatic" : "classic",
-    //         },
-    //       ],
-    //     ],
-    //     // @remove-on-eject-begin
-    //     babelrc: false,
-    //     configFile: false,
-    //     // Make sure we have a unique cache identifier, erring on the
-    //     // side of caution.
-    //     // We remove this when the user ejects because the default
-    //     // is sane and uses Babel options. Instead of options, we use
-    //     // the react-scripts and babel-preset-react-app versions.
-    //     cacheIdentifier: getCacheIdentifier("development", [
-    //       "babel-plugin-named-asset-import",
-    //       "babel-preset-react-app",
-    //       "react-dev-utils",
-    //       "react-scripts",
-    //     ]),
-    //     // @remove-on-eject-end
-    //     plugins: [
-    //       [
-    //         require.resolve("babel-plugin-named-asset-import"),
-    //         {
-    //           loaderMap: {
-    //             svg: {
-    //               ReactComponent: "@svgr/webpack?-svgo,+titleProp,+ref![path]",
-    //             },
-    //           },
-    //         },
-    //       ],
-    //       "production" && require.resolve("react-refresh/babel"),
-    //     ].filter(Boolean),
-    //     // This is a feature of `babel-loader` for webpack (not Babel itself).
-    //     // It enables caching results in ./node_modules/.cache/babel-loader/
-    //     // directory for faster rebuilds.
-    //     cacheDirectory: true,
-    //     // See #6846 for context on why cacheCompression is disabled
-    //     cacheCompression: false,
-    //     compact: "auto",
-    //   },
-    // });
     config.module.rules.push({
       test: /\.s(a|c)ss$/,
       use: [
