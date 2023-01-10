f21e1e32cb6bf3f4c989b903effcd84179b1878b
windatang <windatang@gmail.com>
2023-01-10 14:12:14 +0800

    active account from transfer fee is show as U

--- packages/common-resources/static-resources/src/i18n/en_US/common.ts
+++ packages/common-resources/static-resources/src/i18n/en_US/common.ts
@@ -1305,8 +1305,8 @@ export default {
     "Please waiting a while, Banxa service is not available currently.",
   labelL2toL2AddressFeePaid: "Active account fee had paid",
   labelL2toL2AddressFeeActiveFee:
-    "You can choose to pay their activation fee \n" +
-    "Max activation cost: {{value}}",
+    "You can choose to pay their activation fee. \n" +
+    "Pay recipient's L2 activation fee max cost: {{value}}",
   // "You can choose to pay their activation fee {{fee}} {{symbol}}?",
   // "The recipient does not have Loopring L2 activated. You can choose to pay their activation fee. "\n
   labelL2toL2FeeWithActive: "Active {{addr}} L2 & Transfer Fee",
