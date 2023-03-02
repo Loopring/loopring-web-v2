6881f5c565ac93194cdd2c62d7ce5629acc5a96e
windatang <windatang@gmail.com>
2023-03-02 10:50:54 +0800

    Merge branch 'staging'

--- packages/webapp/src/pages/NFTPage/MintNFTPanel/metaNFTPanel.tsx
+++ packages/webapp/src/pages/NFTPage/MintNFTPanel/metaNFTPanel.tsx
@@ -41,7 +41,7 @@ import {
 import * as sdk from "@loopring-web/loopring-sdk";
 
 const MaxSize = 10485760;
-const MaxMediaSize = 10485760 * 5;
+const MaxMediaSize = 10485760 * 10;
 
 export const MetaNFTPanel = <
   Me extends NFTMETA,
