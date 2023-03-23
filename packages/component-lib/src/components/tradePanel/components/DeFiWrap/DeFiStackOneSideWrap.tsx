f8ec5f72b45e4e47f82470ee9d3ed840673fc7a5
windatang <windatang@gmail.com>
2023-03-23 16:31:16 +0800

    Change log 1. guardian split from loopring.io 2. swap market Price huge diff with loopring pair price alert 3. LRC staking 4. trade record error at show `taker` & maker

--- packages/component-lib/src/components/tradePanel/components/DeFiWrap/DeFiStackOneSideWrap.tsx
+++ packages/component-lib/src/components/tradePanel/components/DeFiWrap/DeFiStackOneSideWrap.tsx
@@ -70,7 +70,7 @@ export const DeFiSideDetail = ({
             <Trans i18nKey={"labelDeFiSideAmount"}>Amount</Trans>
           </Typography>
           <Typography component={"span"} variant={"inherit"}>
-            {order.remainAmount
+            {order.remainAmount && order.remainAmount = "0"
               ? getValuePrecisionThousand(
                   sdk.toBig(order.remainAmount).div("1e" + tokenSell.decimals),
                   tokenSell.precision,
@@ -172,7 +172,7 @@ export const DeFiSideDetail = ({
             </Trans>
           </Typography>
           <Typography component={"span"} variant={"inherit"}>
-            {(order.totalRewards
+            {(order.totalRewards && order.totalRewards!='0'
               ? getValuePrecisionThousand(
                   sdk.toBig(order.totalRewards).div("1e" + tokenSell.decimals),
                   tokenSell.precision,
@@ -205,7 +205,7 @@ export const DeFiSideDetail = ({
             </Trans>
           </Typography>
           <Typography component={"span"} variant={"inherit"}>
-            {(order.lastDayPendingRewards
+            {(order.lastDayPendingRewards && order.lastDayPendingRewards = '0'
               ? getValuePrecisionThousand(
                   sdk
                     .toBig(order.lastDayPendingRewards)
