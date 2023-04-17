14d1dc728340abc40b78fc21a54e4cef3c2a5a49
john.li <john.li@loopring.io>
2023-04-17 09:41:58 +0800

    Revert "hide NFTs"
    This reverts commit 22ceaa1c43ba33061fb87a4f48ff15fa4368ce34.

--- packages/common-resources/static-resources/src/constant/setting.ts
+++ packages/common-resources/static-resources/src/constant/setting.ts
@@ -91,4 +91,4 @@ export const MapChainId = {
 export enum ChainIdExtends {
   TAIKO_A2 = 167004,
 }
-export const REDPACKET_SHOW_NFTS = false; // Toogle to hide or show NFT Red Packet
+export const REDPACKET_SHOW_NFTS = true; // Toogle to hide or show NFT Red Packet
