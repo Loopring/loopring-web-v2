0947ac54d33113d56b1476a44feb811871badee7
windatang <windatang@gmail.com>
2023-05-25 23:15:26 +0700

    color

--- packages/webapp/src/pages/Layer2Page/ReferralRewardsPanel/index.tsx
+++ packages/webapp/src/pages/Layer2Page/ReferralRewardsPanel/index.tsx
@@ -4,38 +4,62 @@ import {
   BoxProps,
   Button,
   Container,
-  Grid,
   InputAdornment,
-  MenuItem,
   OutlinedInput,
-  TextField,
+  Tab,
+  Tabs,
   Typography,
 } from "@mui/material";
 import React, { useState } from "react";
-import {
-  useTranslation,
-  WithTranslation,
-  withTranslation,
-} from "react-i18next";
+import { useTranslation } from "react-i18next";
 import { useAccount, ViewAccountTemplate } from "@loopring-web/core";
 import {
-  Layer2RouterID,
-  LinkedIcon,
-  LinkIcon,
   LinkSharedIcon,
-  SearchIcon,
+  SoursURL,
   TOAST_TIME,
 } from "@loopring-web/common-resources";
-import { ForcewithdrawPanel } from "../ForcewithdrawPanel";
-import { SecurityPanel } from "../SecurityPanel";
-import { VipPanel } from "../VipPanel";
-import { MenuItemProps, Toast } from "@loopring-web/component-lib";
+import { Toast } from "@loopring-web/component-lib";
 
+const BoxStyled = styled(Box)`
+  ol {
+    list-style: decimal inside;
+
+    li {
+      //list-item;
+      color: var(--color-text-third);
+      display: list-item;
+      font-size: ${({ theme }) => theme.fontDefault.body1};
+      // padding: ${({ theme }) => `0 ${theme.unit}`}px;
+      line-height: 2em;
+    }
+  }
+`;
 export const BoxBannerStyle = styled(Box)<
   BoxProps & { backGroundUrl?: string | number; direction?: "left" | "right" }
 >`
   background-color: var(--color-box);
-`;
+
+  .bg:after {
+    display: block;
+    content: "";
+    float: ${({ direction }) => direction};
+    width: 35%;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;
+    background-image: url("${({ backGroundUrl }) => backGroundUrl}");
+  }
+` as (
+  props: BoxProps & {
+    backGroundUrl?: string | number;
+    direction?: "left" | "right";
+  }
+) => JSX.Element;
+
+enum ReferStep {
+  method1 = 0,
+  method2 = 1,
+}
 
 const ReferHeader = ({
   isActive = true,
@@ -47,65 +71,88 @@ const ReferHeader = ({
   const { account } = useAccount();
   const { t } = useTranslation();
   return (
-    <BoxBannerStyle>
+    <BoxBannerStyle
+      backGroundUrl={SoursURL + "/images/giftReward.webp"}
+      direction={"right"}
+    >
       <Container>
-        <Typography component={"h1"} variant={"h2"}>
-          {t(
-            "labelReferTitle //Invite friends to join in Loopring and receive rewards"
-          )}
-        </Typography>
-        <Typography component={"h2"} variant={"body1"} color={"textSecondary"}>
-          {t(
-            "labelReferTitleDes // As referrer: will receive a one-year commission on fees the new referred user trades. \n As referee:  will enjoy a one-year discount on transfer fees."
-          )}
-        </Typography>
-        <Box>
-          <OutlinedInput
-            className={"search"}
-            aria-label={"search"}
-            placeholder={"Search"}
-            value={account.accountId}
-            disabled={true}
-            // onChange={(event: any) => {}}
-            startAdornment={
-              <InputAdornment position="start">
-                <LinkSharedIcon color={"inherit"} />
-              </InputAdornment>
-            }
-            endAdornment={
-              <Button variant={"text"} onClick={() => handleCopy("link")}>
-                {t("labelCopy")}
-              </Button>
-            }
-          />
-        </Box>
-        <Box>
-          <OutlinedInput
-            className={"search"}
-            aria-label={"search"}
-            placeholder={"Search"}
-            value={account.accountId}
-            disabled={true}
-            // onChange={(event: any) => {}}
-            startAdornment={
-              <InputAdornment position="start">
-                <Typography
-                  color={"var(--color-text-third)"}
-                  variant={"body1"}
-                  component={"span"}
-                  paddingX={1 / 2}
-                >
-                  #
-                </Typography>
-                {/*<LinkIcon color={"inherit"} />*/}
-              </InputAdornment>
-            }
-            endAdornment={
-              <Button variant={"text"} onClick={() => handleCopy("id")}>
-                {t("labelCopy")}
-              </Button>
-            }
-          />
+        <Box className={"bg"} marginY={3} display={"flex"}>
+          <Box width={"65%"}>
+            <Typography
+              component={"h1"}
+              variant={"h2"}
+              sx={{ whiteSpace: "pre-line", wordBreak: "break-all" }}
+            >
+              {t("labelReferTitle")}
+            </Typography>
+            <Typography
+              paddingY={1}
+              component={"h2"}
+              variant={"body1"}
+              color={"textSecondary"}
+              sx={{ whiteSpace: "pre-line", wordBreak: "break-all" }}
+            >
+              {t("labelReferTitleDes")}
+            </Typography>
+            <Box paddingTop={1} width={"70%"}>
+              <OutlinedInput
+                size={"medium"}
+                className={"copy"}
+                placeholder={"copy"}
+                value={`https//loopring.io?referralcode=${account.accountId}`}
+                disabled={true}
+                fullWidth={true}
+                // onChange={(event: any) => {}}
+                startAdornment={
+                  <InputAdornment position="start">
+                    <LinkSharedIcon color={"inherit"} />
+                  </InputAdornment>
+                }
+                endAdornment={
+                  <Button
+                    size={"small"}
+                    variant={"text"}
+                    onClick={() => handleCopy("link")}
+                  >
+                    {t("labelCopy")}
+                  </Button>
+                }
+              />
+            </Box>
+            <Box paddingTop={1} width={"70%"}>
+              <OutlinedInput
+                size={"medium"}
+                className={"copy"}
+                placeholder={"copy"}
+                value={account.accountId}
+                disabled={true}
+                fullWidth={true}
+                // onChange={(event: any) => {}}
+                startAdornment={
+                  <InputAdornment position="start">
+                    <Typography
+                      color={"var(--color-text-third)"}
+                      variant={"body1"}
+                      component={"span"}
+                      paddingX={1 / 2}
+                    >
+                      #
+                    </Typography>
+                    {/*<LinkIcon color={"inherit"} />*/}
+                  </InputAdornment>
+                }
+                endAdornment={
+                  <Button
+                    size={"small"}
+                    variant={"text"}
+                    onClick={() => handleCopy("id")}
+                  >
+                    {t("labelCopy")}
+                  </Button>
+                }
+              />
+            </Box>
+          </Box>
         </Box>
       </Container>
     </BoxBannerStyle>
@@ -114,7 +161,7 @@ const ReferHeader = ({
 
 const ReferView = () => {
   const { t } = useTranslation();
-
+  const [currentTab, setCurrentTab] = React.useState(ReferStep.method1);
   const [copyToastOpen, setCopyToastOpen] = useState(false);
   const handleCopy = (selected: "id" | "link") => {
     switch (selected) {
@@ -137,6 +184,50 @@ const ReferView = () => {
         severity={"success"}
       />
       <ReferHeader handleCopy={handleCopy} />
+      <Container>
+        <BoxStyled marginTop={2} paddingY={2} paddingX={0} flex={1}>
+          <Typography component={"h3"} variant={"h4"} marginY={1}>
+            {t("labelReferralRules")}
+          </Typography>
+          <Tabs
+            sx={{ marginLeft: -1 }}
+            value={currentTab}
+            className={"MuiTabs-small"}
+            onChange={(_event, value) => {
+              setCurrentTab(value);
+            }}
+            aria-label="reward-rule-tabs"
+            variant="scrollable"
+          >
+            <Tab label={t("labelReferralMethod1")} value={ReferStep.method1} />
+            <Tab label={t("labelReferralMethod2")} value={ReferStep.method2} />
+          </Tabs>
+          <Box>
+            {currentTab === ReferStep.method1 && (
+              <ol>
+                <li>{t("labelReferralMethod1Step1")}</li>
+                <li>{t("labelReferralMethod1Step2")}</li>
+                <li>{t("labelReferralMethod1Step3")}</li>
+                <li>{t("labelReferralMethod1Step4")}</li>
+              </ol>
+            )}
+            {currentTab === ReferStep.method2 && <></>}
+          </Box>
+        </BoxStyled>
+        <BoxStyled marginTop={2} paddingY={2} paddingX={0} flex={1}>
+          <Typography component={"h3"} variant={"h4"} marginY={1}>
+            {t("labelReferralMyReferrals")}
+          </Typography>
+
+          <Box></Box>
+        </BoxStyled>
+        <BoxStyled marginTop={2} paddingY={2} paddingX={0} flex={1}>
+          <Typography component={"h3"} variant={"h4"} marginY={1}>
+            {t("labelReferralReferralsRefunds")}
+          </Typography>
+          <Box></Box>
+        </BoxStyled>
+      </Container>
     </>
   );
 };
