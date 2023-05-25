b774692f0933ec42d0824672082bf4bdcb53ac8e
windatang <windatang@gmail.com>
2023-05-25 23:15:26 +0700

    color

--- packages/component-lib/src/components/block/RedPacket.tsx
+++ packages/component-lib/src/components/block/RedPacket.tsx
@@ -57,9 +57,7 @@ import { NFTMedia } from "./nftMedia";
 import { sanitize } from "dompurify";
 import { useTheme } from "@emotion/react";
 
-export const RedPacketBg = styled(Box)<
-  BoxProps & { imageSrc?: string; type: string }
->`
+export const RedPacketBg = styled(Box)<BoxProps & { imageSrc?: string; type: string }>`
   display: flex;
   align-items: center;
   position: relative;
