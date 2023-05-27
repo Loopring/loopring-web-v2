2fdf3ec58dd891b3944f043f3f85a062280e58bf
windatang <windatang@gmail.com>
2023-05-28 00:40:13 +0700

    color

--- packages/webapp/public/wallet.js
+++ packages/webapp/public/wallet.js
@@ -1,37 +1,259 @@
-const __language__ = {
-  en: {
-    labelNavZkRollupLayer2: "zkRollup Layer 2",
-    labelNavWallet: "Wallet",
-  },
+const svgGroup = {
+  logSvg: `<svg
+      aria-hidden="true"
+      className="SvgIcon-root SvgIcon-fontSizeMedium "
+      color="var(--color-text-third)"
+      fill="none"
+      focusable="false"
+      height="27"
+      style="height: 40px; width: 120px;"
+      viewBox="0 0 89 27"
+      width="89"
+    >
+      <path
+        clip-rule="evenodd"
+        d="M33.2461 13.1402H19.2495L22.2249 17.7118L11.3922 26.0863L33.2461 13.2211V13.1402ZM11.0988 26.2683V0.740234L0.0356445 17.732L11.0988 26.2683ZM35.2363 26.1875V13.1807H37.4154V24.4074H40.3908V26.1875H35.2363ZM88.6455 15.6486V17.7928H86.5293V15.6283C86.5921 14.6372 84.5387 14.5967 84.6854 15.6283V23.659C84.6854 24.1647 84.9578 24.4277 85.5654 24.4277C86.1311 24.4277 86.5293 24.1647 86.5293 23.659V21.4541H85.6073V19.674H88.6455V23.6792C88.6455 25.257 87.556 26.2887 85.5654 26.2887C83.5749 26.2887 82.4853 25.2368 82.4853 23.6792V15.6486C82.4853 14.091 83.5749 13.0391 85.5654 13.0391C87.556 13.0391 88.6455 14.0708 88.6455 15.6486ZM79.1119 20.2809C79.07 19.0874 79.049 17.9951 79.049 16.9836H79.0281V13.1605H81.0186V26.1875H78.7138L76.6604 18.9256C76.7023 20.1393 76.7233 21.2923 76.7233 22.3441V26.1875H74.7327V13.1605H77.1004L79.1119 20.2809ZM73.1403 26.1673H70.9612V13.1403H73.1403V26.1673ZM66.582 19.411C67.1058 19.411 67.3782 19.1481 67.3782 18.6424V15.7093C67.3782 15.2036 67.1058 14.9406 66.582 14.9406H65.5972V19.411H66.582ZM69.5573 15.7093V18.6424C69.5573 19.6335 69.1383 20.4022 68.3421 20.827L69.8297 26.1875H67.6506L66.2677 21.2114H65.5972V26.1875H63.4181V13.1605H66.582C68.5306 13.1605 69.5573 14.1315 69.5573 15.7093ZM59.3532 19.411C59.8771 19.411 60.1494 19.1481 60.1494 18.6424H60.1704V15.7093C60.1704 15.2036 59.898 14.9406 59.3742 14.9406H58.3894V19.411H59.3532ZM56.2103 13.1605H59.3742C61.3228 13.1605 62.3285 14.1112 62.3495 15.7295V18.6626C62.3495 20.2404 61.3228 21.2114 59.3742 21.2114H58.3894V26.1875H56.2103V13.1605ZM50.7834 23.6792C50.6577 24.6906 52.7111 24.6906 52.5854 23.6792V15.6486C52.6902 14.6372 50.6577 14.6372 50.7834 15.6486V23.6792ZM48.6043 15.6486C48.6043 14.0708 49.6939 13.0391 51.6844 13.0391C53.654 13.0391 54.7645 14.0708 54.7645 15.6486V23.6792C54.7645 25.257 53.675 26.2887 51.6844 26.2887C49.6939 26.2887 48.6043 25.2368 48.6043 23.6792V15.6486ZM43.3242 23.6792C43.1985 24.6906 45.2519 24.6906 45.1261 23.6792V15.6486C45.2309 14.6372 43.1985 14.6372 43.3242 15.6486V23.6792ZM41.1451 15.6486C41.1451 14.0708 42.2346 13.0391 44.2252 13.0391C46.2157 13.0391 47.3053 14.0708 47.3053 15.6486V23.6792C47.3053 25.257 46.2157 26.2887 44.2252 26.2887C42.2346 26.2887 41.1451 25.2368 41.1451 23.6792V15.6486Z"
+        fill-rule="evenodd"
+      />
+    </svg>`,
 };
+
+const resources = {
+  en_US: {
+    common: {
+      labelNavZkRollupLayer2: "zkRollup Layer 2",
+      labelNavWallet: "Wallet",
+      labelNavLanuch: "Launch App",
+      labelAbout: "About",
+      labelLoopringOrg: "Loopring.org",
+      labelLoopringTerms: "Terms of Service",
+      labelPrivacyPolicy: "Privacy Policy",
+      labelRiskDisclosure: "Risk Disclosure",
+      labelSupport: "Support",
+      labelSubmitARequest: "❤️ Submit a Request",
+      labelCreatorGrants: "Creator Grants",
+      labelListAToken: "List a Token",
+      labelWalletGuardian: "Wallet Guardian",
+      labelPlatform: "Platform",
+      labelFees: "Fees",
+      labelVIPProgram: "VIP Program",
+      labelReferrals: "Referrals",
+      labelDevelopers: "Developers",
+      labelSmartContracts: "Smart Contracts",
+      labelAPIs: "APIs",
+      labelLayer2Explorer: "Layer 2 Explorer",
+      labelBugBounty: "Bug Bounty",
+      labelSubgraph: "Subgraph",
+      labelFollowus: "Follow us",
+      labelEthereumUnleashed: "Ethereum Unleashed",
+      labelGatewayToEthereum:
+        "Your Gateway to Ethereum DeFi, \n NFTs, and Financial Freedom",
+      labelIOS: "iOS",
+      labelAndroid: "Android",
+      labelGooglePlay: "labelGooglePlay",
+      labelFeature1: "Fast, Secure, and 100x Lower Fees",
+      labelFeatureDes1:
+        "Experience DeFi as it \n should be - fast and affordable. Built with zkRollups, Loopring provides instant transactions at 100x \n lower fees than Ethereum without sacrificing any of its security.",
+      labelFeature2: "Invest, Stake and Earn",
+      labelFeatureDes2:
+        "Easily earn yield on your \n crypto. Your funds never leave your wallet and can be accessed at any time.",
+      labelFeature3: "Dual Investment",
+      labelFeatureDes3:
+        "Buy Low or Sell High. Get \nrewarded no matter which direction the market moves.",
+      labelFeature4: "Buy Crypto on L2",
+      labelFeatureDes4:
+        "Use card, bank transfer,\nor Apple Pay without ever having to leave your wallet.",
+      labelFeature5: "Your Personal Vault",
+      labelFeatureDes5:
+        "Forget seed phrases. \n Choose people, institutions, and hardware you trust to be your Guardians. Set limits on daily \n transfers or even lock your wallet if needed. You",
+      labelFeature6: "Manage and Display Your NFT Collection",
+      labelFeatureDes6:
+        "Immerse yourself in the \n world of unique digital assets. Safeguard your growing collection, all while enjoying easy access to\n your favorite marketplaces.",
+      labelFeature7: "Digital Red Packets",
+      labelFeatureDes7:
+        "Create memorable \n experiences for friends, family, and your community. Red Packets are perfect for gifts, social events,\n gaming rewards, airdrops, and more!",
+      labelFeatureDes8: "Loopring Wallet",
+      labelFeatureDes8_2: "Crypto exchange on the go",
+      labelCopyRight:
+        "© 2017 Loopring Technology Limited. All rights reserved.",
+    },
+  },
+  // zh_CN: {...zhCN},
+};
+
+// const initLng = JSON.parse(localStorage.getItem('persist:settings') as string)?.language === `"${LanguageType.zh_CN}"` ? LanguageType.zh_CN : LanguageType.en_US
+const initLng = "en_US";
+console.log("en_US");
+const settingPersist = "persist:settings";
 (function init() {
-  const onColorChange = (value) => {
-    window.localStorage.set("themeMode", value);
-    link = document.getElementById("themeModeCss");
-    link.set(
+  let settings = JSON.parse(localStorage.getItem(settingPersist) ?? {});
+  const onColorChange = (value, _this) => {
+    // window.localStorage.set("themeMode", value);
+    let settings = JSON.parse(localStorage.getItem(settingPersist) ?? {});
+    settings.themeMode = value;
+    window.localStorage.setItem(settingPersist, JSON.stringify(settings));
+    let link = document.getElementById("themeModeCss");
+    link.setAttribute(
       "href",
-      value !== "dark"
-        ? "./static/wallet_light.css"
-        : "./static/wallet_dark.css"
+      value !== "light"
+        ? "./static/wallet_dark.css"
+        : "./static/wallet_light.css"
     );
-    document.getElementById("themeModeCss").setAttribute("link", link);
+    if (_this) {
+      _this.setAttribute("value", value);
+    }
+    // document.getElementById("themeModeCss").setAttribute("link", link);
   };
-  var value = window.localStorage.get("themeMode");
-  if (!value) {
+  //
+  if (!settings?.themeMode) {
     value = "dark";
+  } else {
+    value = settings?.themeMode;
   }
   onColorChange(value);
 
   // Language now english only
-
   const onlanguagechange = (value) => {
-    window.localStorage.set("language", value);
-    window.i18n = __language__[value];
+    let settings = JSON.parse(localStorage.getItem(settingPersist) ?? {});
+    settings.language = value;
+    window.localStorage.setItem(settingPersist, JSON.stringify(settings));
+    i18next.changeLanguage("en_US");
+  };
+  const updateI18n = () => {
+    document.getElementById("labelNavZkRollupLayer2").innerHTML = i18next.t(
+      "labelNavZkRollupLayer2"
+    );
+    document.getElementById("labelNavWallet").innerHTML =
+      i18next.t("labelNavWallet");
+    document.getElementById("labelNavLanuch").innerHTML =
+      i18next.t("labelNavLanuch");
+    document.getElementById("labelAbout").innerHTML = i18next.t("labelAbout");
+    document.getElementById("labelLoopringOrg").innerHTML =
+      i18next.t("labelLoopringOrg");
+    document.getElementById("labelLoopringTerms").innerHTML =
+      i18next.t("labelLoopringTerms");
+    document.getElementById("labelPrivacyPolicy").innerHTML =
+      i18next.t("labelPrivacyPolicy");
+    document.getElementById("labelRiskDisclosure").innerHTML = i18next.t(
+      "labelRiskDisclosure"
+    );
+    document.getElementById("labelSupport").innerHTML =
+      i18next.t("labelSupport");
+    document.getElementById("labelSubmitARequest").innerHTML = i18next.t(
+      "labelSubmitARequest"
+    );
+    document.getElementById("labelCreatorGrants").innerHTML =
+      i18next.t("labelCreatorGrants");
+    document.getElementById("labelListAToken").innerHTML =
+      i18next.t("labelListAToken");
+    document.getElementById("labelWalletGuardian").innerHTML = i18next.t(
+      "labelWalletGuardian"
+    );
+    document.getElementById("labelPlatform").innerHTML =
+      i18next.t("labelPlatform");
+    document.getElementById("labelFees").innerHTML = i18next.t("labelFees");
+    document.getElementById("labelVIPProgram").innerHTML =
+      i18next.t("labelVIPProgram");
+    document.getElementById("labelReferrals").innerHTML =
+      i18next.t("labelReferrals");
+    document.getElementById("labelDevelopers").innerHTML =
+      i18next.t("labelDevelopers");
+    document.getElementById("labelSmartContracts").innerHTML = i18next.t(
+      "labelSmartContracts"
+    );
+    document.getElementById("labelAPIs").innerHTML = i18next.t("labelAPIs");
+    document.getElementById("labelLayer2Explorer").innerHTML = i18next.t(
+      "labelLayer2Explorer"
+    );
+    document.getElementById("labelBugBounty").innerHTML =
+      i18next.t("labelBugBounty");
+    document.getElementById("labelSubgraph").innerHTML =
+      i18next.t("labelSubgraph");
+    document.getElementById("labelFollowus").innerHTML =
+      i18next.t("labelFollowus");
+    document.getElementById("labelEthereumUnleashed").innerHTML = i18next.t(
+      "labelEthereumUnleashed"
+    );
+    document.getElementById("labelGatewayToEthereum").innerHTML = i18next.t(
+      "labelGatewayToEthereum"
+    );
+    document.getElementById("labelIOS").innerHTML = i18next.t("labelIOS");
+    document.getElementById("labelAndroid").innerHTML =
+      i18next.t("labelAndroid");
+    document.getElementById("labelGooglePlay").innerHTML =
+      i18next.t("labelGooglePlay");
+    document.getElementById("labelIOS2").innerHTML = i18next.t("labelIOS");
+    document.getElementById("labelAndroid2").innerHTML =
+      i18next.t("labelAndroid");
+    document.getElementById("labelGooglePlay2").innerHTML =
+      i18next.t("labelGooglePlay");
+    document.getElementById("labelFeature1").innerHTML =
+      i18next.t("labelFeature1");
+    document.getElementById("labelFeatureDes1").innerHTML =
+      i18next.t("labelFeatureDes1");
+    document.getElementById("labelFeature2").innerHTML =
+      i18next.t("labelFeature2");
+    document.getElementById("labelFeatureDes2").innerHTML =
+      i18next.t("labelFeatureDes2");
+    document.getElementById("labelFeature3").innerHTML =
+      i18next.t("labelFeature3");
+    document.getElementById("labelFeatureDes3").innerHTML =
+      i18next.t("labelFeatureDes3");
+    document.getElementById("labelFeature4").innerHTML =
+      i18next.t("labelFeature4");
+    document.getElementById("labelFeatureDes4").innerHTML =
+      i18next.t("labelFeatureDes4");
+    document.getElementById("labelFeature5").innerHTML =
+      i18next.t("labelFeature5");
+    document.getElementById("labelFeatureDes5").innerHTML =
+      i18next.t("labelFeatureDes5");
+    document.getElementById("labelFeature6").innerHTML =
+      i18next.t("labelFeature6");
+    document.getElementById("labelFeatureDes6").innerHTML =
+      i18next.t("labelFeatureDes6");
+    document.getElementById("labelFeature7").innerHTML =
+      i18next.t("labelFeature7");
+    document.getElementById("labelFeatureDes7").innerHTML =
+      i18next.t("labelFeatureDes7");
+    document.getElementById("labelFeatureDes8").innerHTML =
+      i18next.t("labelFeatureDes8");
+    document.getElementById("labelFeatureDes8_2").innerHTML =
+      i18next.t("labelFeatureDes8_2");
+    document.getElementById("labelCopyRight").innerHTML =
+      i18next.t("labelCopyRight");
+  };
+  const upLoadSvg = () => {
+    document.getElementById("logSvg").innerHTML = svgGroup.logSvg;
   };
 
-  var i18nLng = window.localStorage.get("lng");
-  if (!i18nLng) {
-    i18nLng = "en";
-  }
-  onlanguagechange(value);
+  window.onload = () => {
+    i18next
+      // .use(i18nextHttpBackend)
+      .use(i18nextBrowserLanguageDetector)
+      .init(
+        {
+          resources,
+          debug: true,
+          ns: ["common"],
+          defaultNS: "common",
+          lng: initLng,
+          fallbackLng: initLng,
+          keySeparator: ".", // we do not use keys in form messages.welcome
+          interpolation: {
+            escapeValue: true, // react already safes from xss
+            formatSeparator: `, `,
+          },
+        },
+        function (err, t) {
+          // init set content
+          updateI18n();
+        }
+      );
+    var i18nLng = window.localStorage.getItem("lng");
+    if (!i18nLng) {
+      i18nLng = "en";
+    }
+    upLoadSvg();
+    onlanguagechange(i18nLng);
+  };
 })();
