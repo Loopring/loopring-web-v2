e3c0bbe43835ba5fa765048d7d5cda04a6ab60a0
windatang <windatang@gmail.com>
2023-06-09 10:28:46 +0800

    wallet connect 2.0

--- packages/webapp/src/pages/ProTradePage/panel/walletInfo/stopLimitInfo.tsx
+++ packages/webapp/src/pages/ProTradePage/panel/walletInfo/stopLimitInfo.tsx
@@ -8,11 +8,12 @@ import {
   ListItem,
   Typography,
 } from "@mui/material";
-import { SoursURL } from "@loopring-web/common-resources";
+import { SoursURL, ThemeType } from "@loopring-web/common-resources";
 import React from "react";
 
 export const StopLimitInfo = withTranslation("common")(
   ({ t }: WithTranslation) => {
+    const theme = useTheme();
     return (
       <Box
         display={"flex"}
@@ -117,7 +118,12 @@ export const StopLimitInfo = withTranslation("common")(
                 }}
                 alt={""}
                 // src={sellData?.icon}
-                src={SoursURL + "images/stoplimitDemo.webp"}
+                src={
+                  SoursURL +
+                  (theme.mode === ThemeType.dark
+                    ? "images/stoplimit_dark.svg"
+                    : "images/stoplimit_light.svg")
+                }
               />
             </Typography>
           </Box>
