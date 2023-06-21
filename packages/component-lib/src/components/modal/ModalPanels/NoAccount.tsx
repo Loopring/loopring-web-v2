2a2c1baca1583205b765da83b7fa7b9fd1a2380a
windatang <windatang@gmail.com>
2023-06-22 01:23:09 +0800

    add

--- packages/component-lib/src/components/modal/ModalPanels/NoAccount.tsx
+++ packages/component-lib/src/components/modal/ModalPanels/NoAccount.tsx
@@ -28,42 +28,81 @@ export const NoAccount = withTranslation("common")(
       chainInfos: AccountHashInfo;
       clearDepositHash: () => void;
     }) => {
-      const theme = useTheme();
-      const { defaultNetwork } = useSettings();
-      const network = MapChainId[defaultNetwork] ?? MapChainId[1];
-      return (
+    const theme = useTheme();
+    const { defaultNetwork } = useSettings();
+    const network = MapChainId[defaultNetwork] ?? MapChainId[1];
+    return (
+      <Box
+        flex={1}
+        display={"flex"}
+        flexDirection={"column"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        className={className}
+        // style={{ transform: "translateY(-40px)" }}
+      >
         <Box
-          flex={1}
           display={"flex"}
-          flexDirection={"column"}
-          justifyContent={"space-between"}
+          flex={1}
+          marginBottom={5}
+          justifyContent={"center"}
           alignItems={"center"}
-          className={className}
-          // style={{ transform: "translateY(-40px)" }}
         >
+          <AccountBasePanel {...props} t={t} />
+        </Box>
+        {noButton ? (
           <Box
             display={"flex"}
-            flex={1}
-            marginBottom={5}
-            justifyContent={"center"}
-            alignItems={"center"}
+            marginX={0}
+            marginTop={3}
+            marginBottom={0}
+            alignSelf={"stretch"}
+            paddingX={5}
+            padding={0}
+            sx={{
+              overflow: "hidden",
+              borderBottomLeftRadius: theme.unit,
+              borderBottomRightRadius: theme.unit,
+            }}
           >
-            <AccountBasePanel {...props} t={t} />
+            <DepositRecorder {...props} clear={props.clearDepositHash} t={t} />
           </Box>
-          {noButton ? (
+        ) : (
+          <>
+            <Box
+              display={"flex"}
+              marginTop={2}
+              alignSelf={"stretch"}
+              paddingX={5}
+              flexDirection={"column"}
+              alignItems={"center"}
+            >
+              <Typography variant={"body2"}>
+                {t("labelActivatedAccountDeposit", {
+                  layer2: L1L2_NAME_DEFINED[network].layer2,
+                })}
+              </Typography>
+              <AnimationArrow className={"arrowCta"} />
+              <Button
+                variant={"contained"}
+                fullWidth
+                size={"medium"}
+                onClick={() => {
+                  goActiveAccount();
+                }}
+              >
+                {t("labelActiveL2Btn", {
+                  loopringL2: L1L2_NAME_DEFINED[network].loopringL2,
+                })}
+              </Button>
+            </Box>
             <Box
               display={"flex"}
               marginX={0}
               marginTop={3}
-              marginBottom={0}
               alignSelf={"stretch"}
               paddingX={5}
               padding={0}
-              sx={{
-                overflow: "hidden",
-                borderBottomLeftRadius: theme.unit,
-                borderBottomRightRadius: theme.unit,
-              }}
             >
               <DepositRecorder
                 {...props}
@@ -71,52 +110,9 @@ export const NoAccount = withTranslation("common")(
                 t={t}
               />
             </Box>
-          ) : (
-            <>
-              <Box
-                display={"flex"}
-                marginTop={2}
-                alignSelf={"stretch"}
-                paddingX={5}
-                flexDirection={"column"}
-                alignItems={"center"}
-              >
-                <Typography variant={"body2"}>
-                  {t("labelActivatedAccountDeposit", {
-                    layer2: L1L2_NAME_DEFINED[network].layer2,
-                  })}
-                </Typography>
-                <AnimationArrow className={"arrowCta"} />
-                <Button
-                  variant={"contained"}
-                  fullWidth
-                  size={"medium"}
-                  onClick={() => {
-                    goActiveAccount();
-                  }}
-                >
-                  {t("labelActiveL2Btn", {
-                    loopringL2: L1L2_NAME_DEFINED[network].loopringL2,
-                  })}
-                </Button>
-              </Box>
-              <Box
-                display={"flex"}
-                marginX={0}
-                marginTop={3}
-                alignSelf={"stretch"}
-                paddingX={5}
-                padding={0}
-              >
-                <DepositRecorder
-                  {...props}
-                  clear={props.clearDepositHash}
-                  t={t}
-                />
-              </Box>
-            </>
-          )}
-        </Box>
-      );
-    }
+          </>
+        )}
+      </Box>
+    );
+  }
 );
