b42d98be95e02037b764c8f3844a5db07e1e7ff0
windatang <windatang@gmail.com>
2023-06-28 19:11:59 +0800

    package update with log

--- packages/webapp/src/pages/LandPage/Card.tsx
+++ packages/webapp/src/pages/LandPage/Card.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { WithTranslation, withTranslation } from "react-i18next";
 import { Box, styled, Typography } from "@mui/material";
 import { useSettings } from "@loopring-web/component-lib";
-// import { useSpring } from "react-spring";
-// import { animated, to } from "@react-spring/web";
+import { useSpring } from "react-spring";
+import { animated, to } from "@react-spring/web";
 import { useTheme } from "@emotion/react";
 import { ThemeType } from "@loopring-web/common-resources";
 
@@ -13,27 +13,31 @@ export type CardProps = {
   animationJSON: string;
   describe: string;
 };
-// const BoxStyle = styled(animated.div)`
-//   //width: 400px !important;
-//   svg {
-//     fill: ${({ theme }: any) => theme.colorBase.textPrimary};
-//     .svg-high {
-//       fill: ${({ theme }: any) => theme.colorBase.primary};
-//     }
-//   }
-//   :hover {
-//     svg {
-//       fill: ${({ theme }: any) => theme.colorBase.textButton};
-//       .svg-high {
-//         fill: ${({ theme }: any) => theme.colorBase.star};
-//       }
-//     }
-//     p,
-//     h3 {
-//       color: ${({ theme }: any) => theme.colorBase.textButton};
-//     }
-//   }
-// ` as unknown as typeof animated.div;
+const BoxStyle = styled(animated.div)`
+  //width: 400px !important;
+  svg {
+    fill: ${({ theme }: any) => theme.colorBase.textPrimary};
+
+    .svg-high {
+      fill: ${({ theme }: any) => theme.colorBase.primary};
+    }
+  }
+
+  :hover {
+    svg {
+      fill: ${({ theme }: any) => theme.colorBase.textButton};
+
+      .svg-high {
+        fill: ${({ theme }: any) => theme.colorBase.star};
+      }
+    }
+
+    p,
+    h3 {
+      color: ${({ theme }: any) => theme.colorBase.textButton};
+    }
+  }
+` as unknown as typeof animated.div;
 export const Card = withTranslation(["landPage", "common"], { withRef: true })(
   ({
     t,
@@ -48,71 +52,65 @@ export const Card = withTranslation(["landPage", "common"], { withRef: true })(
     describe: string;
   }) => {
     const theme = useTheme();
-    // const [styles, api] = useSpring(() => ({
-    //   scale: 1,
-    //   zoom: 1,
-    //   zIndex: 10,
-    //   border: `1px solid ${theme.colorBase.border}`, //"var(--border-card)",
-    //   boxShadow: "var(--shadow3)", //theme.colorBase.boxShadow, //"var(--box-card-shadow)",
-    //   background: theme.mode === ThemeType.dark ? "#283485" : "#fff", //"var(--box-card-background)",
-    //   default: {
-    //     immediate: (key) => {
-    //       return [
-    //         "scale",
-    //         "zoom",
-    //         "zIndex",
-    //         "border",
-    //         "boxShadow",
-    //         "background",
-    //       ].includes(key);
-    //     },
-    //   },
-    //   config: {
-    //     mass: 5,
-    //     tension: 350,
-    //     friction: 40,
-    //   },
-    // }));
+    const [styles, api] = useSpring(() => ({
+      scale: 1,
+      zoom: 1,
+      zIndex: 10,
+      border: `1px solid ${theme.colorBase.border}`, //"var(--border-card)",
+      boxShadow: "var(--shadow3)", //theme.colorBase.boxShadow, //"var(--box-card-shadow)",
+      background: theme.mode === ThemeType.dark ? "#283485" : "#fff", //"var(--box-card-background)",
+      default: {
+        immediate: (key) => {
+          return [
+            "scale",
+            "zoom",
+            "zIndex",
+            "border",
+            "boxShadow",
+            "background",
+          ].includes(key);
+        },
+      },
+      config: {
+        mass: 5,
+        tension: 350,
+        friction: 40,
+      },
+    }));
     const { isMobile } = useSettings();
     return (
-      <Box
-        // onMouseEnter={() =>
-        //   api({
-        //     scale: 1.1,
-        //     zoom: 1,
-        //     zIndex: 99,
-        //     border: "0px solid #fff",
-        //     boxShadow:
-        //       "inset 0px -16px 0px var(--border-card-hover), inset 0px 16px 0px var(--border-card-hover)",
-        //     background: "var(--box-card-background-hover)",
-        //   })
-        // }
+      <BoxStyle
+        onMouseEnter={() =>
+          api({
+            scale: 1.1,
+            zoom: 1,
+            zIndex: 99,
+            border: "0px solid #fff",
+            boxShadow:
+              "inset 0px -16px 0px var(--border-card-hover), inset 0px 16px 0px var(--border-card-hover)",
+            background: "var(--box-card-background-hover)",
+          })
+        }
         className={"card"}
-        // onMouseLeave={() => {
-        //   api({
-        //     scale: 1,
-        //     zoom: 1,
-        //     zIndex: 10,
-        //     border: "var(--border-card)",
-        //     boxShadow: "var(--box-card-shadow)",
-        //     background: "var(--box-card-background)",
-        //   });
-        // }}
+        onMouseLeave={() => {
+          api({
+            scale: 1,
+            zoom: 1,
+            zIndex: 10,
+            border: "var(--border-card)",
+            boxShadow: "var(--box-card-shadow)",
+            background: "var(--box-card-background)",
+          });
+        }}
         style={{
-          scale: 1,
-          zoom: 1,
-          zIndex: 10,
-          border: `1px solid ${theme.colorBase.border}`, //"var(--border-card)",
-          boxShadow: "var(--shadow3)", //theme.colorBase.boxShadow, //"var(--box-card-shadow)",
-          background: theme.mode === ThemeType.dark ? "#283485" : "#fff", //"var(--box-card-background)",
-          // transform: "perspective(600px)",
-          // height: 480,
-          // width: isMobile ? "var(--mobile-full-panel-width)" : 400,
-          // zIndex: to([styles.zIndex], (zIndex) => zIndex),
-          // background: to([styles.background], (background) => background),
-          // boxShadow: to([styles.boxShadow], (boxShadow) => boxShadow),
-          // border: to([styles.border], (border) => border),
-          // scale: to([styles.scale, styles.zoom], (s, z) => s * z),
+          transform: "perspective(600px)",
+          height: 480,
+          width: isMobile ? "var(--mobile-full-panel-width)" : 400,
+          zIndex: to([styles.zIndex], (zIndex) => zIndex),
+          background: to([styles.background], (background) => background),
+          boxShadow: to([styles.boxShadow], (boxShadow) => boxShadow),
+          border: to([styles.border], (border) => border),
+          scale: to([styles.scale, styles.zoom], (s, z) => s * z),
         }}
       >
         <Box marginTop={4}>{icon}</Box>
@@ -150,7 +148,7 @@ export const Card = withTranslation(["landPage", "common"], { withRef: true })(
         {/*    style={{ transform: y.interpolate(v => `translateY(${v}%`) }}*/}
         {/*    className="glance"*/}
         {/*/>*/}
-      </Box>
+      </BoxStyle>
     );
   }
 );
