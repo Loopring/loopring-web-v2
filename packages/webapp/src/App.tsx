156a0ac064dd7c2e74b8bdb6eab76b3b569a57b9
windatang <windatang@gmail.com>
2023-07-07 20:19:52 +0800

    Merge remote-tracking branch 'origin/released' into staging
    # Conflicts:
    #	packages/common-resources/static-resources/src/constant/trade.ts
    #	packages/common-resources/static-resources/src/i18n/en_US/common.ts
    #	packages/common-resources/static-resources/src/i18n/en_US/tables.ts
    #	packages/common-resources/static-resources/src/i18n/zh_CN/common.ts
    #	packages/common-resources/static-resources/src/i18n/zh_CN/tables.ts
    #	packages/common-resources/static-resources/src/loopring-interface/CoinInterface.ts
    #	packages/component-lib/src/components/block/Interface.ts
    #	packages/component-lib/src/components/block/RedPacket.tsx
    #	packages/component-lib/src/components/modal/ModalPanels/CreateRedPacketPanel.tsx
    #	packages/component-lib/src/components/tableList/redPacketTable/RedPacketBlindBoxReceiveTable.tsx
    #	packages/component-lib/src/components/tableList/redPacketTable/RedPacketClaimTable.tsx
    #	packages/component-lib/src/components/tableList/redPacketTable/RedPacketReceiveTable.tsx
    #	packages/component-lib/src/components/tableList/redPacketTable/RedPacketRecodTable.tsx
    #	packages/component-lib/src/components/tradePanel/components/CreateRedPacketWrap.tsx
    #	packages/component-lib/src/components/tradePanel/components/Interface.ts
    #	packages/core/src/hooks/useractions/useCreateRedPacket.ts
    #	packages/core/src/modal/AccountModal/components/NFTDetail.tsx
    #	packages/core/src/modal/RedPacketModal/hook.tsx
    #	packages/webapp/src/pages/RedPacketPage/MyRedPacketPanel/hooks.ts
    #	packages/webapp/src/pages/RedPacketPage/RedPacketClaimPanel/index.tsx

--- packages/webapp/src/App.tsx
+++ packages/webapp/src/App.tsx
@@ -1,50 +1,47 @@
-// import { ModalProvider } from 'styled-react-modal'
-import RouterView from "./routers";
-import { GlobalStyles } from "@mui/material";
-import { css, Theme, useTheme } from "@emotion/react";
-import { globalCss } from "@loopring-web/common-resources";
-import { setLanguage } from "@loopring-web/component-lib";
-import { useInit } from "./hook";
-import React from "react";
-import { useTranslation } from "react-i18next";
+import RouterView from './routers'
+import { GlobalStyles } from '@mui/material'
+import { css, Theme, useTheme } from '@emotion/react'
+import { globalCss } from '@loopring-web/common-resources'
+import { setLanguage } from '@loopring-web/component-lib'
+import { useInit } from './hook'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
 
-import { HashRouter as Router, useLocation } from "react-router-dom";
-import { store } from "@loopring-web/core";
+import { HashRouter as Router, useLocation } from 'react-router-dom'
+import { store } from '@loopring-web/core'
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const { pathname } = useLocation()
 
   React.useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    window.scrollTo(0, 0)
+  }, [pathname])
 
-  return null;
-};
+  return null
+}
 const App = () => {
-  const theme: Theme = useTheme();
+  const theme: Theme = useTheme()
   const {
     i18n: { language },
-  } = useTranslation();
-  const storeLan = store.getState().settings.language;
+  } = useTranslation()
+  const storeLan = store.getState().settings.language
 
   React.useEffect(() => {
     if (storeLan !== language) {
-      store.dispatch(setLanguage(language));
+      store.dispatch(setLanguage(language))
     }
-  }, [storeLan, language]);
+  }, [storeLan, language])
 
   React.useEffect(() => {
-    if (window.location.protocol !== "https:") {
-      console.log("Current PROTOCOL::", window.location.protocol);
+    if (window.location.protocol !== 'https:') {
+      console.log('Current PROTOCOL::', window.location.protocol)
       window.location.replace(
-        `https:${window.location.href.substring(
-          window.location.protocol.length
-        )}`
-      );
+        `https:${window.location.href.substring(window.location.protocol.length)}`,
+      )
     }
-  }, []);
+  }, [])
 
-  const { state } = useInit();
+  const { state } = useInit()
 
   return (
     <>
@@ -54,7 +51,7 @@ const App = () => {
 
           body {
             ${
-              theme.mode === "dark"
+              theme.mode === 'dark'
                 ? `
             color: ${theme.colorBase.textPrimary};
           `
@@ -66,11 +63,11 @@ const App = () => {
 
           body:before {
             ${
-              theme.mode === "dark"
+              theme.mode === 'dark'
                 ? `
             background: var(--color-global-bg);
        `
-                : ""
+                : ''
             }
           }
         }`}
@@ -81,10 +78,10 @@ const App = () => {
         <RouterView state={state} />
       </Router>
     </>
-  );
-};
-const h = new Headers();
-export default App;
+  )
+}
+const h = new Headers()
+export default App
 
 // new ContactAPI({chainId: 1, baseUrl: 'https://uat2.loopring.io'})
 // .getContacts({
