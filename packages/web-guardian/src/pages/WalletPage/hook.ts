98f690214712aeb2d99ca43f7b6d44ee50b9ca86
john.li <john.li@loopring.io>
2023-07-14 15:35:33 +0800

    sprettier

--- packages/web-guardian/src/pages/WalletPage/hook.ts
+++ packages/web-guardian/src/pages/WalletPage/hook.ts
@@ -48,10 +48,12 @@ export const useHebaoMain = <
   H extends sdk.HebaoOperationLog,
 >() => {
   const { account, status: accountStatus } = useAccount()
-  const [loopringSmartContractWallet, setLoopringSmartContractWallet] =
-    React.useState<boolean | undefined>(undefined)
-  const [nonLoopringSmartContractWallet, setNonLoopringSmartContractWallet] =
-    React.useState<boolean | undefined>(undefined)
+  const [loopringSmartContractWallet, setLoopringSmartContractWallet] = React.useState<
+    boolean | undefined
+  >(undefined)
+  const [nonLoopringSmartContractWallet, setNonLoopringSmartContractWallet] = React.useState<
+    boolean | undefined
+  >(undefined)
 
   const [{ guardianConfig, protectList, guardiansList, operationLogList }, setList] =
     React.useState<{
