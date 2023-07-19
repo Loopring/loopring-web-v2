6dae3e3504a6f3be22b44e10abb743025d8267c2
windatang <windatang@gmail.com>
2023-07-20 01:32:16 +0800

    provider update

--- packages/webapp/src/pages/NFTPage/index.tsx
+++ packages/webapp/src/pages/NFTPage/index.tsx
@@ -3,7 +3,7 @@ import { useRouteMatch } from 'react-router-dom'
 import { Box } from '@mui/material'
 import { subMenuNFT } from '@loopring-web/common-resources'
 import React from 'react'
-import { ViewAccountTemplate } from '@loopring-web/core'
+import { useAccount, ViewAccountTemplate } from '@loopring-web/core'
 import { MyNFTPanel } from './MyNFT'
 import { MyNFTHistory } from './NFThistory'
 import { MintNFTAdvancePanel, MintNFTPanel } from './MintNFTPanel'
@@ -12,13 +12,15 @@ import { NFTCollectPanel } from './CollectionPanel'
 import { EditCollectionPanel } from './EditCollectionPanel'
 import { MintLandingPage } from './components/landingPanel'
 import { ImportCollectionPanel } from './ImportCollectionPanel'
+import { CollectionHadUnknown } from '@loopring-web/component-lib'
 
 export const subMenu = subMenuNFT
 
 export const NFTPage = () => {
   let match: any = useRouteMatch('/NFT/:item')
   const selected = match?.params?.item ?? 'assetsNFT'
-
+  const account = useAccount()
+  const [open, setOpen] = React.useState(false)
   const routerNFT = React.useMemo(() => {
     switch (selected) {
       case 'transactionNFT':
@@ -43,6 +45,11 @@ export const NFTPage = () => {
         return <MyNFTPanel />
     }
   }, [selected])
+  React.useEffect(() => {
+    if (account?.hasUnknownCollection) {
+      setOpen(true)
+    }
+  }, [])
 
   const activeViewTemplate = React.useMemo(
     () => (
@@ -64,6 +71,7 @@ export const NFTPage = () => {
 
   return (
     <>
+      <CollectionHadUnknown open={open} onClose={() => setOpen(false)} />
       <ViewAccountTemplate activeViewTemplate={activeViewTemplate} />
     </>
   )
