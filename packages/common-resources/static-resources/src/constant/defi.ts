2285c07aa26eeb3e49c8daca4e4427e6b0fc0cb6
john.li <john.li@loopring.io>
2023-07-26 17:55:06 +0800

    leverage ETH

---
+++ packages/common-resources/static-resources/src/constant/defi.ts
@@ -0,0 +1,10 @@
+export const defiMarkets = {
+  TAIKO: [] as string[],
+  ETHEREUM: ['RETH-ETH', 'WSTETH-ETH'],
+  GOERLI: ['RETH-ETH'],
+}
+export const leverageETHMarkets = {
+  TAIKO: [] as string[],
+  ETHEREUM: ['CiETH-ETH'],
+  GOERLI: ['WSTETH-ETH'],
+}
