2126950dc12e63006e6ed184f1b898ea5c5b8073
windatang <windatang@gmail.com>
2023-08-05 22:25:33 +0800

    lookasset

--- packages/common-resources/static-resources/src/i18n/zh_CN/common.ts
+++ packages/common-resources/static-resources/src/i18n/zh_CN/common.ts
@@ -1742,10 +1742,10 @@ export default {
   labelClaimTypeREBATE_FEE: 'Maker Order Rewards',
   labelClaimTypeRECOMMENDER_FEE: 'Referral Rewards',
   labelClaimTypeREFERER_FEE: 'Refunds Rewards',
-  labelClaimTypePROTOCOL_FEE: 'AMM Pool Rewards',
+  labelClaimTypePROTOCOL_FEE: 'Dedicated AMM Incentive',
   labelClaimTypeLRC_STAKING: 'LRC Staking Rewards',
   labelClaimOtherRewards: 'Other Rewards',
-  labelAMMClaimableEarnings: 'Protocol Fee Rewards',
+  labelAMMClaimableEarnings: 'Dedicated AMM Incentive',
   labelLayer2HistoryOrders: 'Order Records',
   labelLayer2HistoryStopLimitRecords: 'Stop-Limit Records',
   labelLayer2HistoryDefiRecords: 'ETH Staking',
@@ -1756,13 +1756,21 @@ export default {
   labelRedPacketTimeRangeBlindboxDesERC20:
     'The Reveal Time is when the Red Packet ends, and recipients can open it to see if they have received tokens',
   labelBlindBoxExpirationExplainationForTokenBlindbox:
-    "'If the recipients of the Tokens Red Packets do not open their received Tokens, the Tokens will be forfeited and sent back to the Sender's wallet.'",
+    "If the recipients of the Tokens Red Packets do not open their received Tokens, the Tokens will be forfeited and sent back to the Sender's wallet.",
   labelBlindBoxRecieved: 'Received {{deliverdGiftsAmount}}/{{totalGiftsAmount}}',
   labelBlindBoxClaimHint: 'You can visit Assets > Red Packets to claim your rewards.',
   labelRedPacketBlindboxReceived1: 'ERC20 Blind Box',
   labelRedPacketBlindboxReceived2: 'Received',
   labelRedPacketsGiftsEqualsZero: "Number of gifts can't be zero",
   labelRedpacketStandard: 'Standard',
+  labelCiETHDefiRiskTitle: "What's Cian Leveraged ETH?",
+  labelCiETHDefiRisk:
+    '<p>CIAN protocol is a liquid staking derivatives (“LSD”) focused yield strategy platform, where users could earn either through joining algorithmic strategy vaults or through building their own DeFi strategies using CIAN’s advanced automation tools.</p>' +
+    "<p>The stETH/ETH leveraged staking strategy enables users to safely leverage stETH’s staking rewards. This strategy focuses on staking derivatives and protection/optimization tooling. By nature, this strategy is to use the user's asset as collateral to borrow ETH from lending platforms, then stake ETH in Lido to earn ETH staking interest. By utilizing tools like Flashloan, it actually adds leverage to users' ETH investment. If only there is a positive APY diff between the borrowing rate and ETH staking rate, there will be additional earnings from this strategy vs. standard ETH staking.</p>" +
+    '<p>It is quite important to understand that, when using such leveraged strategy, it’s highly advised to intend on holding that position for a while. By doing so, users will give enough time for the high APY to cover their entry & exit costs.</p>\n',
+  labelDefiWithdrawFee:
+    '<p>Finally, there will be an additional 0.05% exit fee incurred, which will be charged at redemption time.</p>',
+  labelLeverageETHTitle: 'Leveraged ETH',
   labelLayer2HistoryleverageETHRecords: 'Leveraged ETH',
   labelSwapMinConverted: 'Minimum Converted',
   labelSwapMinConvertedTooltip:
@@ -1775,6 +1783,17 @@ export default {
   labelStopPriceDes:
     'After the "Stop Price" is triggered, the relayer will automatically place a limit order at this price. ',
   labelStopPriceSell: 'Limit / Sell Price',
+  labelClaimallToken: 'My Rewards',
+  labelConnecting: 'Connecting',
+  labelHadUnknownCollectionTitle: 'Import Collection for Legacy NFT',
+  labelHadUnknownCollectionDes:
+    'As the creator, you will be able to generate collection information for those NFT minted earlier that belong to nowhere. And once done, the other people holding your NFT will be able to view those NFT with proper collection information via loopring.io and loopring wallet. ',
+  labelGo: 'Go',
+  labelAnotherNetworkDes2:
+    'Note: Please ensure to check out the "Change Account" option and input the recipient\'s address carefully. If you want to send token to network other than ethereum, the recipient address must be different than the sender address; else you will lose that asset for ever.',
+  labelAnotherNetworkDes3: '',
+  labelRiskReminder: 'Risk Reminder',
+  labelDefiRedeem: 'Redeem',
   labelDefiSubscribe: 'Subscribe',
   labelDefiMaxBalance1Leverage:
     '<li>The Loopring pool will rebalance soon. Please come back later to redeem.</li>',
@@ -1788,4 +1807,5 @@ export default {
   labelLeverageETHBack: 'Leveraged ETH Staking',
   labelInvestType_LEVERAGEETH: 'Leveraged ETH Staking',
   labelRewardRefresh: 'Refresh',
+  labelToMyL2WidthAddress: 'My {{loopringL2}} <1>({{address}})</1>',
 }
--- packages/common-resources/static-resources/src/themes/overrides/muTheme.ts
+++ packages/common-resources/static-resources/src/themes/overrides/muTheme.ts
@@ -47,6 +47,7 @@ import { LoopringTheme, ThemeKeys } from '../interface'
 
 export { unit }
 export const getTheme = (themeMode: ThemeKeys, _isMobile = false): LoopringTheme => {
+  // @ts-ignore
   const colorBase: typeof ColorDarkDefault =
     themeMode === 'dark' ? ColorDarkDefault : ColorLightDefault
   const theme = createTheme({
@@ -208,5 +209,5 @@ export const getTheme = (themeMode: ThemeKeys, _isMobile = false): LoopringTheme
       fontDefault,
       colorBase: themeMode === 'dark' ? ColorDarkDefault : ColorLightDefault,
     },
-  }
+  } as LoopringTheme
 }
