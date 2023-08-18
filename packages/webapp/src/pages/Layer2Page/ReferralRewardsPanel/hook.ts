65068c4727e8b3c4bc17fe0ff16deb97a723bf42
john.li <john.li@loopring.io>
2023-08-18 14:35:41 +0800

    fix a bug

--- packages/webapp/src/pages/Layer2Page/ReferralRewardsPanel/hook.ts
+++ packages/webapp/src/pages/Layer2Page/ReferralRewardsPanel/hook.ts
@@ -10,8 +10,6 @@ import {
   SagaStatus,
   SDK_ERROR_MAP_TO_UI,
 } from '@loopring-web/common-resources'
-import { GetReferStatisticReason } from '@loopring-web/loopring-sdk/src/defs/loopring_defs'
-
 export function useRefundTable<R = RefundRow>(setToastOpen: (state: any) => void) {
   const {
     account: { accountId, apiKey },
@@ -85,7 +83,7 @@ export function useRefundTable<R = RefundRow>(setToastOpen: (state: any) => void
         ?.getReferStatistic<sdk.ReferStatistic>(
           {
             accountId: accountId.toString(),
-            reason: GetReferStatisticReason.Recommender,
+            reason: sdk.GetReferStatisticReason.Recommender,
           },
           apiKey,
         )
@@ -214,7 +212,7 @@ export function useReferralsTable<R = ReferralsRow>(setToastOpen: (state: any) =
         ?.getReferStatistic<sdk.ReferStatistic>(
           {
             accountId: accountId.toString(),
-            reason: GetReferStatisticReason.Invited,
+            reason: sdk.GetReferStatisticReason.Invited,
           },
           apiKey,
         )
