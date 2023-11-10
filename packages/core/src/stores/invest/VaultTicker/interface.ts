d2af49b8af239de755f572d4c2194c1e2536b927
windatang <windatang@gmail.com>
2023-11-10 22:47:00 +0800

    notice

--- packages/core/src/stores/invest/VaultTicker/interface.ts
+++ packages/core/src/stores/invest/VaultTicker/interface.ts
@@ -1,9 +1,9 @@
 import { StateBase, TickerNew } from '@loopring-web/common-resources'
 
-export type TickerMap<R = { [key: string]: any }> = {
+export type VaultTickerMap<R = { [key: string]: any }> = {
   [key in keyof R]: TickerNew
 }
 export type VaultTickerStates<C = { [key: string]: any }> = {
-  vaultTickerMap: TickerMap<C>
+  vaultTickerMap: VaultTickerMap<C>
   __timer__?: number
 } & StateBase
