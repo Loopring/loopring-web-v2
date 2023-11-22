41a300ee43c078b9e04e0005d18f8e46a4da3e00
windatang <windatang@gmail.com>
2023-11-22 14:10:17 +0800

    new Loopring.io

--- packages/earn-l1-manage/src/pages/AssetPage/index.tsx
+++ packages/earn-l1-manage/src/pages/AssetPage/index.tsx
@@ -1,3 +1,7 @@
+import * as dualManageConfig from '../../config/dualConfig.json'
+import { useTokenMap } from '@loopring-web/core'
 export const AssetPage = () => {
+  // dualManageConfig.tokenList
+  const { tokenMap } = useTokenMap()
   return <></>
 }
