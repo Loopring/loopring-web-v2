9122dd3067b74304d5fd5d5d9a720d0e81d9ed20
windatang <windatang@gmail.com>
2023-12-26 18:26:09 +0800

    vault
    (cherry picked from commit 642df38cdc3f36554df468c26c0bfd94ad68551d)

--- packages/core/src/stores/system/saga.ts
+++ packages/core/src/stores/system/saga.ts
@@ -342,7 +342,7 @@ const should15MinutesUpdateDataGroup = async (
     }
   }
   return {
-    forexMap: {} as ForexMap<sdk.Currency>,
+    forexMap: {} as ForexMap,
     gasPrice: undefined,
   }
 }
