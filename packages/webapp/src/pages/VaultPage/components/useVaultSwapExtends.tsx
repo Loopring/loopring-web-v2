1151f27bd972ab5310b2fa33bdbb7ec93071d6ed
john.li <john.li@loopring.io>
2024-02-23 11:49:14 +0800

    line 32

--- packages/webapp/src/pages/VaultPage/components/useVaultSwapExtends.tsx
+++ packages/webapp/src/pages/VaultPage/components/useVaultSwapExtends.tsx
@@ -264,6 +264,7 @@ export const useVaultSwapExtends = ({
                             toastOpen.type == ToastType.error &&
                             toastOpen.step == tradeCalcData.step
                           }
+                          sx={{width: '120px'}}
                         >
                           {label}
                         </StepLabel>
