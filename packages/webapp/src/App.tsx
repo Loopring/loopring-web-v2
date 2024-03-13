d77a519a5ecdfe8b143a1c9f08af0b5a45444e75
john.li <john.li@loopring.io>
2024-03-13 15:30:14 +0800

    eip6963

--- packages/webapp/src/App.tsx
+++ packages/webapp/src/App.tsx
@@ -2,20 +2,21 @@ import RouterView from './routers'
 import { GlobalStyles } from '@mui/material'
 import { css, Theme, useTheme } from '@emotion/react'
 import { globalCss, SagaStatus } from '@loopring-web/common-resources'
-import { setDefaultNetwork, setLanguage, useSettings } from '@loopring-web/component-lib'
+import { setDefaultNetwork, setLanguage } from '@loopring-web/component-lib'
 import { useInit } from './hook'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { HashRouter as Router, useLocation } from 'react-router-dom'
-import { checkAccount, store, useAccount, useSystem } from '@loopring-web/core'
-import { ConnectProviders, walletServices } from '@loopring-web/web3-provider'
-import { useWeb3Modal, useWeb3ModalAccount, useWeb3ModalProvider } from '@web3modal/ethers5/react'
-import { ChainId } from '@loopring-web/loopring-sdk'
+import { checkAccount, store, useAccount, useSystem, web3Modal } from '@loopring-web/core'
+import { ConnectProviders, connectProvides, walletServices } from '@loopring-web/web3-provider'
+import { useWeb3ModalAccount, useWeb3ModalProvider } from '@web3modal/ethers5/react'
 import { updateSystem } from '@loopring-web/core/src/stores/system/reducer'
 import { updateAccountStatus } from '@loopring-web/core/src/stores/account/reducer'
 import { providers } from 'ethers'
 import { useDispatch } from 'react-redux'
+import Web3 from "web3";
+
 
 const ScrollToTop = () => {
   const { pathname } = useLocation()
@@ -26,6 +27,7 @@ const ScrollToTop = () => {
 
   return null
 }
+
 const App = () => {
   const theme: Theme = useTheme()
   const {
@@ -47,7 +49,6 @@ const App = () => {
   React.useEffect(() => {
     ;(async () => {
       if (address) {
-
         dispatch(
           updateAccountStatus({
             connectName: ConnectProviders.MetaMask
@@ -67,9 +68,19 @@ const App = () => {
       }
     })()
   }, [address, walletProvider, status, chainId])
-
-
   
+  React.useEffect(() => {
+    const provider = web3Modal.getWalletProvider()
+    connectProvides.usedProvide = new providers.Web3Provider(provider as any)
+    // @ts-ignore
+    connectProvides.usedWeb3 = new Web3(provider as any)
+    web3Modal.subscribeProvider(async (pro) => {
+      connectProvides.usedProvide = new providers.Web3Provider(pro.provider as any)
+      // @ts-ignore
+      connectProvides.usedWeb3 = new Web3(pro.provider as any)
+    })
+  }, [])
+
   React.useEffect(() => {
     if (window.location.protocol !== 'https:') {
       console.log('Current PROTOCOL::', window.location.protocol)
