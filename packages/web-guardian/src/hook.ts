450af5eb2c6707ca57052130325695a6f0f6db65
john.li <john.li@loopring.io>
2024-04-10 11:44:21 +0800

    fix unkonw wallet bug

--- packages/web-guardian/src/hook.ts
+++ packages/web-guardian/src/hook.ts
@@ -75,9 +75,7 @@ export function useInit() {
     }
     ;(async (account) => {
       if (
-        account.accAddress !== '' &&
-        account.connectName &&
-        account.connectName !== ConnectProviders.Unknown
+        account.accAddress !== ''
       ) {
         try {
           ConnectProvides.IsMobile = isMobile
@@ -116,7 +114,7 @@ export function useInit() {
           }
         }
       } else {
-        if (account.accAddress === '' || account.connectName === ConnectProviders.Unknown) {
+        if (account.accAddress === '') {
           resetAccount()
         }
         const chainId =
