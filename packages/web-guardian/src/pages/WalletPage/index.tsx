6d29c5c38a01b5b7881abbb10f9c9362775a9afb
john.li <john.li@loopring.io>
2024-08-07 17:24:28 +0800

    update networkName

--- packages/web-guardian/src/pages/WalletPage/index.tsx
+++ packages/web-guardian/src/pages/WalletPage/index.tsx
@@ -140,9 +140,7 @@ export const GuardianPage = withTranslation(['common'])(({ t, ..._rest }: WithTr
   const { account } = useAccount()
   const { defaultNetwork, isMobile } = useSettings()
   const network = MapChainId[defaultNetwork] ?? MapChainId[1]
-  const networkName: sdk.NetworkWallet = ['ETHEREUM', 'GOERLI', 'SEPOLIA'].includes(network)
-    ? sdk.NetworkWallet.ETHEREUM
-    : sdk.NetworkWallet[network]
+  const networkName: sdk.NetworkWallet = sdk.NetworkWallet[network]
   const [openQRCode, setOpenQRCode] = React.useState(false)
   const onOpenAdd = React.useCallback((open: boolean) => {
     setOpenQRCode(open)
