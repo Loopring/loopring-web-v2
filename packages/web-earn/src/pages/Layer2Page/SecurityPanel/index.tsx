c4a0a05ac8f3d5301953334944b5ca26097470d9
john.li <john.li@loopring.io>
2025-01-21 10:46:03 +0800

    dontmerge, add export account to taiko

--- packages/web-earn/src/pages/Layer2Page/SecurityPanel/index.tsx
+++ packages/web-earn/src/pages/Layer2Page/SecurityPanel/index.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled'
 import { Box, Button, Grid, Typography } from '@mui/material'
 import React from 'react'
 import { Trans, WithTranslation, withTranslation } from 'react-i18next'
-import { useResetAccount } from './hook'
+import { useExportAccountInfo, useResetAccount } from './hook'
 import { useSettings } from '@loopring-web/component-lib'
 import { useAccount } from '@loopring-web/core'
 import {
@@ -19,6 +19,7 @@ const StyledPaper = styled(Grid)`
 export const SecurityPanel = withTranslation(['common', 'layout'])(({ t }: WithTranslation) => {
   const { account } = useAccount()
   const { resetKeypair } = useResetAccount()
+  const { exportAccount } = useExportAccountInfo()
   const { defaultNetwork } = useSettings()
   const network = MapChainId[defaultNetwork] ?? MapChainId[1]
 
@@ -83,6 +84,50 @@ export const SecurityPanel = withTranslation(['common', 'layout'])(({ t }: WithT
             </Grid>
           </Box>
         )}
+        <Box component={'section'} display={'flex'} flexDirection={'column'} padding={5 / 2}>
+                <Grid container display={'flex'}>
+                  <Grid item xs={7}>
+                    <Typography
+                      variant={'h4'}
+                      color={'text.primary'}
+                      component={'h4'}
+                      marginBottom={1}
+                    >
+                      {t('labelTitleExportAccount')}
+                    </Typography>
+                    <Typography variant={'body1'} color={'text.secondary'} component={'p'}>
+                      {t('labelDescriptionExportAccount', {
+                        loopringLayer2: L1L2_NAME_DEFINED[network].loopringLayer2,
+                        loopringL2: L1L2_NAME_DEFINED[network].loopringL2,
+                        l2Symbol: L1L2_NAME_DEFINED[network].l2Symbol,
+                        l1Symbol: L1L2_NAME_DEFINED[network].l1Symbol,
+                        ethereumL1: L1L2_NAME_DEFINED[network].ethereumL1,
+                      })}
+                    </Typography>
+                  </Grid>
+                  <Grid
+                    item
+                    xs={5}
+                    display={'flex'}
+                    justifyContent={'flex-start'}
+                    alignItems={'flex-end'}
+                    flexDirection={'column'}
+                  >
+                    <Button
+                      onClick={() => {
+                        // exportAccInfo()
+                        exportAccount()
+                      }}
+                      variant={'outlined'}
+                      size={'medium'}
+                      color={'primary'}
+                      disabled={false}
+                    >
+                      {t('labelBtnExportAccount')}
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Box>
       </Grid>
     </StyledPaper>
   )
