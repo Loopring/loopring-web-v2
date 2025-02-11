f4fed8742d118227040075c55fd69d49d2b81279
john.li <john.li@loopring.io>
2025-02-11 15:20:50 +0800

    reset ui

--- packages/component-lib/src/components/modal/ModalPanels/UnlockAccount.tsx
+++ packages/component-lib/src/components/modal/ModalPanels/UnlockAccount.tsx
@@ -1,12 +1,14 @@
 import { Trans } from 'react-i18next'
 import { IconType, PanelProps, UnlockAccountBase } from './BasicPanel'
-import { Box, Link, Typography } from '@mui/material'
+import { Box, Button, Link, Typography } from '@mui/material'
 import { WalletType } from '@loopring-web/loopring-sdk'
 import { FEED_BACK_LINK } from '@loopring-web/common-resources'
 import { TextareaAutosizeStyled } from '../../basic-lib'
 import React from 'react'
 import { DropdownIconStyled } from '../../tradePanel'
 import { TransErrorHelp } from '../../block'
+import { Checkbox } from '@mui/material'
+import { CheckBoxIcon, CheckedIcon } from '@loopring-web/common-resources'
 
 // symbol
 export const UnlockAccount_WaitForAuth = (props: PanelProps) => {
@@ -38,8 +40,9 @@ export const UnlockAccount_Failed = ({
   error,
   errorOptions,
   t,
+  onClickReset,
   ...props
-}: PanelProps & { walletType?: WalletType; resetAccount: () => void }) => {
+}: PanelProps & { walletType?: WalletType; onClickReset: () => void }) => {
   const isContractOrInCounterFactual =
     props.walletType && (props.walletType.isContract || props.walletType.isInCounterFactualStatus)
   const showDropdown =
@@ -61,12 +64,7 @@ export const UnlockAccount_Failed = ({
               justifyContent={'center'}
               alignItems={'center'}
             >
-              <TransErrorHelp
-                error={
-                  error.message === 'unKnown' ? { ...error, message: 'Unknown Failure' } : error
-                }
-                options={errorOptions}
-              />
+              <TransErrorHelp error={error} options={errorOptions} />
               {showDropdown && <DropdownIconStyled status={dropdownStatus} fontSize={'medium'} />}
             </Typography>
             {isContractOrInCounterFactual && (
@@ -95,14 +93,75 @@ export const UnlockAccount_Failed = ({
               value={`${JSON.stringify(error)}}`}
             />
           ) : (
-            <Typography textAlign={'left'} marginTop={1} variant={'body2'}>
-              {t('labelUnlockErrorSupport1')} <br />
-              <br />
-              {t('labelUnlockErrorSupport2')}
-            </Typography>
+            <Box>
+              <Typography textAlign={'left'} variant={'body2'}>
+                {t('labelUnlockErrorLine1Part1')}
+                <Link onClick={onClickReset}>{t('labelUnlockErrorLine1Part2')}</Link>
+                {t('labelUnlockErrorLine1Part3')}
+              </Typography>
+              <Typography
+                textAlign={'left'}
+                onClick={props.resetAccount}
+                marginTop={1}
+                variant={'body2'}
+              >
+                {t('labelUnlockErrorLine2Part1')}
+                <Link onClick={onClickReset}>{t('labelUnlockErrorLine2Part2')}</Link>
+              </Typography>
+            </Box>
           ))}
       </>
     ),
   }
   return <UnlockAccountBase {...{ ...propsPatch, t }} />
+}
+
+export const UnlockAccount_Reset_Key_Confirm = ({
+  t,
+  resetAccount
+}: PanelProps & { walletType?: WalletType; resetAccount: () => void }) => {
+  const [checked, setChecked] = React.useState(false)
+  return (
+    <Box>
+      <Typography textAlign={'center'} mt={3} variant={'h3'}>
+        {t('labelUpdateAccount')}
+      </Typography>
+      <Box padding={4} mt={0}>
+        <Typography textAlign={'left'} color={'var(--color-text-secondary)'}>
+          Please read the following caveats carefully before resetting your EDDSA key:
+          <br />
+          <br />
+          • If you have any unsettled Dual Investment portfolios, your account will be locked until
+          they are settled.
+          <br />
+          • If you have an existing Portal position, you will not be able to reset your L2 on your
+          own.
+          <br />• If you are unsure about the sequence of this operation, please contact us at
+          support@loopring.io and provide your wallet details for assistance.
+        </Typography>
+
+        <Typography
+          textAlign={'left'}
+          color={checked ? 'var(--color-text-primary)' : 'var(--color-text-secondary)'}
+          my={3}
+        >
+          <Checkbox
+            checked={checked}
+            checkedIcon={<CheckedIcon />}
+            icon={<CheckBoxIcon />}
+            color='default'
+            sx={{ height: 16, width: 16, mr: 0.5 }}
+            onChange={() => {
+              setChecked(!checked)
+            }}
+          />
+          I have read the caveats and still want to reset the EDDSA key on my own.
+        </Typography>
+
+        <Button variant={'contained'} disabled={!checked} fullWidth onClick={resetAccount}>
+          Reset EDDSA Key
+        </Button>
+      </Box>
+    </Box>
+  )
 }
