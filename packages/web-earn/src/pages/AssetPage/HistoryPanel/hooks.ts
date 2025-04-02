3b47dc869e782bf8d7cdd838b095b85d684ad1fd
john.li <john.li@loopring.io>
2025-04-02 10:10:39 +0800

    wip

--- packages/web-earn/src/pages/AssetPage/HistoryPanel/hooks.ts
+++ packages/web-earn/src/pages/AssetPage/HistoryPanel/hooks.ts
@@ -1314,7 +1314,7 @@ export const useVaultTransaction = <R extends RawDataVaultTxItem>(
             operateTypes:
               props?.operateTypes ??
               [
-                0, 1, 2, 3, 4, 5, 6, 7
+                0, 1, 2, 3, 4, 5, 6, 7, 8
               ].join(','),
           },
           apiKey,
